6461f8176c33f9d66dad69c52c5f192428d8080c
Zebeqo <z715596546@qq.com>
2023-01-25 03:19:45 +0800

    refactor: put loadingSpinner icon in LucideIcon

--- templates/next-template/components/icons.tsx
+++ templates/next-template/components/icons.tsx
@@ -27,4 +27,26 @@ export const Icons = {
       ></path>
     </svg>
   ),
+  loadingSpinner: (props: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  ),
 }
--- templates/next-template/components/theme-toggle.tsx
+++ templates/next-template/components/theme-toggle.tsx
@@ -19,31 +19,10 @@ export function ThemeToggle() {
     setMounted(true)
   }, [])
 
-  if (!mounted)
+  if (mounted)
     return (
       <Button variant="ghost" size="sm">
-        <span className="h-6 w-6 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100">
-          <svg
-            className="animate-spin"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        </span>
+        <Icons.loadingSpinner className="h-6 w-6 animate-spin hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
       </Button>
     )
 
