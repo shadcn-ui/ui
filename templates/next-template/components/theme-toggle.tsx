23130195b9b35d8d098b6b445ec04ed6bc850e3b
Zebeqo <z715596546@qq.com>
2023-01-25 02:33:40 +0800

    feat: toggle theme icon at the same time

--- templates/next-template/components/theme-toggle.tsx
+++ templates/next-template/components/theme-toggle.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useEffect, useState } from "react"
 import { useTheme } from "next-themes"
 
 import { Icons } from "@/components/icons"
@@ -11,13 +12,52 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted)
+    return (
+      <Button variant="ghost" size="sm">
+        <span className="h-6 w-6 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100">
+          <svg
+            className="animate-spin"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </span>
+      </Button>
+    )
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm">
-          <Icons.sun className="hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
+          {theme === "dark" ? (
+            <Icons.moon className="hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
+          ) : theme === "light" ? (
+            <Icons.sun className="hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
+          ) : (
+            <Icons.laptop className="hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
