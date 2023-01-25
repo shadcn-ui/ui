411ac8ad37df00dfd43c320c5ced42c41cfc1de3
zebeqo <68463968+Zebeqo@users.noreply.github.com>
2023-01-26 00:00:03 +0800

    refactor: remove closing tag
    Co-authored-by: Franklin Javier <franklinjalves@gmail.com>

--- templates/next-template/components/icons.tsx
+++ templates/next-template/components/icons.tsx
@@ -41,12 +41,12 @@ export const Icons = {
         r="10"
         stroke="currentColor"
         strokeWidth="4"
-      ></circle>
+      />
       <path
         className="opacity-75"
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
+     />
     </svg>
   ),
 }
