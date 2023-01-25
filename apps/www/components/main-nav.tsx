57a2dcd9f78bf042471a3da13802780c89b8ed9a
joshwaiam <joshwaiam@gmail.com>
2023-01-24 20:05:20 -0500

    Added missing new line

--- apps/www/components/main-nav.tsx
+++ apps/www/components/main-nav.tsx
@@ -28,6 +28,7 @@ interface MainNavProps {
 
 export function MainNav({ items, children }: MainNavProps) {
   const segment = useSelectedLayoutSegment()
+
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="hidden items-center space-x-2 lg:flex">
