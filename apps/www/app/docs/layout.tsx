35bbd5eb5b245b6b547217dcbac9fde02dc32d4c
Doğukan Çavuş <38227845+dgknca@users.noreply.github.com>
2023-01-26 11:00:20 +0300

    fix(#7): always prefer `minmax(0, 1fr)` instead of `1fr`

--- apps/www/app/docs/layout.tsx
+++ apps/www/app/docs/layout.tsx
@@ -8,7 +8,7 @@ interface DocsLayoutProps {
 
 export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
-    <div className="flex-1 items-start md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10">
+    <div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
       <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r border-r-slate-100 dark:border-r-slate-700 md:sticky md:block">
         <ScrollArea className="pr-6 lg:py-10">
           <DocsSidebarNav items={docsConfig.sidebarNav} />
