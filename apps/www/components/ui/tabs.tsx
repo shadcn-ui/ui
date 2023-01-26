f4b93447864fc55aaff634574ef2203019c903a2
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2023-01-26 13:29:49 -0300

    fix: minor typos (#2)

--- apps/www/components/ui/tabs.tsx
+++ apps/www/components/ui/tabs.tsx
@@ -1,17 +1,17 @@
 "use client"
 
 import * as React from "react"
-import * as TabsPrimivite from "@radix-ui/react-tabs"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
 
 import { cn } from "@/lib/utils"
 
-const Tabs = TabsPrimivite.Root
+const Tabs = TabsPrimitive.Root
 
 const TabsList = React.forwardRef<
-  React.ElementRef<typeof TabsPrimivite.List>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimivite.List>
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
 >(({ className, ...props }, ref) => (
-  <TabsPrimivite.List
+  <TabsPrimitive.List
     ref={ref}
     className={cn(
       "inline-flex items-center justify-center rounded-md bg-slate-100 p-1 dark:bg-slate-800",
@@ -20,13 +20,13 @@ const TabsList = React.forwardRef<
     {...props}
   />
 ))
-TabsList.displayName = TabsPrimivite.List.displayName
+TabsList.displayName = TabsPrimitive.List.displayName
 
 const TabsTrigger = React.forwardRef<
-  React.ElementRef<typeof TabsPrimivite.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimivite.Trigger>
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
 >(({ className, ...props }, ref) => (
-  <TabsPrimivite.Trigger
+  <TabsPrimitive.Trigger
     className={cn(
       "inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-3 py-1.5  text-sm font-medium text-slate-700 transition-all  disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm dark:text-slate-200 dark:data-[state=active]:bg-slate-900",
       className
@@ -35,13 +35,13 @@ const TabsTrigger = React.forwardRef<
     ref={ref}
   />
 ))
-TabsTrigger.displayName = TabsPrimivite.Trigger.displayName
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
 
 const TabsContent = React.forwardRef<
-  React.ElementRef<typeof TabsPrimivite.Content>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimivite.Content>
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
 >(({ className, ...props }, ref) => (
-  <TabsPrimivite.Content
+  <TabsPrimitive.Content
     className={cn(
       "mt-2 rounded-md border border-slate-200 p-6 dark:border-slate-700",
       className
@@ -50,6 +50,6 @@ const TabsContent = React.forwardRef<
     ref={ref}
   />
 ))
-TabsContent.displayName = TabsPrimivite.Content.displayName
+TabsContent.displayName = TabsPrimitive.Content.displayName
 
 export { Tabs, TabsList, TabsTrigger, TabsContent }
