058c81bc88e39ab946a10b051152350573e18d4b
0xATHERIS <ath3ris@proton.me>
2023-01-27 01:19:35 +0000

    Removed animate-in class causing bug from MenubarContent

--- templates/next-template/components/ui/menubar.tsx
+++ templates/next-template/components/ui/menubar.tsx
@@ -97,7 +97,7 @@ const MenubarContent = React.forwardRef<
         alignOffset={alignOffset}
         sideOffset={sideOffset}
         className={cn(
-          "z-50 min-w-[12rem] overflow-hidden rounded-md border border-slate-100 bg-white p-1 text-slate-700 shadow-md animate-in slide-in-from-top-1 dark:border-slate-800 dark:bg-slate-800 dark:text-slate-400",
+          "z-50 min-w-[12rem] overflow-hidden rounded-md border border-slate-100 bg-white p-1 text-slate-700 shadow-md dark:border-slate-800 dark:bg-slate-800 dark:text-slate-400",
           className
         )}
         {...props}
