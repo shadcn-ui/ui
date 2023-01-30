6d37fd603f7bfb830c8c3d6b2e1dee9a4bf8e225
Vincent Dusautoir <vincent.dusautoir.pro@gmail.com>
2023-01-30 11:59:00 +0100

    fix: move position in toast component

--- apps/www/components/examples/toast/advanced.tsx
+++ apps/www/components/examples/toast/advanced.tsx
@@ -91,7 +91,7 @@ export const ToastAdvanced = () => {
           Show toast
         </Button>
         <ToastProvider swipeDirection={swipeDirection}>
-          <Toast open={open} onOpenChange={setOpen}>
+          <Toast open={open} onOpenChange={setOpen} position={position}>
             <ToastTitle>Awesome toast</ToastTitle>
             <ToastDescription>
               Made with RadixUI and TailwindCSS
@@ -99,7 +99,7 @@ export const ToastAdvanced = () => {
             <ToastClose />
           </Toast>
 
-          <ToastViewport position={position} />
+          <ToastViewport />
         </ToastProvider>
       </div>
     </div>
