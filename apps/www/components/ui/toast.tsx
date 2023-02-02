0929a214cdcd780726c61c7ef16bdfbafb52d58c
Vincent Dusautoir <vincent.dusautoir.pro@gmail.com>
2023-02-02 22:38:56 +0100

    feat: add ref on every component

--- apps/www/components/ui/toast.tsx
+++ apps/www/components/ui/toast.tsx
@@ -1,27 +1,32 @@
-import * as ToastPrimitives from "@radix-ui/react-toast"
+import React from "react"
+import * as ToastPrimitive from "@radix-ui/react-toast"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-const ToastAction: React.FC<ToastPrimitives.ToastActionProps> = ({
-  className,
-  ...props
-}) => (
-  <ToastPrimitives.Action
+const ToastAction = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitive.ToastAction>,
+  React.ComponentPropsWithoutRef<typeof ToastPrimitive.ToastAction>
+>(({ className, ...props }, ref) => (
+  <ToastPrimitive.Action
+    ref={ref}
     className={cn(
       "inline-flex h-6 items-center justify-center rounded-md border border-slate-400/30 bg-transparent px-2 text-sm font-medium leading-none transition-colors hover:bg-slate-500/10 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=open]:bg-slate-100 dark:border-slate-50/50 dark:text-slate-100 dark:hover:bg-slate-100/30 dark:hover:text-slate-100 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900 dark:data-[state=open]:bg-slate-800",
       className
     )}
     {...props}
   />
-)
+))
 
-const ToastClose: React.FC<ToastPrimitives.ToastCloseProps> = ({
-  className,
-  ...props
-}) => (
-  <ToastPrimitives.Close
+ToastAction.displayName = ToastPrimitive.Action.displayName
+
+const ToastClose = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitive.ToastClose>,
+  React.ComponentPropsWithoutRef<typeof ToastPrimitive.ToastClose>
+>(({ className, ...props }, ref) => (
+  <ToastPrimitive.Close
+    ref={ref}
     className={cn(
       "absolute top-1 right-1 rounded-md p-1 hover:bg-slate-500/10 dark:hover:bg-slate-100/30",
       className
@@ -29,33 +34,45 @@ const ToastClose: React.FC<ToastPrimitives.ToastCloseProps> = ({
     {...props}
   >
     <X className="h-4 w-4" />
-  </ToastPrimitives.Close>
-)
+  </ToastPrimitive.Close>
+))
 
-const ToastTitle: React.FC<ToastPrimitives.ToastTitleProps> = ({
-  className,
-  ...props
-}) => (
-  <ToastPrimitives.Title
+ToastClose.displayName = ToastPrimitive.Close.displayName
+
+const ToastTitle = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof ToastPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <ToastPrimitive.Title
+    ref={ref}
     className={cn("font-semibold text-slate-900 dark:text-slate-50", className)}
     {...props}
   />
-)
+))
 
-const ToastDescription: React.FC<ToastPrimitives.ToastDescriptionProps> = ({
-  className,
-  ...props
-}) => (
-  <ToastPrimitives.Description
+ToastTitle.displayName = ToastPrimitive.Title.displayName
+
+const ToastDescription = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitive.Description>,
+  React.ComponentPropsWithoutRef<typeof ToastPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <ToastPrimitive.Description
+    ref={ref}
     className={cn("text-sm leading-none", className)}
     {...props}
   />
-)
+))
 
-type ToastProps = ToastPrimitives.ToastProps
+ToastDescription.displayName = ToastPrimitive.Description.displayName
 
-const Toast: React.FC<ToastProps> = ({ className, ...props }) => (
-  <ToastPrimitives.Root
+type ToastProps = ToastPrimitive.ToastProps
+
+const Toast = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitive.Root>,
+  ToastProps
+>(({ className, ...props }, ref) => (
+  <ToastPrimitive.Root
+    ref={ref}
     className={cn(
       [
         "pointer-events-auto relative w-full items-start rounded-md border border-b-4 border-slate-900 bg-white p-3 shadow-lg dark:border-slate-700 dark:bg-slate-800",
@@ -69,11 +86,11 @@ const Toast: React.FC<ToastProps> = ({ className, ...props }) => (
     )}
     {...props}
   />
-)
+))
 
-const ToastProvider: React.FC<ToastPrimitives.ToastProviderProps> = (props) => (
-  <ToastPrimitives.Provider swipeDirection="right" duration={3000} {...props} />
-)
+Toast.displayName = ToastPrimitive.Root.displayName
+
+const ToastProvider = ToastPrimitive.Provider
 
 const toastViewportVariants = cva(
   "fixed z-50 flex max-h-screen w-full flex-col items-end justify-end gap-3 p-6 md:max-w-[390px] pointer-events-none",
@@ -95,15 +112,19 @@ const toastViewportVariants = cva(
   }
 )
 
-const ToastViewport: React.FC<
-  ToastPrimitives.ToastViewportProps &
+const ToastViewport = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitive.Viewport>,
+  React.ComponentPropsWithoutRef<typeof ToastPrimitive.Viewport> &
     VariantProps<typeof toastViewportVariants>
-> = ({ className, position, ...props }) => (
-  <ToastPrimitives.Viewport
+>(({ className, position, ...props }, ref) => (
+  <ToastPrimitive.Viewport
+    ref={ref}
     className={cn(toastViewportVariants({ position }), className)}
     {...props}
   />
-)
+))
+
+ToastViewport.displayName = ToastPrimitive.Viewport.displayName
 
 export {
   ToastProvider,
