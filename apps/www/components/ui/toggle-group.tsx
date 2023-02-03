15638ae640767b327bc85029ee0d593cf20d9d6b
KXLAA <kolade.afode@yahoo.com>
2023-02-03 22:13:44 +0100

    fix: remove cva from toggle group component
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- apps/www/components/ui/toggle-group.tsx
+++ apps/www/components/ui/toggle-group.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group"
-import { VariantProps, cva } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
