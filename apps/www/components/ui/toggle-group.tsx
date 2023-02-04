c2d4a9c951f0c2f6244b6b223fbf926f5772dbba
KXLAA <kolade.afode@yahoo.com>
2023-02-04 11:02:10 +0100

    feat: update toggle group component with size and variant  variants
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- apps/www/components/ui/toggle-group.tsx
+++ apps/www/components/ui/toggle-group.tsx
@@ -2,25 +2,83 @@
 
 import * as React from "react"
 import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group"
+import { VariantProps, cva } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
-const ToggleGroup = ToggleGroupPrimitive.Root
+const ToggleGroupContext = React.createContext<
+  VariantProps<typeof toggleGroupItemVariants>
+>({
+  size: "default",
+  variant: "default",
+})
 
-const ToggleGroupItem = React.forwardRef<
-  React.ElementRef<typeof ToggleGroupPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item>
->(({ className, children, ...props }, ref) => (
-  <ToggleGroupPrimitive.Item
+const toggleGroupItemVariants = cva(
+  "ml-[1px] inline-flex h-10 items-center justify-center transition-colors first:ml-0 first:rounded-l last:rounded-r text-sm font-medium dark:hover:bg-slate-800 data-[state=on]:bg-slate-200 dark:data-[state=on]:text-slate-100 dark:data-[state=on]:bg-slate-700 focus:outline-none focus:relative focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:focus:ring-offset-slate-900 dark:text-slate-100 dark:hover:text-slate-100",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-slate-900 text-white  hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900 data-[state=on]:text-slate-900 ",
+        outline: "bg-transparent hover:bg-slate-100 text-slate-900",
+      },
+      size: {
+        default: "h-10 px-3",
+        sm: "h-9 px-2.5",
+        lg: "h-11 px-5",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+const ToggleGroup = React.forwardRef<
+  React.ElementRef<typeof ToggleGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root> &
+    VariantProps<typeof toggleGroupItemVariants>
+>(({ className, variant, size, children, ...props }, ref) => (
+  <ToggleGroupPrimitive.Root
     ref={ref}
     className={cn(
-      "ml-[1px] inline-flex h-10 items-center justify-center bg-slate-900 px-3 text-sm font-medium text-white transition-colors first:ml-0 first:rounded-l last:rounded-r hover:bg-slate-700 focus:relative focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-slate-500 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-100 dark:focus:ring-offset-slate-900 dark:data-[state=on]:bg-slate-500 dark:data-[state=on]:text-slate-100"
+      "inline-flex items-center justify-center rounded border border-slate-200 bg-transparent shadow dark:border-slate-700",
+      className
     )}
     {...props}
   >
-    {children}
-  </ToggleGroupPrimitive.Item>
+    <ToggleGroupContext.Provider value={{ variant, size }}>
+      {children}
+    </ToggleGroupContext.Provider>
+  </ToggleGroupPrimitive.Root>
 ))
+
+ToggleGroup.displayName = ToggleGroupPrimitive.Root.displayName
+
+const ToggleGroupItem = React.forwardRef<
+  React.ElementRef<typeof ToggleGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item> &
+    VariantProps<typeof toggleGroupItemVariants>
+>(({ className, children, variant, size, ...props }, ref) => {
+  const context = React.useContext(ToggleGroupContext)
+
+  return (
+    <ToggleGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        toggleGroupItemVariants({
+          variant: context.variant || variant,
+          size: context.size || size,
+        }),
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </ToggleGroupPrimitive.Item>
+  )
+})
 
 ToggleGroupItem.displayName = ToggleGroupPrimitive.Item.displayName
 
