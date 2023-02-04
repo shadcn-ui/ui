3e23b2a55f6bee7b283b00c28a1d5ceb0440a321
Lucas Thevenet Cervantes <lucastcervantes@gmail.com>
2023-02-04 17:14:10 -0300

    fix(slider): value prop not being passed

--- templates/next-template/components/ui/slider.tsx
+++ templates/next-template/components/ui/slider.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils"
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
