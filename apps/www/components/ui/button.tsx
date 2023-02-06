8fc72f6f1608543685a02227a597053a7d45c69e
Ding <ouud@fastmail.com>
2023-02-07 06:15:20 +0900

    feat: add isLoading property to button component

--- apps/www/components/ui/button.tsx
+++ apps/www/components/ui/button.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { VariantProps, cva } from "class-variance-authority"
+import { Loader2 } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -35,16 +36,32 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean
+}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, isLoading, variant, size, children, ...props }, ref) => {
     return (
       <button
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(
+          buttonVariants({ variant, size, className })
+          // todo: cursor-not-allowed won't work
+          // because hover event looks not triggering
+          // due to this
+          //     .disabled\:pointer-events-none:disabled pointer-events: none;
+          //
+          // isLoading ? "cursor-not-allowed" : "disabled:pointer-events-none"
+        )}
         ref={ref}
-        {...props}
-      />
+        {...{
+          disabled: isLoading,
+          ...props,
+        }}
+      >
+        {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+        {children}
+      </button>
     )
   }
 )
