14fe0825b332a50a845c33a53f424e9578139039
Pedro Filho <pedro@filho.me>
2023-02-13 13:12:46 +0000

    fixing a11y issue with "danger" button

--- apps/www/components/callout.tsx
+++ apps/www/components/callout.tsx
@@ -1,9 +1,9 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 interface CalloutProps {
-  icon?: string
-  children?: React.ReactNode
-  type?: "default" | "warning" | "danger"
+  icon?: string;
+  children?: React.ReactNode;
+  type?: "default" | "warning" | "danger";
 }
 
 export function Callout({
@@ -18,7 +18,7 @@ export function Callout({
         "my-6 flex items-start rounded-md border border-b-4 border-slate-900 p-4",
         {
           "border-slate-900 dark:border-slate-700": type === "default",
-          "border-red-500": type === "danger",
+          "border-red-600": type === "danger",
           "border-yellow-500": type === "warning",
         }
       )}
@@ -27,5 +27,5 @@ export function Callout({
       {icon && <span className="mr-4 text-2xl">{icon}</span>}
       <div>{children}</div>
     </div>
-  )
+  );
 }
--- templates/next-template/components/ui/button.tsx
+++ templates/next-template/components/ui/button.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { VariantProps, cva } from "class-variance-authority"
+import * as React from "react";
+import { VariantProps, cva } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
@@ -11,7 +11,7 @@ const buttonVariants = cva(
         default:
           "bg-slate-900 text-white hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900",
         destructive:
-          "bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600",
+          "bg-red-600 text-white hover:bg-red-600 dark:hover:bg-red-600",
         outline:
           "bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100",
         subtle:
@@ -31,7 +31,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -45,9 +45,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
