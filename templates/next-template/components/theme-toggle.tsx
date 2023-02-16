75226c19acd966fa9cfb2cd3e75931d939943bdf
its-monotype <its.monotype@gmail.com>
2023-02-16 15:13:19 +0100

    feat: make icon reflect theme setting

--- templates/next-template/components/theme-toggle.tsx
+++ templates/next-template/components/theme-toggle.tsx
@@ -17,7 +17,8 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm">
-          <Icons.sun className="hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
+          <Icons.sun className="rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
+          <Icons.moon className="absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
