277d49c90609709fe2f88bf8c319c4a8223fbdd1
Damian Ricobelli <dricobelli@gmail.com>
2023-04-09 20:23:58 -0300

    fix(menu triggers): prevents white trigger background when background is dark

--- apps/www/components/ui/context-menu.tsx
+++ apps/www/components/ui/context-menu.tsx
@@ -8,7 +8,19 @@ import { cn } from "@/lib/utils"
 
 const ContextMenu = ContextMenuPrimitive.Root
 
-const ContextMenuTrigger = ContextMenuPrimitive.Trigger
+const ContextMenuTrigger = React.forwardRef<
+  React.ElementRef<typeof ContextMenuPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <ContextMenuPrimitive.Trigger
+    ref={ref}
+    className={cn("data-[state='open']:bg-inherit", className)}
+    {...props}
+  >
+    {children}
+  </ContextMenuPrimitive.Trigger>
+))
+ContextMenuTrigger.displayName = ContextMenuPrimitive.Trigger.displayName
 
 const ContextMenuGroup = ContextMenuPrimitive.Group
 
--- apps/www/components/ui/dropdown-menu.tsx
+++ apps/www/components/ui/dropdown-menu.tsx
@@ -8,7 +8,19 @@ import { cn } from "@/lib/utils"
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
-const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
+const DropdownMenuTrigger = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <DropdownMenuPrimitive.Trigger
+    ref={ref}
+    className={cn("data-[state='open']:bg-inherit", className)}
+    {...props}
+  >
+    {children}
+  </DropdownMenuPrimitive.Trigger>
+))
+DropdownMenuTrigger.displayName = DropdownMenuPrimitive.Trigger.displayName
 
 const DropdownMenuGroup = DropdownMenuPrimitive.Group
 
