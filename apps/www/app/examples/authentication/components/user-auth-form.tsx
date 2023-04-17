7a87fbfa771449a8e3a3b502399791278ae1f86b
shadcn <m@shadcn.com>
2023-04-17 19:41:45 +0400

    fix(www): background color for auth form

--- apps/www/app/examples/authentication/components/user-auth-form.tsx
+++ apps/www/app/examples/authentication/components/user-auth-form.tsx
@@ -53,7 +53,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
           <span className="w-full border-t" />
         </div>
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-white px-2 text-muted-foreground">
+          <span className="bg-background px-2 text-muted-foreground">
             Or continue with
           </span>
         </div>
