c3d2a9da31aa23b2287bc51a03d64a7fd774fedb
Gimel Dick <gimeldick@gmail.com>
2023-04-17 19:30:06 -0400

    fix: typo in badge component

--- apps/www/components/examples/badge/destructive.tsx
+++ apps/www/components/examples/badge/destructive.tsx
@@ -1,5 +1,5 @@
 import { Badge } from "@/components/ui/badge"
 
 export function BadgeDestructive() {
-  return <Badge variant="desctructive">Destructive</Badge>
+  return <Badge variant="destructive">Destructive</Badge>
 }
