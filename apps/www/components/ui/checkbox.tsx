9376ca1921cd7357165d3189bb77f9ef6b418f89
Mike Robinson <multiwebinc@users.noreply.github.com>
2023-04-20 10:29:16 -0600

    Update checkbox.tsx

--- apps/www/components/ui/checkbox.tsx
+++ apps/www/components/ui/checkbox.tsx
@@ -13,13 +13,15 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-input ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary",
+      "peer h-4 w-4 shrink-0 rounded-sm border border-primary bg-white ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary",
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-primary")}
+      className={cn(
+        "flex items-center justify-center text-primary dark:text-primary-foreground"
+      )}
     >
       <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
