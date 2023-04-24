3594682c9f19772b6d1011bf903790214f075575
Prince Hernandez <princezhm@gmail.com>
2023-04-24 09:59:05 -0300

    feat(cli): add config files and examples in docs

--- packages/cli/src/utils/get-project-info.ts
+++ packages/cli/src/utils/get-project-info.ts
@@ -5,19 +5,15 @@ import fs from "fs-extra"
 export async function getProjectInfo() {
   const info = {
     tsconfig: null,
-    alias: null,
     srcDir: false,
     appDir: false,
   }
 
   try {
     const tsconfig = await getTsConfig()
-    const paths = tsconfig?.compilerOptions?.paths
-    const alias = paths ? Object.keys(paths)[0].replace("*", "") : null
 
     return {
       tsconfig,
-      alias,
       srcDir: existsSync(path.resolve("./src")),
       appDir:
         existsSync(path.resolve("./app")) ||
