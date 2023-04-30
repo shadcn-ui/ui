a623c7d86126d6b9acec17420338e2745bc0751b
shadcn <m@shadcn.com>
2023-04-30 12:13:38 +0400

    fix(accordion): add focus-visible states

--- apps/www/components/ui/accordion.tsx
+++ apps/www/components/ui/accordion.tsx
@@ -28,7 +28,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 pr-1 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between rounded-md py-4 font-medium transition-all hover:underline focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-4 [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
