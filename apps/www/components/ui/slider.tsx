6a4cd2cbe33c28265e46242f62a3eb2e12158171
tcortega <tarcisiocaetano2009@gmail.com>
2023-05-12 18:39:56 -0300

    feat(Slider): add support for multiple thumbs in Slider component

--- apps/www/components/ui/slider.tsx
+++ apps/www/components/ui/slider.tsx
@@ -5,10 +5,26 @@ import * as SliderPrimitive from "@radix-ui/react-slider"
 
 import { cn } from "@/lib/utils"
 
+const SliderThumb = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Thumb>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Thumb>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Thumb
+    ref={ref}
+    className={cn(
+      "block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+      className
+    )}
+    {...props}
+  />
+));
+
+SliderThumb.displayName = SliderPrimitive.Thumb.displayName
+
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
@@ -20,9 +36,10 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
       <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    {children || <SliderThumb />}
   </SliderPrimitive.Root>
 ))
+
 Slider.displayName = SliderPrimitive.Root.displayName
 
-export { Slider }
+export { Slider, SliderThumb }
