3f249a543ad4cd1d6df017081b2967b175d7f677
iqbalio <133704899+iqbalio@users.noreply.github.com>
2023-05-16 01:51:47 +0500

    Update calendar.tsx
    Pushing the days to end of row instead of start

--- apps/www/components/ui/calendar.tsx
+++ apps/www/components/ui/calendar.tsx
@@ -35,7 +35,7 @@ function Calendar({
         head_row: "flex",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
-        row: "flex w-full mt-2",
+        row: "flex first:justify-end w-full mt-2",
         cell: "text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
