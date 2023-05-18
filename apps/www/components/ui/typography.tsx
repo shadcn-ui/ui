17585f85a6d7959d9c3db17aa0f006cfeb7c06b4
nilaq <nbitzer@stanford.edu>
2023-05-17 17:28:33 -0700

    feat: variants and <Typography> component

--- apps/www/components/ui/typography.tsx
+++ apps/www/components/ui/typography.tsx
@@ -1,214 +1,51 @@
-'use client'
+import * as React from "react";
+import { VariantProps, cva } from "class-variance-authority";
 
-import * as React from 'react'
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const H1 = React.forwardRef<
-  HTMLHeadingElement,
-  React.ComponentPropsWithoutRef<'h1'>
->(({ className, ...props }, ref) => {
-  return (
-    <h1
-      className={cn(
-        'scroll-m-20 text-4xl font-extrabold tracking-tight text-foreground lg:text-5xl',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-H1.displayName = 'H1'
+const typographyVariants = cva("text-foreground", {
+    variants: {
+        as: {
+            h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+            h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+            h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
+            h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+            h5: "scroll-m-20 text-lg font-semibold tracking-tight",
+            h6: "scroll-m-20 text-base font-semibold tracking-tight",
+            p: "leading-7 [&:not(:first-child)]:mt-6",
+            blockquote: "mt-6 border-l-2 pl-6 italic",
+            ul: "my-6 ml-6 list-disc [&>li]:mt-2",
+            inlineCode:
+                "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+            lead: "text-xl text-muted-foreground",
+            largeText: "text-lg font-semibold",
+            smallText: "text-sm font-medium leading-none",
+            mutedText: "text-sm text-muted-foreground",
+        },
+    },
+});
 
-const H2 = React.forwardRef<
-  HTMLHeadingElement,
-  React.ComponentPropsWithoutRef<'h2'>
->(({ className, ...props }, ref) => {
-  return (
-    <h2
-      className={cn(
-        'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight text-foreground first:mt-0',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-H2.displayName = 'H2'
+type Element = keyof JSX.IntrinsicElements;
 
-const H3 = React.forwardRef<
-  HTMLHeadingElement,
-  React.ComponentPropsWithoutRef<'h3'>
->(({ className, ...props }, ref) => {
-  return (
-    <h3
-      className={cn(
-        'scroll-m-20 text-2xl font-semibold tracking-tight text-foreground',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-H3.displayName = 'H3'
+type TypographyProps<T extends Element> = {
+    element: T;
+} & VariantProps<typeof typographyVariants> &
+    React.HTMLAttributes<HTMLElement>;
 
-const H4 = React.forwardRef<
-  HTMLHeadingElement,
-  React.ComponentPropsWithoutRef<'h4'>
->(({ className, ...props }, ref) => {
-  return (
-    <h4
-      className={cn(
-        'scroll-m-20 text-xl font-semibold tracking-tight text-foreground',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-H4.displayName = 'H4'
+const Typography = <T extends Element>({
+    className,
+    element,
+    as,
+    ...props
+}: TypographyProps<T>) => {
+    const Component = element;
 
-const P = React.forwardRef<
-  HTMLParagraphElement,
-  React.ComponentPropsWithoutRef<'p'>
->(({ className, ...props }, ref) => {
-  return (
-    <p
-      className={cn(
-        'leading-7 text-foreground [&:not(:first-child)]:mt-6',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-P.displayName = 'P'
+    const componentProps = {
+        className: cn(typographyVariants({ as, className })),
+        ...props,
+    };
 
-const Blockquote = React.forwardRef<
-  HTMLQuoteElement,
-  React.ComponentPropsWithoutRef<'blockquote'>
->(({ className, ...props }, ref) => {
-  return (
-    <blockquote
-      className={cn(
-        'mt-6 border-l-2 pl-6 italic text-foreground',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-Blockquote.displayName = 'Blockquote'
+    return React.createElement(Component, componentProps);
+};
 
-const List = React.forwardRef<
-  HTMLUListElement,
-  React.ComponentPropsWithoutRef<'ul'>
->(({ className, ...props }, ref) => {
-  return (
-    <ul
-      className={cn(
-        'my-6 ml-6 list-disc text-foreground [&>li]:mt-2',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-List.displayName = 'List'
-
-const InlineCode = React.forwardRef<
-  HTMLElement,
-  React.ComponentPropsWithoutRef<'code'>
->(({ className, ...props }, ref) => {
-  return (
-    <code
-      className={cn(
-        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold text-foreground',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-InlineCode.displayName = 'InlineCode'
-
-const Lead = React.forwardRef<
-  HTMLParagraphElement,
-  React.ComponentPropsWithoutRef<'p'>
->(({ className, ...props }, ref) => {
-  return (
-    <p
-      className={cn('text-xl text-muted-foreground', className)}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-Lead.displayName = 'Lead'
-
-const LargeText = React.forwardRef<
-  HTMLDivElement,
-  React.ComponentPropsWithoutRef<'div'>
->(({ className, ...props }, ref) => {
-  return (
-    <div
-      className={cn('text-lg font-semibold text-foreground', className)}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-LargeText.displayName = 'LargeText'
-
-const SmallText = React.forwardRef<
-  HTMLDivElement,
-  React.ComponentPropsWithoutRef<'div'>
->(({ className, ...props }, ref) => {
-  return (
-    <div
-      className={cn(
-        'text-sm font-medium leading-none text-foreground',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-SmallText.displayName = 'SmallText'
-
-const MutatedText = React.forwardRef<
-  HTMLDivElement,
-  React.ComponentPropsWithoutRef<'div'>
->(({ className, ...props }, ref) => {
-  return (
-    <div
-      className={cn('text-sm text-muted-foreground', className)}
-      ref={ref}
-      {...props}
-    />
-  )
-})
-MutatedText.displayName = 'MutatedText'
-
-export {
-  H1,
-  H2,
-  H3,
-  H4,
-  P,
-  Blockquote,
-  List,
-  InlineCode,
-  Lead,
-  LargeText,
-  SmallText,
-  MutatedText
-}
+export default React.forwardRef(Typography);
