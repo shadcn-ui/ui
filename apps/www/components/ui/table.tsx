b0119e3d44106af65dadc7687bed14e5559133fb
Yash Baranwal <35179810+yashbaranwal@users.noreply.github.com>
2023-05-21 04:21:50 +0530

    yb/issue:#391/remove-div-wrapper

--- apps/www/components/ui/table.tsx
+++ apps/www/components/ui/table.tsx
@@ -6,13 +6,11 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="w-full overflow-auto">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
       {...props}
     />
-  </div>
 ))
 Table.displayName = "Table"
 
