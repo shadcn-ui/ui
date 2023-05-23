c655eef58cb95d0aea13b8c05d8ba398b33ca9f7
Joseph Carroll <jdsalingerjr@gmail.com>
2023-05-23 12:25:54 -0700

    feat: deeper intellisense support

--- templates/next-template/lib/utils.ts
+++ templates/next-template/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function unmap(styles: Record<string, string>) {
+  return Object.values(styles).join(' ');
+}
