089059b359e0ce2289e520ae81643b76d0bf5ea9
Shoaib Ahmed <ashoaib540918@gmail.com>
2023-05-23 22:50:32 +0530

    add position relative to table element

--- apps/www/components/ui/table.tsx
+++ apps/www/components/ui/table.tsx
@@ -9,7 +9,7 @@ const Table = React.forwardRef<
   <div className="w-full overflow-auto">
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn("w-full caption-bottom text-sm relative", className)}
       {...props}
     />
   </div>
