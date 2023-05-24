1e846392be93216140a6807e6814fed33c1e6ecb
Joseph Carroll <jdsalingerjr@gmail.com>
2023-05-24 09:37:03 -0700

    fix: remove unnecessary method

--- templates/next-template/lib/utils.ts
+++ templates/next-template/lib/utils.ts
@@ -4,7 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function unmap(styles: Record<string, string>) {
-  return Object.values(styles).join(' ');
-}
