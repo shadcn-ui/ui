5736ae18f88e3d518dea9bc3b555ef80940998c7
zce <w@zce.me>
2023-05-24 19:23:56 +0800

    feat: add collapsible animation

--- apps/www/components/ui/collapsible.tsx
+++ apps/www/components/ui/collapsible.tsx
@@ -1,11 +1,16 @@
-"use client"
-
+import React from 'react'
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+import { cn } from "@/lib/utils"
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+const CollapsibleContent = React.forwardRef<React.ElementRef<typeof CollapsiblePrimitive.Content>, React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Content>>(({ className, children, ...props }, ref) => (
+  <CollapsiblePrimitive.Content ref={ref} className={cn('overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down', className)} {...props}>
+    {children}
+  </CollapsiblePrimitive.Content>
+))
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -65,10 +65,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        'collapsible-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-collapsible-content-height)' }
+        },
+        'collapsible-up': {
+          from: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: 0 }
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        'collapsible-down': 'collapsible-down 0.2s ease-out',
+        'collapsible-up': 'collapsible-up 0.2s ease-out',
       },
     },
   },
