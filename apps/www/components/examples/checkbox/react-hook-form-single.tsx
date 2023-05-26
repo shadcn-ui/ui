0659d67db4931603630c0c1f3873de8e85d7653b
ozzyfromspace <float.circuit@gmail.com>
2023-05-25 20:16:59 -0400

    refactor: simplify checkbox-react-hook-form-single example

--- apps/www/components/examples/checkbox/react-hook-form-single.tsx
+++ apps/www/components/examples/checkbox/react-hook-form-single.tsx
@@ -7,25 +7,18 @@ import * as z from "zod"
 
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
 import { toast } from "@/components/ui/use-toast"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-} from "@/components/react-hook-form/form"
 
 const FormSchema = z.object({
   mobile: z.boolean().default(false).optional(),
 })
 
 export function CheckboxReactHookFormSingle() {
-  const form = useForm<z.infer<typeof FormSchema>>({
+  const { register, handleSubmit } = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      mobile: true,
+      mobile: false,
     },
   })
 
@@ -41,33 +34,18 @@ export function CheckboxReactHookFormSingle() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="mobile"
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-              <FormControl>
-                <Checkbox
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-              <div className="space-y-1 leading-none">
-                <FormLabel>
-                  Use different settings for my mobile devices
-                </FormLabel>
-                <FormDescription>
-                  You can manage your mobile notifications in the{" "}
-                  <Link href="/examples/forms">mobile settings</Link> page.
-                </FormDescription>
-              </div>
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+      <div className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+        <Checkbox {...register("mobile")} />
+        <div className="space-y-1 leading-none">
+          <Label>Use different settings for my mobile devices</Label>
+          <p className="text-sm text-muted-foreground">
+            You can manage your mobile notifications in the{" "}
+            <Link href="/examples/forms">mobile settings</Link> page.
+          </p>
+        </div>
+      </div>
+      <Button type="submit">Submit</Button>
+    </form>
   )
 }
