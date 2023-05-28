29cd2bfac0b64b0e2a21ee07596a101cb6f37564
Lucas Thevenet Cervantes <lucastcervantes@gmail.com>
2023-05-28 13:38:17 -0300

    fix(progress): allow max value to be different from 100

--- apps/www/components/ui/progress.tsx
+++ apps/www/components/ui/progress.tsx
@@ -8,18 +8,20 @@ import { cn } from "@/lib/utils"
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ className, value = 0, max = 100, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
       "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
       className
     )}
+    value={value}
+    max={max}
     {...props}
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${max - value}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
