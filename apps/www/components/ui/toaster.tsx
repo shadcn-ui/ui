fb0253a0d7c49fc2cf8f4cfdec10313858d6efb8
key7men <key7men@gmail.com>
2023-06-07 15:31:13 +0800

    feat(toast): support position setting
    use 'bottom-left' / 'bottom-right' / 'top-left' / 'top-right' for setting display position of toast
    
    fix #128

--- apps/www/components/ui/toaster.tsx
+++ apps/www/components/ui/toaster.tsx
@@ -10,14 +10,32 @@ import {
 } from "@/components/ui/toast"
 import { useToast } from "@/components/ui/use-toast"
 
-export function Toaster() {
+ const showAt = {
+  "top-left": "sm:top-0 sm:left-0",
+  "top-right": "sm:top-0 sm:right-0",
+  "bottom-left": "sm:bottom-0 sm:left-0",
+  "bottom-right": "sm:bottom-0 sm:right-0"
+}
+
+const slideBy = {
+  "top-left": "data-[state=open]:sm:slide-in-from-top-full data-[state=closed]:slide-out-to-left-full",
+  "top-right": "data-[state=open]:sm:slide-in-from-top-full data-[state=closed]:slide-out-to-right-full",
+  "bottom-left": "data-[state=open]:sm:slide-in-from-bottom-full data-[state=closed]:slide-out-to-left-full",
+  "bottom-right": "data-[state=open]:sm:slide-in-from-bottom-full data-[state=closed]:slide-out-to-right-full"
+}
+
+type Props = {
+  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right"
+}
+
+export function Toaster({ position, ...props }: Props) {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...props} className={position ? slideBy[position] : slideBy["bottom-left"]}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
@@ -29,7 +47,7 @@ export function Toaster() {
           </Toast>
         )
       })}
-      <ToastViewport />
+      <ToastViewport className={position ? showAt[position] : showAt["bottom-left"]} />
     </ToastProvider>
   )
 }
