0f847ace0c73abfc6c50ddf8d7ba2ac07e71578d
bdbch <dominik@bdbch.com>
2023-06-15 00:52:57 +0200

    fix: accordion flex styles breaking on with longer trigger text

--- apps/www/components/ui/accordion.tsx
+++ apps/www/components/ui/accordion.tsx
@@ -28,13 +28,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between gap-4 py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
     >
-      {children}
-      <ChevronDown className="h-4 w-4 transition-transform duration-200" />
+      <span className="flex-1 text-left">{children}</span>
+      <ChevronDown className="flex-0 h-4 w-4 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
