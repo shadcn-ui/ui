b30790fa825f13804cd82c72fff1bf120c1ac376
dan5py <git@dan5py.com>
2023-06-18 22:56:21 +0200

    fix(alert): remove duplicate class (#613)

--- apps/www/components/ui/alert.tsx
+++ apps/www/components/ui/alert.tsx
@@ -10,7 +10,7 @@ const alertVariants = cva(
       variant: {
         default: "bg-background text-foreground",
         destructive:
-          "text-destructive border-destructive/50 dark:border-destructive [&>svg]:text-destructive text-destructive",
+          "text-destructive border-destructive/50 dark:border-destructive [&>svg]:text-destructive",
       },
     },
     defaultVariants: {
