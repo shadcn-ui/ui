97e357b52a784816ef5e9fedaf84103535ba52d8
Pablo Lizardo <pablo.lizardo@blackboard.com>
2023-06-23 12:38:29 -0300

    BadgeProp must extend from HTMLSpanElement instead Div

--- apps/www/registry/default/ui/badge.tsx
+++ apps/www/registry/default/ui/badge.tsx
@@ -24,7 +24,7 @@ const badgeVariants = cva(
 )
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
+  extends React.HTMLAttributes<HTMLSpanElement>,
     VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
