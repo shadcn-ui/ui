c0e5f1d40a552e7631f9b91f342686e8c8b2cc63
Anand Chowdhary <github@anandchowdhary.com>
2023-06-26 17:03:21 +0200

    chore: Add type fix to new-york theme

--- apps/www/registry/new-york/ui/use-toast.ts
+++ apps/www/registry/new-york/ui/use-toast.ts
@@ -143,7 +143,7 @@ type Toast = Omit<ToasterToast, "id">
 function toast({ ...props }: Toast) {
   const id = genId()
 
-  const update = (props: ToasterToast) =>
+  const update = (props: Toast) =>
     dispatch({
       type: "UPDATE_TOAST",
       toast: { ...props, id },
