51312218b1d068b148d3f5dabd631758c79fd1ba
lloydrichards <lloyd.d.richards@gmail.com>
2023-07-11 07:25:19 +0200

    fix(storybook): :bug: get tailwind and nextjs working with storybook

--- apps/www/.storybook/preview.tsx
+++ apps/www/.storybook/preview.tsx
@@ -1,13 +1,35 @@
-// import "tailwindcss/tailwind.css";
-import "../styles/mdx.css"
+import type { Preview } from "@storybook/react"
+import { Inter } from "next/font/google"
+import React from "react"
+
+import { cn } from "../lib/utils"
 import "../styles/globals.css"
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+export const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+export const decorators = [
+  (Story) => (
+    <div className={cn("font-sans", fontSans.variable)}>
+      <Story />
+    </div>
+  ),
+]
+
+const preview: Preview = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
   },
 }
+
+export default preview
