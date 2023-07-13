b76f31874a00f8e1fa3df3bc0985e420cdb3f20e
NGAU Zeonfung <github@ox.ci>
2023-07-13 14:28:37 +0900

    Support for selecting the year and month using the select component.
    ## Support for selecting the year and month using the select component.
    
    Related to:
    
    #320 #546 #509 #880 
    
    ### Usage:
    ```
    <Calendar
        initialFocus
        mode="range" // range | single
        captionLayout="dropdown-buttons" 
        // Optionally, enables the year and month drop-down selector.
        // also: dropdown-buttons | dropdown | buttons
        fromYear={2022} 
        toYear={2023} 
        defaultMonth={date?.from}
        selected={date}
        onSelect={setDate}
        numberOfMonths={2}
        {...props}
    />
    ```
    
    ---
    ZEONFUNG, NGAU
    github@ox.ci #230713 142000

--- apps/www/registry/default/ui/calendar.tsx
+++ apps/www/registry/default/ui/calendar.tsx
@@ -3,9 +3,9 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
-
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/registry/default/ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/registry/default/ui/select"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -14,7 +14,17 @@ function Calendar({
   classNames,
   showOutsideDays = true,
   ...props
-}: CalendarProps) {
+}: CalendarProps & { onChange: React.ChangeEventHandler<HTMLSelectElement> }) {
+
+  const handleCalendarChange = (_value: string, _e: React.ChangeEventHandler<HTMLSelectElement>) => {
+    const _event = {
+      target: {
+        value: _value
+      },
+    } as React.ChangeEvent<HTMLSelectElement>
+    _e(_event);
+  };
+
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
@@ -22,22 +32,24 @@ function Calendar({
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
-        nav: "space-x-1 flex items-center",
+        caption: "flex justify-center pt-1 relative items-center gap-2",
+        caption_label: "flex text-sm font-medium justify-center grow",
+        caption_dropdowns: "flex justify-center gap-1 grow dropdowns",
+        vhidden: "hidden",
+        nav: "flex items-center [&:has([name='previous-month'])]:order-first [&:has([name='next-month'])]:order-last",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+          "h-7 w-7 bg-transparent p-0 text-muted-foreground"
         ),
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
+        nav_button_previous: cn({ 'absolute left-1': props.mode === 'single'}),
+        nav_button_next: cn({ 'absolute right-1' : props.mode === 'single'}),
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
-        head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+        head_cell: "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: "text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
+        day: cn(buttonVariants({ variant: "ghost" }), "h-9 w-9 p-0 font-normal aria-selected:opacity-100"),
           buttonVariants({ variant: "ghost" }),
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
@@ -46,14 +58,32 @@ function Calendar({
         day_today: "bg-accent text-accent-foreground",
         day_outside: "text-muted-foreground opacity-50",
         day_disabled: "text-muted-foreground opacity-50",
-        day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+        day_range_middle: "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        Dropdown: ({ ...props }) => (
+          <Select
+            {...props}
+            onValueChange={(value) => {
+              handleCalendarChange(value, props.onChange)
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder={props.caption} />
+            </SelectTrigger>
+            <SelectContent>
+              {
+                React.Children.map(props.children, (child) => (
+                  <SelectItem value={child.props.value}>{child.props.children}</SelectItem>
+                ))
+              }
+            </SelectContent>
+          </Select>
+        ),
       }}
       {...props}
     />
