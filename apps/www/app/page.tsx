c838e06c78bc278f012a3f18d66c923934522b5b
Man From Earth <manfromearth25@gmail.com>
2023-07-17 21:43:02 +0000

    feat: initial commit

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -26,7 +26,7 @@ export default function IndexPage() {
           🎉 <Separator className="mx-2 h-4" orientation="vertical" />{" "}
           <span className="sm:hidden">Style, a new CLI and more.</span>
           <span className="hidden sm:inline">
-            Introducing Style, a new CLI and more.
+            I am ManFromEarth
           </span>
           <ArrowRightIcon className="ml-1 h-4 w-4" />
         </Link>
