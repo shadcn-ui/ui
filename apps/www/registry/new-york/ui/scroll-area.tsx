87655e582e11a89a520a0c0897e3e7efd50399f6
Man From Earth <manfromearth25@gmail.com>
2023-07-21 13:57:42 +0000

    feat: omg

--- apps/www/registry/new-york/ui/scroll-area.tsx
+++ apps/www/registry/new-york/ui/scroll-area.tsx
@@ -11,7 +11,7 @@ const ScrollArea = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
-    className={cn("relative overflow-hidden", className)}
+    className={cn("relative overflow-hidden ", className)}
     {...props}
   >
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
@@ -31,9 +31,9 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      "flex touch-none select-none transition-colors",
+      "flex touch-none select-none transition-colors ",
       orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
+        "h-full w-2.5 border-l border-l-transparent p-[1px] ",
       orientation === "horizontal" &&
         "h-2.5 border-t border-t-transparent p-[1px]",
       className
