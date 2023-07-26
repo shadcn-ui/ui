795a640da48e6cac4ef56bb596e2efe2c84b9ce6
notjawad <bawskiya@gmail.com>
2023-07-26 16:17:19 -0500

    Add feature: Icon support in inputs

--- apps/www/registry/default/ui/input.tsx
+++ apps/www/registry/default/ui/input.tsx
@@ -13,7 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     const paddingRight = iconPosition === 'right' && icon ? 'pr-10' : '';
 
     const inputClasses = cn(
-      'flex h-10 w-full rounded-md border-0 bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+      'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
       paddingLeft,
       paddingRight,
       className
