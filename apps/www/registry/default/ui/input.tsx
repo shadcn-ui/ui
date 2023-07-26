8e60f00f9fda05742dacccab70fd7bb2bf1538db
notjawad <bawskiya@gmail.com>
2023-07-26 16:07:54 -0500

    Add feature: Icon support in inputs

--- apps/www/registry/default/ui/input.tsx
+++ apps/www/registry/default/ui/input.tsx
@@ -1,25 +1,45 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
+import * as React from 'react';
+import { cn } from '@/lib/utils';
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactNode;
+  iconPosition?: 'left' | 'right';
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+  ({ className, type, icon, iconPosition = 'left', ...props }, ref) => {
+    const paddingLeft = iconPosition === 'left' && icon ? 'pl-10' : '';
+    const paddingRight = iconPosition === 'right' && icon ? 'pr-10' : '';
 
-export { Input }
+    const inputClasses = cn(
+      'flex h-10 w-full rounded-md border-0 bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+      paddingLeft,
+      paddingRight,
+      className
+    );
+
+    return (
+      <div
+        className={`w-full relative ${
+          iconPosition === 'right' ? 'flex-row-reverse' : ''
+        }`}
+      >
+        {icon && (
+          <div
+            className={`absolute inset-y-0 ${
+              iconPosition === 'right' ? 'right-0' : 'left-0'
+            } flex items-center ${iconPosition === 'right' ? 'pr-3' : 'pl-3'}`}
+          >
+            {icon}
+          </div>
+        )}
+        <input type={type} className={inputClasses} ref={ref} {...props} />
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export { Input };
