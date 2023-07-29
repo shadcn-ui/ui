13f7f88238614de4c513b914f9fd9ec90d8c776a
pyloverm <34107909+pyloverm@users.noreply.github.com>
2023-07-29 19:41:24 +0200

    Update sheet.tsx
    added a "center" side option for Sheet Component.

--- apps/www/registry/default/ui/sheet.tsx
+++ apps/www/registry/default/ui/sheet.tsx
@@ -47,6 +47,7 @@ const sheetVariants = cva(
         left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
         right:
           "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+        center: "inset-x-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:max-w-sm data-[state=closed]:duration-0 data-[state=open]:duration-0 ",
       },
     },
     defaultVariants: {
