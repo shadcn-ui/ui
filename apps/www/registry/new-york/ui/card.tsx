a7e570ad43ce359874510bee7a815b41fb4eaebf
Joschua Schneider <schneider@joschua.dev>
2023-08-03 20:58:32 +0200

    fix(new-york/card): improve ref and attributes types consistency

--- apps/www/registry/new-york/ui/card.tsx
+++ apps/www/registry/new-york/ui/card.tsx
@@ -30,7 +30,7 @@ const CardHeader = React.forwardRef<
 CardHeader.displayName = "CardHeader"
 
 const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
+  HTMLHeadingElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
   <h3
