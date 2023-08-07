ace3767d717e3c801db342328e00a6e64a238e78
Jack Chi <905852308@qq.com>
2023-08-07 16:19:12 +0800

    fix(ui): the Overlay mask would collapse in IOS safari devices.
    Issue: the Overlay mask would collapse in IOS safari devices.
    Solution: Add some class names like `left-0 top-0 right-0 bottom-0` to fix it.

--- apps/www/registry/default/ui/sheet.tsx
+++ apps/www/registry/default/ui/sheet.tsx
@@ -27,7 +27,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      "fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed top-0 right-0 bottom-0 left-0 inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props}
