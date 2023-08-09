64fc76eb18b786dbf04ec663c337da2443408201
Valentin Politov <valentin@politov.md>
2023-08-09 12:25:46 +0300

    feat: add collapsible animation

--- apps/www/registry/default/ui/collapsible.tsx
+++ apps/www/registry/default/ui/collapsible.tsx
@@ -1,11 +1,27 @@
 "use client"
 
+import * as React from "react"
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+import { cn } from "@/lib/utils"
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+const CollapsibleContent = React.forwardRef<
+  React.ElementRef<typeof CollapsiblePrimitive.CollapsibleContent>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleContent>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.CollapsibleContent
+    ref={ref}
+    className={cn(
+      "data-[state=closed]:animate-collapse-up data-[state=open]:animate-collapse-down overflow-hidden",
+      className
+    )}
+    {...props}
+  />
+))
+CollapsibleContent.displayName = "CollapsibleContent"
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -66,10 +66,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "collapse-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-collapse-content-height)" },
+        },
+        "collapse-up": {
+          from: {  height: "var(--radix-collapse-content-height)" },
+          to: { height: 0 },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "collapse-down": "collapse-down 0.2s ease-out",
+        "collapse-up": "collapse-up 0.2s ease-out",
       },
     },
   },
