1cb96784f3573d7ee510a01d6762cfb4761f3341
K-Sato1995 <Katsuki_Sato0130@icloud.com>
2023-08-16 20:49:34 +0900

    chore: extract the add command's logic

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -28,6 +28,172 @@ const addOptionsSchema = z.object({
   path: z.string().optional(),
 })
 
+const runAdd = async (
+  components: string[],
+  opts: {
+    yes: boolean
+    overwrite: boolean
+    cwd: string
+    all: boolean
+    path?: string
+  }
+) => {
+  try {
+    const options = addOptionsSchema.parse({
+      components,
+      ...opts,
+    })
+
+    const cwd = path.resolve(options.cwd)
+
+    if (!existsSync(cwd)) {
+      logger.error(`The path ${cwd} does not exist. Please try again.`)
+      process.exit(1)
+    }
+
+    const config = await getConfig(cwd)
+    if (!config) {
+      logger.warn(
+        `Configuration is missing. Please run ${chalk.green(
+          `init`
+        )} to create a components.json file.`
+      )
+      process.exit(1)
+    }
+
+    const registryIndex = await getRegistryIndex()
+
+    let selectedComponents = options.all
+      ? registryIndex.map((entry) => entry.name)
+      : options.components
+    if (!options.components?.length && !options.all) {
+      const { components } = await prompts({
+        type: "multiselect",
+        name: "components",
+        message: "Which components would you like to add?",
+        hint: "Space to select. A to toggle all. Enter to submit.",
+        instructions: false,
+        choices: registryIndex.map((entry) => ({
+          title: entry.name,
+          value: entry.name,
+          selected: options.all
+            ? true
+            : options.components?.includes(entry.name),
+        })),
+      })
+      selectedComponents = components
+    }
+
+    if (!selectedComponents?.length) {
+      logger.warn("No components selected. Exiting.")
+      process.exit(0)
+    }
+
+    const tree = await resolveTree(registryIndex, selectedComponents)
+    const payload = await fetchTree(config.style, tree)
+    const baseColor = await getRegistryBaseColor(config.tailwind.baseColor)
+
+    if (!payload.length) {
+      logger.warn("Selected components not found. Exiting.")
+      process.exit(0)
+    }
+
+    if (!options.yes) {
+      const { proceed } = await prompts({
+        type: "confirm",
+        name: "proceed",
+        message: `Ready to install components and dependencies. Proceed?`,
+        initial: true,
+      })
+
+      if (!proceed) {
+        process.exit(0)
+      }
+    }
+
+    const spinner = ora(`Installing components...`).start()
+    for (const item of payload) {
+      spinner.text = `Installing ${item.name}...`
+      const targetDir = await getItemTargetPath(
+        config,
+        item,
+        options.path ? path.resolve(cwd, options.path) : undefined
+      )
+
+      if (!targetDir) {
+        continue
+      }
+
+      if (!existsSync(targetDir)) {
+        await fs.mkdir(targetDir, { recursive: true })
+      }
+
+      const existingComponent = item.files.filter((file) =>
+        existsSync(path.resolve(targetDir, file.name))
+      )
+
+      if (existingComponent.length && !options.overwrite) {
+        if (selectedComponents.includes(item.name)) {
+          spinner.stop()
+          const { overwrite } = await prompts({
+            type: "confirm",
+            name: "overwrite",
+            message: `Component ${item.name} already exists. Would you like to overwrite?`,
+            initial: false,
+          })
+
+          if (!overwrite) {
+            logger.info(
+              `Skipped ${item.name}. To overwrite, run with the ${chalk.green(
+                "--overwrite"
+              )} flag.`
+            )
+            continue
+          }
+
+          spinner.start(`Installing ${item.name}...`)
+        } else {
+          continue
+        }
+      }
+
+      for (const file of item.files) {
+        let filePath = path.resolve(targetDir, file.name)
+
+        // Run transformers.
+        const content = await transform({
+          filename: file.name,
+          raw: file.content,
+          config,
+          baseColor,
+        })
+
+        if (!config.tsx) {
+          filePath = filePath.replace(/\.tsx$/, ".jsx")
+          filePath = filePath.replace(/\.ts$/, ".js")
+        }
+
+        await fs.writeFile(filePath, content)
+      }
+
+      // Install dependencies.
+      if (item.dependencies?.length) {
+        const packageManager = await getPackageManager(cwd)
+        await execa(
+          packageManager,
+          [packageManager === "npm" ? "install" : "add", ...item.dependencies],
+          {
+            cwd,
+          }
+        )
+      }
+    }
+    spinner.succeed(`Done.`)
+  } catch (error) {
+    handleError(error)
+  }
+}
+
 export const add = new Command()
   .name("add")
   .description("add a component to your project")
@@ -41,162 +207,4 @@ export const add = new Command()
   )
   .option("-a, --all", "add all available components", false)
   .option("-p, --path <path>", "the path to add the component to.")
-  .action(async (components, opts) => {
-    try {
-      const options = addOptionsSchema.parse({
-        components,
-        ...opts,
-      })
-
-      const cwd = path.resolve(options.cwd)
-
-      if (!existsSync(cwd)) {
-        logger.error(`The path ${cwd} does not exist. Please try again.`)
-        process.exit(1)
-      }
-
-      const config = await getConfig(cwd)
-      if (!config) {
-        logger.warn(
-          `Configuration is missing. Please run ${chalk.green(
-            `init`
-          )} to create a components.json file.`
-        )
-        process.exit(1)
-      }
-
-      const registryIndex = await getRegistryIndex()
-
-      let selectedComponents = options.all
-        ? registryIndex.map((entry) => entry.name)
-        : options.components
-      if (!options.components?.length && !options.all) {
-        const { components } = await prompts({
-          type: "multiselect",
-          name: "components",
-          message: "Which components would you like to add?",
-          hint: "Space to select. A to toggle all. Enter to submit.",
-          instructions: false,
-          choices: registryIndex.map((entry) => ({
-            title: entry.name,
-            value: entry.name,
-            selected: options.all
-              ? true
-              : options.components?.includes(entry.name),
-          })),
-        })
-        selectedComponents = components
-      }
-
-      if (!selectedComponents?.length) {
-        logger.warn("No components selected. Exiting.")
-        process.exit(0)
-      }
-
-      const tree = await resolveTree(registryIndex, selectedComponents)
-      const payload = await fetchTree(config.style, tree)
-      const baseColor = await getRegistryBaseColor(config.tailwind.baseColor)
-
-      if (!payload.length) {
-        logger.warn("Selected components not found. Exiting.")
-        process.exit(0)
-      }
-
-      if (!options.yes) {
-        const { proceed } = await prompts({
-          type: "confirm",
-          name: "proceed",
-          message: `Ready to install components and dependencies. Proceed?`,
-          initial: true,
-        })
-
-        if (!proceed) {
-          process.exit(0)
-        }
-      }
-
-      const spinner = ora(`Installing components...`).start()
-      for (const item of payload) {
-        spinner.text = `Installing ${item.name}...`
-        const targetDir = await getItemTargetPath(
-          config,
-          item,
-          options.path ? path.resolve(cwd, options.path) : undefined
-        )
-
-        if (!targetDir) {
-          continue
-        }
-
-        if (!existsSync(targetDir)) {
-          await fs.mkdir(targetDir, { recursive: true })
-        }
-
-        const existingComponent = item.files.filter((file) =>
-          existsSync(path.resolve(targetDir, file.name))
-        )
-
-        if (existingComponent.length && !options.overwrite) {
-          if (selectedComponents.includes(item.name)) {
-            spinner.stop()
-            const { overwrite } = await prompts({
-              type: "confirm",
-              name: "overwrite",
-              message: `Component ${item.name} already exists. Would you like to overwrite?`,
-              initial: false,
-            })
-
-            if (!overwrite) {
-              logger.info(
-                `Skipped ${item.name}. To overwrite, run with the ${chalk.green(
-                  "--overwrite"
-                )} flag.`
-              )
-              continue
-            }
-
-            spinner.start(`Installing ${item.name}...`)
-          } else {
-            continue
-          }
-        }
-
-        for (const file of item.files) {
-          let filePath = path.resolve(targetDir, file.name)
-
-          // Run transformers.
-          const content = await transform({
-            filename: file.name,
-            raw: file.content,
-            config,
-            baseColor,
-          })
-
-          if (!config.tsx) {
-            filePath = filePath.replace(/\.tsx$/, ".jsx")
-            filePath = filePath.replace(/\.ts$/, ".js")
-          }
-
-          await fs.writeFile(filePath, content)
-        }
-
-        // Install dependencies.
-        if (item.dependencies?.length) {
-          const packageManager = await getPackageManager(cwd)
-          await execa(
-            packageManager,
-            [
-              packageManager === "npm" ? "install" : "add",
-              ...item.dependencies,
-            ],
-            {
-              cwd,
-            }
-          )
-        }
-      }
-      spinner.succeed(`Done.`)
-    } catch (error) {
-      handleError(error)
-    }
-  })
+  .action(runAdd)
