1675b7caef8d0682526a35759d8442345a3571c6
Diego Franchina <diegofranchina@gmail.com>
2023-08-25 05:24:04 +0200

    fix(scroll-area): missing key prop error (#1295)
    Co-authored-by: shadcn <m@shadcn.com>

--- apps/www/registry/new-york/example/scroll-area-demo.tsx
+++ apps/www/registry/new-york/example/scroll-area-demo.tsx
@@ -14,7 +14,9 @@ export default function ScrollAreaDemo() {
         <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
         {tags.map((tag) => (
           <>
-            <div className="text-sm">{tag}</div>
+            <div key={tag} className="text-sm">
+              {tag}
+            </div>
             <Separator className="my-2" />
           </>
         ))}
