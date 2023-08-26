34f50aa9da1f15051fc540824ec6818cb90c7b85
Kevin Pham <k_pham@ucsb.edu>
2023-08-26 00:39:54 -0700

    fix: stop unnecessary rerenders from useFormField

--- apps/www/registry/default/ui/form.tsx
+++ apps/www/registry/default/ui/form.tsx
@@ -42,9 +42,9 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-  const { getFieldState, formState } = useFormContext()
+  const { formState } = useFormContext()
 
-  const fieldState = getFieldState(fieldContext.name, formState)
+  const error = formState.errors[fieldContext.name]
 
   if (!fieldContext) {
     throw new Error("useFormField should be used within <FormField>")
@@ -58,7 +58,7 @@ const useFormField = () => {
     formItemId: `${id}-form-item`,
     formDescriptionId: `${id}-form-item-description`,
     formMessageId: `${id}-form-item-message`,
-    ...fieldState,
+    error,
   }
 }
 
