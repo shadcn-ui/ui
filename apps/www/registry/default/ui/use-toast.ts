860dd4dd80c7433d3bd00b221f24d5601667c3fa
fahmij8 <fahmijabbar12@gmail.com>
2023-08-27 22:22:51 +0700

    fix: type error toast title

--- apps/www/registry/default/ui/use-toast.ts
+++ apps/www/registry/default/ui/use-toast.ts
@@ -9,7 +9,7 @@ import type {
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
 
-type ToasterToast = ToastProps & {
+type ToasterToast = Omit<ToastProps, 'id' | 'title' | 'description' | 'action'> & {
   id: string
   title?: React.ReactNode
   description?: React.ReactNode
