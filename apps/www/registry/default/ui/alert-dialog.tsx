46297e30aed5d83b3a7d3f216063faf72f2815e3
Ahmad Awais <me@AhmadAwais.com>
2023-08-31 21:37:24 +0500

    refactor(components): alert dialog variants

--- apps/www/registry/default/ui/alert-dialog.tsx
+++ apps/www/registry/default/ui/alert-dialog.tsx
@@ -5,6 +5,7 @@ import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/registry/default/ui/button"
+import { VariantProps } from 'class-variance-authority';
 
 const AlertDialog = AlertDialogPrimitive.Root
 
@@ -106,11 +107,13 @@ AlertDialogDescription.displayName =
 
 const AlertDialogAction = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Action>,
-  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
+  { variant?: VariantProps<typeof buttonVariants>['variant'] } & React.ComponentPropsWithoutRef<
+		typeof AlertDialogPrimitive.Action
+	>
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Action
     ref={ref}
-    className={cn(buttonVariants(), className)}
+    className={cn(buttonVariants(), props.variant && buttonVariants({ variant: props.variant }), className)}
     {...props}
   />
 ))
