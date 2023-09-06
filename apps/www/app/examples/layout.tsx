66cb789a2856b5c6d03ec80bf2fb0b251f02a6a3
MohamedGado <boogado@yahoo.com>
2023-09-06 12:02:37 +0300

    fix(examples): removed duplicated code in examples layout & dashboard page into 1 reusable component

--- apps/www/app/examples/layout.tsx
+++ apps/www/app/examples/layout.tsx
@@ -1,16 +1,7 @@
 import { Metadata } from "next"
-import Link from "next/link"
-import { ArrowRightIcon } from "@radix-ui/react-icons"
 
-import { cn } from "@/lib/utils"
+import { HeaderIntro } from "@/components/dashboard-examples-header"
 import { ExamplesNav } from "@/components/examples-nav"
-import {
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from "@/components/page-header"
-import { buttonVariants } from "@/registry/new-york/ui/button"
-import { Separator } from "@/registry/new-york/ui/separator"
 
 export const metadata: Metadata = {
   title: "Examples",
@@ -25,44 +16,7 @@ export default function ExamplesLayout({ children }: ExamplesLayoutProps) {
   return (
     <>
       <div className="container relative">
-        <PageHeader className="page-header pb-8">
-          <Link
-            href="/docs/changelog"
-            className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
-          >
-            🎉 <Separator className="mx-2 h-4" orientation="vertical" />{" "}
-            <span className="sm:hidden">Style, a new CLI and more.</span>
-            <span className="hidden sm:inline">
-              Introducing Style, a new CLI and more.
-            </span>
-            <ArrowRightIcon className="ml-1 h-4 w-4" />
-          </Link>
-          <PageHeaderHeading className="hidden md:block">
-            Check out some examples.
-          </PageHeaderHeading>
-          <PageHeaderHeading className="md:hidden">Examples</PageHeaderHeading>
-          <PageHeaderDescription>
-            Dashboard, cards, authentication. Some examples built using the
-            components. Use this as a guide to build your own.
-          </PageHeaderDescription>
-          <section className="flex w-full items-center space-x-4 pb-8 pt-4 md:pb-10">
-            <Link
-              href="/docs"
-              className={cn(buttonVariants(), "rounded-[6px]")}
-            >
-              Get Started
-            </Link>
-            <Link
-              href="/components"
-              className={cn(
-                buttonVariants({ variant: "outline" }),
-                "rounded-[6px]"
-              )}
-            >
-              Components
-            </Link>
-          </section>
-        </PageHeader>
+        <HeaderIntro className="page-header" isExamplesPage />
         <section>
           <ExamplesNav />
           <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow">
