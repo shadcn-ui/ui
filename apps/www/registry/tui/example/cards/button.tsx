f130e78a152102933153d8a1201031ea4fd51e19
kaushal patel <kaushal.patel@cakewalk.in>
2023-09-07 10:55:12 +0400

    button component

--- apps/www/registry/tui/example/cards/button.tsx
+++ apps/www/registry/tui/example/cards/button.tsx
@@ -1,74 +1,80 @@
 "use client"
 
 import { addDays } from "date-fns"
-
+import { Check, Plus, Send } from "lucide-react"
 import { Card, CardContent } from "@/registry/tui/ui/card"
 import { } from "../../ui/badge"
+import ButtonAsChild from "../button-as-child"
+import ButtonDemo from "../button-demo"
+import ButtonDestructive from "../button-destructive"
+import ButtonGhost from "../button-ghost"
+import ButtonIcon from "../button-icon"
+import ButtonLink from "../button-link"
+import ButtonLoading from "../button-loading"
+import ButtonOutline from "../button-outline"
+import ButtonSecondary from "../button-secondary"
+import ButtonWithIcon from "../button-with-icon"
 import { Button } from "../../ui/button"
-import { CheckCircleIcon } from "lucide-react"
-import { Icons } from "@/components/icons"
 
 const start = new Date(2023, 5, 5)
 
 export function CardsButton() {
   return (
-    <Card className="">
+    <Card>
       <CardContent className="space-y-1 p-2">
-        <div className="truncate text-sm font-medium leading-7 text-slate-900">Primary buttons</div>
-        <div className="flex items-center space-x-2">
-          <Button type="button" size="xs">Button text</Button>
-          <Button type="button" size="sm">Button text</Button>
-          <Button type="button" size="md">Button text</Button>
-          <Button type="button" size="lg">Button text</Button>
-          <Button type="button" size="xl">Button text</Button>
+        <div className="space-x-2 space-y-2">
+          <Button size="xs">Button Text</Button>
+          <Button size="sm">Button Text</Button>
+          <Button>Button Text</Button>
+          <Button size="lg">Button Text</Button>
+          <Button size="xl">Button Text</Button>
         </div>
-        <div className="truncate text-sm font-medium leading-7 text-slate-900">Secondary buttons</div>
-        <div className="flex items-center space-x-2">
-          <Button variant="outline" type="button" size="xs">Button text</Button>
-          <Button variant="outline" type="button" size="sm">Button text</Button>
-          <Button variant="outline" type="button" size="md">Button text</Button>
-          <Button variant="outline" type="button" size="lg">Button text</Button>
-          <Button variant="outline" type="button" size="xl">Button text</Button>
+        <div className="space-x-2 space-y-2">
+          <Button variant="outline" size="xs">Button Text</Button>
+          <Button variant="outline" size="sm">Button Text</Button>
+          <Button variant="outline">Button Text</Button>
+          <Button variant="outline" size="lg">Button Text</Button>
+          <Button variant="outline" size="xl">Button Text</Button>
         </div>
-        <div className="truncate text-sm font-medium leading-7 text-slate-900">Soft buttons</div>
-        <div className="flex items-center space-x-2">
-          <Button variant="ghost" type="button" size="xs" className="bg-indigo-50 text-indigo-600">Button text</Button>
-          <Button variant="ghost" type="button" size="sm" className="bg-indigo-50 text-indigo-600">Button text</Button>
-          <Button variant="ghost" type="button" size="md" className="bg-indigo-50 text-indigo-600">Button text</Button>
-          <Button variant="ghost" type="button" size="lg" className="bg-indigo-50 text-indigo-600">Button text</Button>
-          <Button variant="ghost" type="button" size="xl" className="bg-indigo-50 text-indigo-600">Button text</Button>
+        <div className="space-x-2 space-y-2">
+          <Button variant="soft" size="xs">Button Text</Button>
+          <Button variant="soft" size="sm">Button Text</Button>
+          <Button variant="soft">Button Text</Button>
+          <Button variant="soft" size="lg">Button Text</Button>
+          <Button variant="soft" size="xl">Button Text</Button>
         </div>
-        <div className="truncate text-sm font-medium leading-7 text-slate-900">Buttons with leading icon</div>
-        <div className="flex items-center space-x-2">
-          <Button type="button" size="xs" ><Icons.gitHub className="-ml-0.5 h-4 w-4" />Button text</Button>
-          <Button type="button" size="sm" ><Icons.gitHub className="-ml-0.5 h-4 w-4" />Button text</Button>
-          <Button type="button" size="md" ><Icons.gitHub className="-ml-0.5 h-4 w-4" />Button text</Button>
-          <Button type="button" size="lg" ><Icons.gitHub className="-ml-0.5 h-4 w-4" />Button text</Button>
-          <Button type="button" size="xl" ><Icons.gitHub className="-ml-0.5 h-4 w-4" />Button text</Button>
+        <div className="space-x-2 space-y-2">
+          <Button rounded="full" size="xs">Button Text</Button>
+          <Button rounded="full" size="sm">Button Text</Button>
+          <Button rounded="full">Button Text</Button>
+          <Button rounded="full" size="lg">Button Text</Button>
+          <Button rounded="full" size="xl">Button Text</Button>
         </div>
-        <div className="truncate text-sm font-medium leading-7 text-slate-900">Buttons with leading icon</div>
-        <div className="flex items-center space-x-2">
-          <Button type="button" size="xs" className="rounded-full">Button text</Button>
-          <Button type="button" size="sm" className="rounded-full">Button text</Button>
-          <Button type="button" size="md" className="rounded-full">Button text</Button>
-          <Button type="button" size="lg" className="rounded-full">Button text</Button>
-          <Button type="button" size="xl" className="rounded-full">Button text</Button>
+        <div className="space-x-2 space-y-2">
+          <Button rounded="full" variant="outline" size="xs">Button Text</Button>
+          <Button rounded="full" variant="outline" size="sm">Button Text</Button>
+          <Button rounded="full" variant="outline">Button Text</Button>
+          <Button rounded="full" variant="outline" size="lg">Button Text</Button>
+          <Button rounded="full" variant="outline" size="xl">Button Text</Button>
         </div>
-        <div className="truncate text-sm font-medium leading-7 text-slate-900">Buttons with leading icon</div>
-        <div className="flex items-center space-x-2">
-          <Button variant="outline" type="button" size="xs" className="rounded-full">Button text</Button>
-          <Button variant="outline" type="button" size="sm" className="rounded-full">Button text</Button>
-          <Button variant="outline" type="button" size="md" className="rounded-full">Button text</Button>
-          <Button variant="outline" type="button" size="lg" className="rounded-full">Button text</Button>
-          <Button variant="outline" type="button" size="xl" className="rounded-full">Button text</Button>
+        <div className="space-x-2 space-y-2">
+          <Button icon={Check} size="xs">Button Text</Button>
+          <Button icon={Check} size="sm">Button Text</Button>
+          <Button icon={Check}>Button Text</Button>
+          <Button icon={Check} size="lg">Button Text</Button>
+          <Button icon={Check} size="xl">Button Text</Button>
         </div>
-        <div className="truncate text-sm font-medium leading-7 text-slate-900">Buttons with leading icon</div>
-        <div className="flex items-center space-x-2">
-          <Button variant="icon" type="button" size="xs" className="rounded-full"><Icons.google className="h-5 w-4" /></Button>
-          <Button variant="icon" type="button" size="sm" className="rounded-full"><Icons.google className="h-5 w-4" /></Button>
-          <Button variant="icon" type="button" size="md" className="rounded-full"><Icons.google className="h-5 w-4" /></Button>
-          <Button variant="icon" type="button" size="lg" className="rounded-full"><Icons.google className="h-5 w-4" /></Button>
-          <Button variant="icon" type="button" size="xl" className="rounded-full"><Icons.google className="h-5 w-4" /></Button>
+        <div className="space-x-2 space-y-2">
+          <ButtonAsChild />
+          <ButtonDemo />
+          <ButtonDestructive />
+          <ButtonGhost />
+          <ButtonIcon />
+          <ButtonLink />
+          <ButtonLoading />
+          <ButtonOutline />
+          <ButtonSecondary />
+          <ButtonWithIcon />
         </div>
       </CardContent>
     </Card>
--- apps/www/registry/tui/ui/button.tsx
+++ apps/www/registry/tui/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-x-1.5 rounded-md text-sm font-medium shadow-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center text-sm font-medium shadow-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -14,23 +14,34 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        soft:
+          "text-black-foreground hover:bg-indigo/90 bg-indigo-50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        icon: "rounded-full bg-primary p-1 text-primary-foreground hover:bg-primary/90",
       },
       size: {
         xs: "px-2 py-1 text-xs",
         sm: "px-2 py-1 text-sm",
-        md: "px-2.5 py-1.5",
+        default: "px-2.5 py-1.5 text-sm",
         lg: "px-3 py-2 text-sm",
-        xl: "px-3.5 py-2.5",
+        xl: "px-3.5 py-2.5 text-sm",
+        icon: "h-10 w-10",
       },
+      rounded: {
+        sm: "rounded-sm",
+        md: "rounded-md",
+        lg: "rounded-lg",
+        xl: "rounded-xl",
+        none: "rounded-none",
+        full: "rounded-full",
+      }
     },
     defaultVariants: {
       variant: "default",
-      size: "md",
+      size: "default",
+      rounded: "md"
     },
   }
 )
@@ -39,23 +50,19 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
   VariantProps<typeof buttonVariants> {
   asChild?: boolean;
-  prefixIcon?: any;
-  suffixIcon?: any;
+  icon?: any
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, className, variant, size, asChild = false, prefixIcon, suffixIcon, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, icon, rounded, ...props }, ref) => {
+    console.log(typeof icon,'#######')
     const Comp = asChild ? Slot : "button"
     return (
-      <button
-        className={cn(buttonVariants({ variant, size, className }))}
+      <Comp
+        className={cn(buttonVariants({ variant, size, className, rounded }))}
         ref={ref}
         {...props}
-      >
-        {prefixIcon && prefixIcon()}
-        {children}
-        {suffixIcon && suffixIcon()}
-      </button>
+      />
     )
   }
 )
