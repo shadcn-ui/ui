14418b52b87cb3f4b1d826b46350bf4be41b7381
peeranat-dan <ppeeranat.d@gmail.com>
2023-09-10 10:15:08 +0700

    style: add import type for Command component

--- apps/www/registry/new-york/ui/command.tsx
+++ apps/www/registry/new-york/ui/command.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { DialogProps } from "@radix-ui/react-dialog"
+import { type DialogProps } from "@radix-ui/react-dialog"
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
 import { Command as CommandPrimitive } from "cmdk"
 
