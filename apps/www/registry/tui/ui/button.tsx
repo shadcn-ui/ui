2ff7eb7905832cf205362fe19d8037ff1d85657a
kaushal patel <kaushal.patel@cakewalk.in>
2023-09-11 17:37:38 +0400

    common font awesome function

--- apps/www/registry/tui/ui/button.tsx
+++ apps/www/registry/tui/ui/button.tsx
@@ -4,16 +4,11 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 // TODO: pnpm i -w [the following libs]
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconName, IconStyle, IconDefinition, findIconDefinition, library, IconPrefix, IconPack, } from "@fortawesome/fontawesome-svg-core";
-
-import { fas } from '@fortawesome/pro-solid-svg-icons';
-import { fal } from '@fortawesome/pro-light-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { fad } from '@fortawesome/pro-duotone-svg-icons';
-import { far } from '@fortawesome/pro-regular-svg-icons';
-import { fat } from '@fortawesome/pro-thin-svg-icons';
-library.add(fas, fal, fab, fad, far, fat)
+import {
+  IconName,
+  IconStyle,
+} from "@fortawesome/fontawesome-svg-core"
+import { fontAwesomeIcon } from "./helper/fontAwesome";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -53,26 +48,8 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, icon, iconStyle = "solid", ...props }, ref) => {
+  ({ className, variant, size, asChild = false, icon, iconStyle, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    let renderIcon: React.JSX.Element | null = null;
-    if (icon) {
-      const perfixList: { name: IconStyle, value: IconPrefix }[] = [
-        { name: "solid", value: "fas" },
-        { name: "regular", value: "far" },
-        { name: "light", value: "fal" },
-        { name: "duotone", value: "fad" },
-        { name: "brands", value: "fab" },
-        { name: "thin", value: "fat" },
-      ]
-      const iconPrefix: IconPrefix | undefined = perfixList.find((obj) => obj.name === iconStyle)?.value || "fas";
-      renderIcon = icon ? (
-        <FontAwesomeIcon icon={[iconPrefix, icon]} />
-      ) : null;
-    }
-
-
-
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
@@ -81,7 +58,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       >
         <div className="flex items-center">
           <div className="mr-2">
-            {renderIcon}
+            {fontAwesomeIcon(icon, iconStyle)}
           </div>
           {props.children}
         </div>
