f0b483e8a3ba21ad267efa8be6ecaa66ddf8cf27
kaushal patel <kaushal.patel@cakewalk.in>
2023-09-12 15:33:14 +0400

    optimized component

--- apps/www/registry/tui/example/cards/avatar.tsx
+++ apps/www/registry/tui/example/cards/avatar.tsx
@@ -9,52 +9,32 @@ export function CardsAvatar() {
     <Card className="">
       <CardContent className="space-y-1 p-2">
         <div className="flex items-center space-x-2">
-          <Avatar size="xs" src="https://github.com/shadcn.png" alt="@shadcn">
-          </Avatar>
-          <Avatar size="sm" src="https://github.com/shadcn.png" alt="@shadcn">
-          </Avatar>
-          <Avatar size="md" src="https://github.com/shadcn.png" alt="@shadcn">
-          </Avatar>
-          <Avatar size="lg" src="https://github.com/shadcn.png" alt="@shadcn">
-          </Avatar>
-          <Avatar size="xl" src="https://github.com/shadcn.png" alt="@shadcn">
-          </Avatar>
+          <Avatar size="xs" src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar size="sm" src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar size="md" src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar size="lg" src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar size="xl" src="https://github.com/shadcn.png" alt="@shadcn" />
         </div>
         <div className="flex items-center space-x-2">
-          <Avatar size="xs" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded">
-          </Avatar>
-          <Avatar size="sm" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded">
-          </Avatar>
-          <Avatar size="md" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded">
-          </Avatar>
-          <Avatar size="lg" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded">
-          </Avatar>
-          <Avatar size="xl" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded">
-          </Avatar>
+          <Avatar size="xs" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded" />
+          <Avatar size="sm" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded" />
+          <Avatar size="md" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded" />
+          <Avatar size="lg" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded" />
+          <Avatar size="xl" src="https://github.com/shadcn.png" alt="@shadcn" variant="rounded" />
         </div>
         <div className="flex items-center space-x-2">
-          <Avatar variant="dotCircular" color="gray" size="xs" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
-          <Avatar variant="dotCircular" color="red" size="sm" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
-          <Avatar variant="dotCircular" color="green" size="md" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
-          <Avatar variant="dotCircular" color="blue" size="lg" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
-          <Avatar variant="dotCircular" color="yellow" size="xl" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
+          <Avatar variant="dotCircular" color="gray" size="xs" src="https://github.com/shadcn.png" alt="@shadcn"/>
+          <Avatar variant="dotCircular" color="red" size="sm" src="https://github.com/shadcn.png" alt="@shadcn"/>
+          <Avatar variant="dotCircular" color="green" size="md" src="https://github.com/shadcn.png" alt="@shadcn"/>
+          <Avatar variant="dotCircular" color="blue" size="lg" src="https://github.com/shadcn.png" alt="@shadcn"/>
+          <Avatar variant="dotCircular" color="yellow" size="xl" src="https://github.com/shadcn.png" alt="@shadcn"/>
         </div>
         <div className="flex items-center space-x-2">
-          <Avatar variant="dotRounded" color="gray" size="xs" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
-          <Avatar variant="dotRounded" color="red" size="sm" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
-          <Avatar variant="dotRounded" color="green" size="md" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
-          <Avatar variant="dotRounded" color="blue" size="lg" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
-          <Avatar variant="dotRounded" color="yellow" size="xl" src="https://github.com/shadcn.png" alt="@shadcn" >
-          </Avatar>
+          <Avatar variant="dotRounded" color="gray" size="xs" src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar variant="dotRounded" color="red" size="sm" src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar variant="dotRounded" color="green" size="md" src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar variant="dotRounded" color="blue" size="lg" src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar variant="dotRounded" color="yellow" size="xl" src="https://github.com/shadcn.png" alt="@shadcn" />
         </div>
         <div className="flex items-center space-x-2">
           <Avatar size="xs" className="bg-red-500">
