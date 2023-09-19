f234aaf0376568f2cd93b8b7de0e4c56d4d9994e
Raghav Sobti <raghavsobti@cakewalk.in>
2023-09-19 11:02:19 +0400

    Organized imports

--- apps/www/registry/tui/ui/checkbox.tsx
+++ apps/www/registry/tui/ui/checkbox.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { Check } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Label } from "./label"
