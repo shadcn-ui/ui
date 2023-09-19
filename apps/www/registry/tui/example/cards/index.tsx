406f4fda93b780628deb14242d3e4320d71829fc
Raghav Sobti <raghavsobti@cakewalk.in>
2023-09-19 23:20:55 +0400

    Update index.tsx

--- apps/www/registry/tui/example/cards/index.tsx
+++ apps/www/registry/tui/example/cards/index.tsx
@@ -24,32 +24,15 @@ import SwitchCard from "./switchCard"
 
 export default function CardsDemo() {
   return (
-    <div className="md:grids-col-2 grid md:gap-4 lg:grid-cols-10 xl:grid-cols-11 xl:gap-4">
-      <div className="col-span-12 space-y-4">
-        <div className="grid grid-cols-2 gap-4">
-          <DropdownMenuCard />
-        </div>
-        <div className="grid grid-cols-2 gap-4">
-          <CardsBadge />
-          <CardsButton />
-        </div>
-      </div>
-      <div className="col-span-12 space-y-4">
-        <div className="grid grid-cols-2 gap-4">
-          <CardsAvatar />
-        </div>
-      </div>
-      <div className="space-y-4 lg:col-span-4 xl:col-span-6 xl:space-y-4">
-        <CardsComboBox />
-        <SelectMenu />
-        <CardsStats />
-        <div className="grid gap-1 sm:grid-cols-[280px_1fr] md:hidden">
-          <CardsCalendar />
-          <div className="pt-3 sm:pl-2 sm:pt-0 xl:pl-4">
-            <CardsActivityGoal />
+    <>
+      <div className="md:grids-col-2 grid md:gap-4 lg:grid-cols-10 xl:grid-cols-11 xl:gap-4">
+        <div className="col-span-12 space-y-4">
+          <div className="grid grid-cols-2 gap-4">
+            <DropdownMenuCard />
           </div>
-          <div className="pt-3 sm:col-span-2 xl:pt-4">
-            <CardsMetric />
+          <div className="grid grid-cols-2 gap-4">
+            <CardsBadge />
+            <CardsButton />
           </div>
         </div>
         <div className="col-span-12 space-y-4">
@@ -58,6 +41,8 @@ export default function CardsDemo() {
           </div>
         </div>
         <div className="space-y-4 lg:col-span-4 xl:col-span-6 xl:space-y-4">
+          <CardsComboBox />
+          <SelectMenu />
           <CardsStats />
           <div className="grid gap-1 sm:grid-cols-[280px_1fr] md:hidden">
             <CardsCalendar />
@@ -68,37 +53,54 @@ export default function CardsDemo() {
               <CardsMetric />
             </div>
           </div>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
-            <div className="space-y-4 xl:space-y-4">
-              <CardsTeamMembers />
-              <CardsCookieSettings />
-              <CardsPaymentMethod />
+          <div className="col-span-12 space-y-4">
+            <div className="grid grid-cols-2 gap-4">
+              <CardsAvatar />
             </div>
-            <div className="space-y-4 xl:space-y-4">
-              <CardsChat />
-              <CardsCreateAccount />
-              <div className="hidden xl:block">
-                <CardsReportIssue />
+          </div>
+          <div className="space-y-4 lg:col-span-4 xl:col-span-6 xl:space-y-4">
+            <CardsStats />
+            <div className="grid gap-1 sm:grid-cols-[280px_1fr] md:hidden">
+              <CardsCalendar />
+              <div className="pt-3 sm:pl-2 sm:pt-0 xl:pl-4">
+                <CardsActivityGoal />
+              </div>
+              <div className="pt-3 sm:col-span-2 xl:pt-4">
+                <CardsMetric />
+              </div>
+            </div>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
+              <div className="space-y-4 xl:space-y-4">
+                <CardsTeamMembers />
+                <CardsCookieSettings />
+                <CardsPaymentMethod />
+              </div>
+              <div className="space-y-4 xl:space-y-4">
+                <CardsChat />
+                <CardsCreateAccount />
+                <div className="hidden xl:block">
+                  <CardsReportIssue />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="space-y-4 lg:col-span-6 xl:col-span-5 xl:space-y-4">
-          <div className="hidden gap-1 sm:grid-cols-[280px_1fr] md:grid">
-            <CardsCalendar />
-            <div className="pt-3 sm:pl-2 sm:pt-0 xl:pl-3">
-              <CardsActivityGoal />
+          <div className="space-y-4 lg:col-span-6 xl:col-span-5 xl:space-y-4">
+            <div className="hidden gap-1 sm:grid-cols-[280px_1fr] md:grid">
+              <CardsCalendar />
+              <div className="pt-3 sm:pl-2 sm:pt-0 xl:pl-3">
+                <CardsActivityGoal />
+              </div>
+              <div className="pt-3 sm:col-span-2 xl:pt-3">
+                <CardsMetric />
+              </div>
             </div>
-            <div className="pt-3 sm:col-span-2 xl:pt-3">
-              <CardsMetric />
+            <div className="hidden md:block">
+              <CardsDataTable />
             </div>
-          </div>
-          <div className="hidden md:block">
-            <CardsDataTable />
-          </div>
-          <CardsShare />
-          <div className="xl:hidden">
-            <CardsReportIssue />
+            <CardsShare />
+            <div className="xl:hidden">
+              <CardsReportIssue />
+            </div>
           </div>
         </div>
       </div>
@@ -119,6 +121,6 @@ export default function CardsDemo() {
           <DialogCard />
         </div>
       </div>
-    </div>
+    </>
   )
 }
