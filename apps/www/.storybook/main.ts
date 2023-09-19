ddf9db851c86496fb4372e331c0683650e3a5329
lloydrichards <lloyd.d.richards@gmail.com>
2023-09-19 23:07:21 +0200

    build(storybook): :arrow_up: upgrade to storybook v7.4.2

--- apps/www/.storybook/main.ts
+++ apps/www/.storybook/main.ts
@@ -1,15 +1,17 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../registry/stories/**/*.mdx", "../registry/stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-styling",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-styling"),
+    getAbsolutePath("@storybook/addon-mdx-gfm")
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: getAbsolutePath("@storybook/nextjs"),
     options: {},
   },
   docs: {
@@ -17,3 +19,7 @@ const config: StorybookConfig = {
   },
 }
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
