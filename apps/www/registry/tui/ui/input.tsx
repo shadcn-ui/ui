e1bb9ce7ae5398593a6cdf63eb88402ae3c20d90
giradkarParag <129063759+Parag27-cakewalk@users.noreply.github.com>
2023-09-25 15:59:31 +0530

    updated the alignment of Icon
    https://github.com/CakewalkUAE/softstudio-projectmanagement/issues/468

--- apps/www/registry/tui/ui/input.tsx
+++ apps/www/registry/tui/ui/input.tsx
@@ -147,7 +147,7 @@ const Input = React.forwardRef<
           </label>
           )
         }
-        <div className={cn("mt-2 flex rounded-md shadow-sm", className)}>
+        <div className={cn("mt-2 flex rounded-md shadow-sm relative", className)}>
 
           {(borderInside === "withBorder") ?
             <div className={cn("flex rounded-l-md shadow-sm max-w-md", className)}>
@@ -156,11 +156,11 @@ const Input = React.forwardRef<
               <span className={cn("inline-flex items-center  rounded-l-md border border-r-0 px-3 sm:text-sm", className)}{...props}>
                 {addOnLabel}</span> : null
           }
-          {icon ? <Icon name={icon} className={cn(`${alignIcon === "left" ? "absolute top-[37px] left-0 flex items-center pl-3" : "mr-2 absolute top-[37px] right-0 flex items-center pl-3"} ${iconStyle}`, fontColor(color), className)} {...props} /> : null}
+          {icon ? <Icon name={icon} className={cn(`${alignIcon === "left" ? "absolute top-[11px] left-0 flex items-center pl-3" : "mr-2 absolute top-[11px] right-0 flex items-center pl-3"} ${iconStyle}`, fontColor(color), className)} {...props} /> : null}
           {
             alignDropdown && (
               <div className={cn(`absolute text-sm inset-y-0 ${alignDropdown === 'prefix' ? 'left-0 flex items-center' : 'right-0 flex pr-px items-center -mb-[7px]'}`, className)} {...props}>
-                <select className={cn(`mt-${alignDropdown === 'prefix' ? '8' : '6'} ${alignDropdown === 'prefix' ? 'pl-2' : 'pr-2'} bg-inherit`, fontColor(color), className)}>
+                <select className={cn(`mt-${alignDropdown === 'prefix' ? '8' : '6'} ${alignDropdown === 'prefix' ? 'pl-2 -top-[23px]' : 'pr-2 -left-[61px] -top-[14px]'} bg-inherit absolute`, fontColor(color), className)}>
                   {options && options.map((option:string, index: number) => (
                     <option key={index}>{option}</option>
                   ))}
@@ -192,7 +192,7 @@ const Input = React.forwardRef<
 
         {trailingAddOn && (
           <div className="pointer-events-none absolute -mb-[30px] inset-y-0 right-0 flex items-center pr-3">
-            <span className={cn("text-sm", fontColor(color), className)} {...props}>{addOnLabel}</span>
+            <span className={cn("text-sm absolute top-[8px] -left-[28px]", fontColor(color), className)} {...props}>{addOnLabel}</span>
           </div>
         )}
 
