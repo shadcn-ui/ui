ac1860404cea10e4f68b49e2ee7514e056d700b7
Lachlan Heywood <lachieh@users.noreply.github.com>
2023-09-26 17:50:17 -0400

    fix(components): remove className from dialog portals

--- apps/www/registry/new-york/ui/sheet.tsx
+++ apps/www/registry/new-york/ui/sheet.tsx
@@ -13,13 +13,7 @@ const SheetTrigger = SheetPrimitive.Trigger
 
 const SheetClose = SheetPrimitive.Close
 
-const SheetPortal = ({
-  className,
-  ...props
-}: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
-)
-SheetPortal.displayName = SheetPrimitive.Portal.displayName
+const SheetPortal = SheetPrimitive.Portal
 
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
