d34e45ffb85f978746966b0fb4f9064b27379e5e
lloydrichards <lloyd.d.richards@gmail.com>
2023-09-28 13:17:05 +0200

    refactor(storybook): :recycle: add missing Destructive story to Button

--- apps/www/registry/stories/button.stories.tsx
+++ apps/www/registry/stories/button.stories.tsx
@@ -1,46 +1,58 @@
-import { Meta, StoryObj } from "@storybook/react"
-import { Loader2, Mail } from "lucide-react"
+import { Meta, StoryObj } from "@storybook/react";
+import { Loader2, Mail } from "lucide-react";
 
-import { Button } from "../default/ui/button"
+import { Button } from "../default/ui/button";
 
 const meta: Meta<typeof Button> = {
   title: "ui/Button",
   component: Button,
   tags: ["autodocs"],
   argTypes: {},
-}
-export default meta
+};
+export default meta;
 
-type Story = StoryObj<typeof Button>
+type Story = StoryObj<typeof Button>;
 
 export const Base: Story = {
   render: (args) => <Button {...args}>Button</Button>,
   args: {},
-}
+};
+
 export const Outline: Story = {
   render: (args) => <Button {...args}>Button</Button>,
   args: {
     variant: "outline",
   },
-}
+};
+
 export const Ghost: Story = {
   render: (args) => <Button {...args}>Button</Button>,
   args: {
     variant: "ghost",
   },
-}
+};
+
 export const Secondary: Story = {
   render: (args) => <Button {...args}>Button</Button>,
   args: {
     variant: "secondary",
   },
-}
+};
+
+export const Destructive: Story = {
+  render: (args) => <Button {...args}>Button</Button>,
+  args: {
+    variant: "destructive",
+  },
+};
+
 export const Link: Story = {
   render: (args) => <Button {...args}>Button</Button>,
   args: {
     variant: "link",
   },
-}
+};
+
 export const Loading: Story = {
   render: (args) => (
     <Button {...args}>
@@ -51,7 +63,8 @@ export const Loading: Story = {
   args: {
     variant: "outline",
   },
-}
+};
+
 export const WithIcon: Story = {
   render: (args) => (
     <Button {...args}>
@@ -61,4 +74,4 @@ export const WithIcon: Story = {
   args: {
     variant: "secondary",
   },
-}
+};
