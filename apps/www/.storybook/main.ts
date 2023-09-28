f22c6615e3f7076edae34470f05b1987244ec2d4
lloydrichards <lloyd.d.richards@gmail.com>
2023-09-28 13:06:12 +0200

    chore(storybook): :arrow_up: upgrade storybook to v7.4.5

--- apps/www/.storybook/main.ts
+++ apps/www/.storybook/main.ts
@@ -1,17 +1,20 @@
-import { dirname, join } from "path";
+import { dirname, join } from "path"
 import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
-  stories: ["../registry/stories/**/*.mdx", "../registry/stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../registry/stories/**/*.mdx",
+    "../registry/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-styling"),
-    getAbsolutePath("@storybook/addon-mdx-gfm")
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling",
+    "@storybook/addon-mdx-gfm",
   ],
   framework: {
-    name: getAbsolutePath("@storybook/nextjs"),
+    name: "@storybook/nextjs",
     options: {},
   },
   docs: {
@@ -19,7 +22,3 @@ const config: StorybookConfig = {
   },
 }
 export default config
-
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
-}
