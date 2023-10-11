15da12b03b94f43543f2df3f799adc3b49c78c68
giradkarParag <129063759+Parag27-cakewalk@users.noreply.github.com>
2023-10-11 16:13:59 +0530

    requested changes
    https://github.com/CakewalkUAE/softstudio-projectmanagement/issues/468

--- apps/www/registry/tui/example/breadcrumbIcon.tsx
+++ apps/www/registry/tui/example/breadcrumbIcon.tsx
@@ -18,18 +18,18 @@ export const BreadcrumbIcon = () => {
     ]
     return (
         <>
-            <h2 className="text-m text-primary pt-11">BreadCrumb With Icon</h2>
+            <h2 className="text-primary pt-11">BreadCrumb With Icon</h2>
 
-            <div className="text-m font-bold text-primary pt-2 pb-6">Contained</div>
+            <div className="font-bold text-primary pt-2 pb-6">Contained</div>
             <Breadcrumbs dataItem={firstItem} contained={true} iconStyle="h-5 w-5 text-gray-400 hover:text-gray-500" separatorIconStyle="h-full w-6 flex-shrink-0 text-gray-200" textColor="gray" />
 
-            <div className="text-m pt-11 font-bold text-primary pb-6">Full Width Bar</div>
+            <div className="pt-11 font-bold text-primary pb-6">Full Width Bar</div>
             <Breadcrumbs dataItem={firstItem} fullWidthContained={true} iconStyle="h-5 w-5 text-gray-400 hover:text-gray-500" separatorIconStyle="h-full w-6 flex-shrink-0 text-gray-200" textColor="gray" size="default" />
 
-            <div className="text-m pt-11 font-bold text-primary pb-6">Simple With Chevron</div>
+            <div className="pt-11 font-bold text-primary pb-6">Simple With Chevron</div>
             <Breadcrumbs dataItem={secondItem} withChevronSeparator={true} iconStyle="h-5 w-5 text-gray-400 hover:text-gray-500" separatorIconStyle="h-4 w-4 flex-shrink-0 text-gray-400" textColor="gray" />
 
-            <div className="text-m pt-11 font-bold text-primary pb-6 ">Simple with slashes</div>
+            <div className="pt-11 font-bold text-primary pb-6 ">Simple with slashes</div>
             <Breadcrumbs dataItem={thirdItem} withSlashSeparator={true} iconStyle="h-5 w-5 text-gray-400 hover:text-gray-500" separatorIconStyle="h-4 w-4 flex-shrink-0 text-gray-400" textColor="gray" />
         </>
 
--- apps/www/registry/tui/example/cards/breadcrumb.tsx
+++ apps/www/registry/tui/example/cards/breadcrumb.tsx
@@ -29,16 +29,16 @@ export function CardsBreadcrumb() {
         <CardTitle className="text-base font-normal text-primary">Breadcrumbs</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="text-m font-bold text-primary pb-6">Contained</div>
+        <div className="font-bold text-primary pb-6">Contained</div>
         <Breadcrumbs dataItem={firstItem} contained={true} iconStyle="h-5 w-5 text-gray-400 hover:text-gray-500" separatorIconStyle="h-full w-6 flex-shrink-0 text-gray-200" textColor="gray" />
 
-        <div className="text-m mt-14 font-bold text-primary pb-6">Full Width Bar</div>
+        <div className="mt-14 font-bold text-primary pb-6">Full Width Bar</div>
         <Breadcrumbs dataItem={firstItem} fullWidthContained={true} iconStyle="h-5 w-5 text-gray-400 hover:text-gray-500" separatorIconStyle="h-full w-6 flex-shrink-0 text-gray-200" textColor="gray" size="default" />
 
-        <div className="text-m mt-14 font-bold text-primary pb-6">Simple With Chevron</div>
+        <div className="mt-14 font-bold text-primary pb-6">Simple With Chevron</div>
         <Breadcrumbs dataItem={secondItem} withChevronSeparator={true} iconStyle="h-5 w-5 text-gray-400 hover:text-gray-500" separatorIconStyle="h-4 w-4 flex-shrink-0 text-gray-400" textColor="gray" />
 
-        <div className="text-m mt-14 font-bold text-primary pb-6">Simple with slashes</div>
+        <div className="mt-14 font-bold text-primary pb-6">Simple with slashes</div>
         <Breadcrumbs dataItem={thirdItem} withSlashSeparator={true} iconStyle="h-5 w-5 text-gray-400 hover:text-gray-500" separatorIconStyle="h-4 w-4 flex-shrink-0 text-gray-400" textColor="gray" />
 
         <div className="space-x-2 space-y-2">
