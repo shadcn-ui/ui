065e3a27cea41646a74d287ae6c9bdb8c4a316c9
qq <79323963+rainyEra@users.noreply.github.com>
2023-10-17 02:35:02 +0300

    (textarea)fix: use client w useRef

--- apps/www/registry/default/ui/textarea.tsx
+++ apps/www/registry/default/ui/textarea.tsx
@@ -1,6 +1,6 @@
-"use client"
+'use client'
 import * as React from "react"
-import { useLayoutEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 import { mergeRefs } from "react-merge-refs"
 
 import { cn } from "@/lib/utils"
@@ -11,23 +11,16 @@ export interface TextareaProps
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     const texteAreaRef = useRef<HTMLTextAreaElement>(null)
-    const [height, setHeight] = React.useState(80)
-    console.log(texteAreaRef?.current?.clientHeight);
-    useLayoutEffect(() => {
+
+    useEffect(() => {
       const ref = texteAreaRef?.current
-      console.log(texteAreaRef?.current?.clientHeight);
 
       const updateTextareaHeight = () => {
         if (ref) {
-          const ml = window.getComputedStyle(ref)
-          const cmp = ml.getPropertyValue('clientHeight')
-          console.log('cmp', Number(cmp))
           ref.style.height = "auto"
           ref.style.height = ref?.scrollHeight + "px"
-          setHeight(ref.scrollHeight);
         }
       }
-      console.log(texteAreaRef?.current?.clientHeight);
 
       updateTextareaHeight()
       ref?.addEventListener("input", updateTextareaHeight)
@@ -38,7 +31,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          `flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
+          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={mergeRefs([texteAreaRef, ref])}
@@ -49,5 +42,3 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
 )
 
 Textarea.displayName = "Textarea"
-
-export { Textarea }
