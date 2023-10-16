2707f3e39452c040cc59d9b908584a660be19f94
giradkarParag <129063759+Parag27-cakewalk@users.noreply.github.com>
2023-10-16 16:55:12 +0530

    Alignment Changes
    https://github.com/CakewalkUAE/softstudio-projectmanagement/issues/468

--- apps/www/registry/tui/example/cards/text-area.tsx
+++ apps/www/registry/tui/example/cards/text-area.tsx
@@ -1,6 +1,7 @@
 
 import { Card, CardContent, CardHeader, CardTitle, } from "@/registry/tui/ui/card"
-import { Textarea } from "@/registry/tui/ui/textarea"
+import { ButtonList, Textarea } from "@/registry/tui/ui/textarea"
+import profile from '../../assest/profile.webp';
 
 export function CardsTextArea() {
   const assignList = [
@@ -19,15 +20,15 @@ export function CardsTextArea() {
     { label: "Tomorrow" }
   ]
 
-  const IconButton = [
+  const IconButton:ButtonList[] = [
     { bg: "bg-red-500", label: "Excited", icon: "fire-regular", height: "h-8", width: "w-8" },
     { bg: "bg-pink-600", label: "Loved", icon: "circle-heart-regular", height: "h-8", width: "w-8" },
     { bg: "bg-green-400", label: "Happy", icon: "face-smile-regular", height: "h-8", width: "w-8" },
     { bg: "bg-yellow-400", label: "Sad", icon: "face-worried-regular", height: "h-8", width: "w-8" },
     { bg: "bg-blue-500", label: "Thumbsy", icon: "thumbs-up-regular", height: "h-8", width: "w-8" },
-    { bg: "bg-transparent", label: "I feel nothing", icon: "" }
+    { bg: "bg-transparent", label: "I feel nothing" }
   ]
-
+  const profileImageUrl = profile.src;
   return (
     <Card className="">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -37,17 +38,17 @@ export function CardsTextArea() {
         <div className="text-m font-bold text-primary">Simple</div>
         <Textarea label=" Add your comment" />
 
-        <div className="text-m mt-3 font-bold text-primary">With underline and actions</div>
-        <Textarea placeholder="Add your comment..." iconButton={IconButton} color="indigo" widthForUl="w-56" iconStyle='h-5 w-5 text-gray-400' attachIcon='paperclip-solid' submitButtonName="Post" icon="face-smile-beam-solid" variant="outline" showMoodButton="resultButton" textareavariant="default" showIconForTextArea={true} image="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+        <div className="text-m mt-3 font-bold text-primary">With avatar and actions</div>
+        <Textarea placeholder="Add your comment..." iconList={IconButton}  icon="face-smile-beam-solid" attachIcon='paperclip-solid'  buttonText="Post" textareavariant="default" showIconListForTextArea={true} imageSrc={profileImageUrl} textColor="indigo" />
 
         <div className="text-m mt-3 font-bold text-primary">With preview button</div>
-        <Textarea placeholder="Add your comment..." iconStyle='h-5 w-5 text-gray-400' color="indigo" icons={["link-simple-solid", "code-solid", "at-solid"]} submitButton="Post" showMoodButton="default" writeButton='Write' previewContent="Preview Content Will be here...." previewButton='Preview' />
+        <Textarea placeholder="Add your comment..."  icons={["link-simple-solid", "code-solid", "at-solid"]} submitButton="Post" showMoodButton="default" firstButtonText='Write' buttonContent="Preview Content Will be here...." secondButtonText='Preview' textColor="indigo" />
 
         <div className="text-m mt-3 font-bold text-primary">With underline and actions</div>
-        <Textarea placeholder="Add your comment..." iconStyle='h-5 w-5 text-gray-400' color="indigo" widthForUl="w-56" attachIcon='paperclip-solid' icon="face-smile-beam-solid" submitButtonName="Post" iconButton={IconButton} showIconForTextArea={true} image="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+        <Textarea placeholder="Add your comment..."  attachIcon='paperclip-solid' icon="face-smile-beam-solid" buttonText="Post" iconList={IconButton} showIconListForTextArea={true} imageSrc={profileImageUrl} textColor="indigo" />
 
         <div className="text-m font-bold text-primary">With title and pill actions</div>
-        <Textarea placeholder="Add your comment..." iconStyle='h-5 w-5 text-gray-400' color="indigo" hasDivider={true} titlePlaceholder="Title" assignButtonName='Assign' icon="circle-user-regular" labelButtonName="Label" assignList={assignList} createButton='Create' labelList={labelList} dueDateButtonName="Due Date" dueDateList={dueDateList} attachFileName="Attach File" attachIcon='paperclip-solid' textareavariant="borderForInput" />
+        <Textarea placeholder="Add your comment..."  hasDivider={true} titlePlaceholder="Title" assignButtonName='Assign' icon="circle-user-regular" labelButtonName="Label" assignList={assignList} createButton='Create' labelList={labelList} dueDateButtonName="Due Date" dueDateList={dueDateList} dividerText="Attach File" attachIcon='paperclip-solid' textareavariant="borderForInput" textColor="indigo" />
 
       </CardContent>
 
