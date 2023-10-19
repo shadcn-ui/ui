96820d04255661b03fc05e74034c145df38009e6
giradkarParag <129063759+Parag27-cakewalk@users.noreply.github.com>
2023-10-19 12:05:09 +0530

    Update input.tsx

--- apps/www/registry/tui/ui/input.tsx
+++ apps/www/registry/tui/ui/input.tsx
@@ -160,7 +160,7 @@ const Input = React.forwardRef<
             <div className={cn("flex max-w-md rounded-l-md shadow-sm", className)}>
               <span className={cn("flex select-none items-center rounded-l-md border py-2 px-3 text-sm text-primary/80 ", className)}{...props}>{addOnText}</span> </div>
             : (addOnBorder === "withoutBorder") ?
-              <span className={cn("flex items-center rounded-l-md border border-r-0 pl-3 py-2 text-sm", className)}{...props}>
+              <span className={cn("flex items-center rounded-l-md border border-r-0 pl-3 py-2 text-sm text-primary/80", className)}{...props}>
                 {addOnText}</span> : null
           }
           {icon ? <Icon name={icon} className={cn(" h-5 w-5 absolute flex items-center pl-3 text-primary/50", `${alignIcon === "left" ? "left-0" : " right-0 pr-2"} ${iconStyle}`, className)} {...props} /> : null}
@@ -168,7 +168,7 @@ const Input = React.forwardRef<
           {
             alignDropdown && (
               <div className={cn(`absolute text-sm  pr-2 ${alignDropdown === 'right' ? 'right-0' : 'left-0 '} flex items-center`, className)} {...props}>
-                <select className={cn(`${alignDropdown === 'right' ? 'pr-1 ' : 'pl-1'} h-full rounded-md border-0 bg-transparent py-2 pl-1.5 pr-0 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary/60 sm:text-sm text-primary/80`, className)}>
+                <select className={cn(`${alignDropdown === 'right' ? 'pr-1 ' : 'pl-1'} h-full rounded-md border-0 bg-transparent py-2 pl-1.5 pr-0 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary/60 sm:text-sm text-primary/50`, className)}>
                   {options && options.map((option: string, index: number) => (
                     <option key={index}>{option}</option>
                   ))}
@@ -217,7 +217,7 @@ const Input = React.forwardRef<
 
         {bottomBorder && <div className={cn("absolute inset-x-0 bottom-0 border-t border-primary/30 focus:border-t focus:ring-1 ring-primary/30 ring-1", className)} {...props} />}
 
-        {note && <span className={cn("text-sm text-primary/50", className)} {...props}>{note}</span>}
+        {note && <span className={cn("text-sm text-primary/80", className)} {...props}>{note}</span>}
 
       </div>
     )
