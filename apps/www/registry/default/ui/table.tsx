4a6db0a5f76de43faf2fedfc35fcb8a41d13f1f5
Angel <angelhodar76@gmail.com>
2023-10-21 16:04:19 +0200

    Added whitespace-nowrap to table cell to allow proper expand

--- apps/www/registry/default/ui/table.tsx
+++ apps/www/registry/default/ui/table.tsx
@@ -84,7 +84,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("p-4 whitespace-nowrap align-middle [&:has([role=checkbox])]:pr-0", className)}
     {...props}
   />
 ))
--- apps/www/registry/new-york/ui/table.tsx
+++ apps/www/registry/new-york/ui/table.tsx
@@ -85,7 +85,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "p-2 whitespace-nowrap align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className
     )}
     {...props}
