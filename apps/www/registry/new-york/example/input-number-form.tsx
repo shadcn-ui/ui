cfcc165f299bf4f24012bf2c9a7ce012d5bb1a11
Dino Kljuco <dinokljuco@gmail.com>
2023-10-24 00:23:29 +0200

    feat(input-number): update docs

--- apps/www/registry/new-york/example/input-number-form.tsx
+++ apps/www/registry/new-york/example/input-number-form.tsx
@@ -4,62 +4,62 @@ import * as z from "zod"
 
 import { Button } from "@/registry/new-york/ui/button"
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/registry/new-york/ui/form"
-import { toast } from "@/registry/new-york/ui/use-toast"
 import { InputNumber } from "@/registry/new-york/ui/input-number"
+import { toast } from "@/registry/new-york/ui/use-toast"
 
 const FormSchema = z.object({
-    age: z.number({
-        required_error: "Age is required",
-        invalid_type_error: "Age must be a number",
-    })
+  age: z.number({
+    required_error: "Age is required",
+    invalid_type_error: "Age must be a number",
+  }),
 })
 
-export function InputNumberForm() {
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
+export default function InputNumberForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      age: 0,
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
     })
+  }
 
-    function onSubmit(data: z.infer<typeof FormSchema>) {
-        toast({
-            title: "You submitted the following values:",
-            description: (
-                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-                </pre>
-            ),
-        })
-    }
-
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                <FormField
-                    control={form.control}
-                    name="age"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Age</FormLabel>
-                            <FormControl>
-                                <InputNumber
-                                    min={0}
-                                    step={5}
-                                    {...field} />
-                            </FormControl>
-                            <FormDescription>Enter your age in years.</FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
-    )
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="age"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Age</FormLabel>
+              <FormControl>
+                <InputNumber className="w-[320px]" min={0} step={5} {...field} />
+              </FormControl>
+              <FormDescription>Enter your age in years.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
 }
