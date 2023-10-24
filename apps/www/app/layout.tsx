dd0b9b1a065bbfd2dc66ffc585639e5dc2386109
Being Of Existence <manfromearth25@gmail.com>
2023-10-24 08:26:06 +0000

    chore: codes updated for light mode and scrollbar added for all browsers || opengraph warning fixed

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -14,6 +14,7 @@ import { Toaster as DefaultToaster } from "@/registry/default/ui/toaster"
 import { Toaster as NewYorkToaster } from "@/registry/new-york/ui/toaster"
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://shadcn.com"),
   title: {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`,
