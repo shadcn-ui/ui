b5dc9836c9a5767f922e64409c9be84678cbe590
Being Of Existence <manfromearth25@gmail.com>
2023-10-26 02:12:07 +0000

    chore: searchbar ⌘ button centered || scrollbar code updated for simplicity

--- apps/www/components/command-menu.tsx
+++ apps/www/components/command-menu.tsx
@@ -60,7 +60,7 @@ export function CommandMenu({ ...props }: DialogProps) {
         <span className="hidden lg:inline-flex">Search documentation...</span>
         <span className="inline-flex lg:hidden">Search...</span>
         <kbd className="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-          <span className="text-xs">⌘</span>K
+          <span className="text-xs mt-[3px]">⌘</span>K
         </kbd>
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
