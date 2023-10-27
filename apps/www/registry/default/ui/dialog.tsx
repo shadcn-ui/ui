20135d3f8aa7e0bfc417726d35359f3ff300af31
Ayhan <2834954+c0b41@users.noreply.github.com>
2023-10-27 23:28:51 +0300

    fix(dialog): dark mode support for dialog title

--- apps/www/registry/default/ui/dialog.tsx
+++ apps/www/registry/default/ui/dialog.tsx
@@ -88,7 +88,7 @@ const DialogTitle = React.forwardRef<
   <DialogPrimitive.Title
     ref={ref}
     className={cn(
-      "text-lg font-semibold leading-none tracking-tight",
+      "text-lg font-semibold leading-none tracking-tight text-muted-foreground",
       className
     )}
     {...props}
