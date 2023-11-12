4ca4cf03ceb87ae8c32f4aea2c5d77915d6d7943
shadcn <m@shadcn.com>
2023-11-12 14:54:27 +0400

    fix(accordion): update new york style

--- apps/www/registry/default/ui/accordion.tsx
+++ apps/www/registry/default/ui/accordion.tsx
@@ -49,9 +49,9 @@ const AccordionContent = React.forwardRef<
     className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn('pb-4 pt-0', className)}>{children}</div>
+    <div className={cn("pb-4 pt-0", className)}>{children}</div>
   </AccordionPrimitive.Content>
-));
+))
 
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
