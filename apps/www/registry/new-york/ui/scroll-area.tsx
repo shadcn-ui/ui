7df58147e79ad254d61152db5bc39846b7e0d03a
Yahor Barkouski <yahor.barkouski@gmail.com>
2023-11-12 14:22:02 +0100

    chore: pnpm format:write

--- apps/www/registry/new-york/ui/scroll-area.tsx
+++ apps/www/registry/new-york/ui/scroll-area.tsx
@@ -40,9 +40,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb
-      className="relative flex-1 rounded-full bg-border"
-    />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
