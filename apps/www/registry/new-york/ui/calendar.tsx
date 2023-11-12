949ae407b75e2887840d6c087c200cd0f1b08d7c
shadcn <m@shadcn.com>
2023-11-12 13:29:38 +0400

    style(calendar): fix code check

--- apps/www/registry/new-york/ui/calendar.tsx
+++ apps/www/registry/new-york/ui/calendar.tsx
@@ -51,7 +51,8 @@ function Calendar({
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
-        day_outside: "day-outside text-muted-foreground opacity-50  aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
+        day_outside:
+          "day-outside text-muted-foreground opacity-50  aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
