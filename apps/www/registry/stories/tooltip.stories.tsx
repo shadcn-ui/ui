8ef99b4ededfc6c791c5a8f2b1295742485d1158
lloydrichards <lloyd.d.richards@gmail.com>
2023-11-13 18:19:11 +0100

    fix(storybook): :bug: wrap tooltip story in TooltipProvider

--- apps/www/registry/stories/tooltip.stories.tsx
+++ apps/www/registry/stories/tooltip.stories.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/registry/default/ui/button"
 import {
   Tooltip,
   TooltipContent,
+  TooltipProvider,
   TooltipTrigger,
 } from "@/registry/default/ui/tooltip"
 
@@ -20,17 +21,19 @@ type Story = StoryObj<typeof Tooltip>
 
 export const Base: Story = {
   render: (args) => (
-    <Tooltip {...args}>
-      <TooltipTrigger asChild>
-        <Button variant="outline" className="w-10 rounded-full p-0">
-          <Plus className="h-4 w-4" />
-          <span className="sr-only">Add</span>
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent>
-        <p>Add to library</p>
-      </TooltipContent>
-    </Tooltip>
+    <TooltipProvider>
+      <Tooltip {...args}>
+        <TooltipTrigger asChild>
+          <Button variant="outline" className="w-10 rounded-full p-0">
+            <Plus className="h-4 w-4" />
+            <span className="sr-only">Add</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add to library</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   ),
   args: {},
 }
