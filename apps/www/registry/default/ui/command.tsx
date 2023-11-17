008a75b88dd085a1c5dbd8af148a7648f3759f28
Kevin Mitchell <kevinmitch14@gmail.com>
2023-11-17 20:15:19 -0300

    fix(cmdk):  `data-[disabled]` => `aria-disabled` `aria-selected`=> `data-[selected]`

--- apps/www/registry/default/ui/command.tsx
+++ apps/www/registry/default/ui/command.tsx
@@ -117,7 +117,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[selected]:bg-accent data-[selected]:text-accent-foreground",
       className
     )}
     {...props}
