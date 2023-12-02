877eb00c081f40b33bd59f5c878dc24662969250
Kevin Mitchell <kevinmitch14@gmail.com>
2023-12-02 13:17:25 -0300

    feat(data-table): dont show page count if zero

--- apps/www/app/examples/tasks/components/data-table-pagination.tsx
+++ apps/www/app/examples/tasks/components/data-table-pagination.tsx
@@ -49,10 +49,12 @@ export function DataTablePagination<TData>({
             </SelectContent>
           </Select>
         </div>
-        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
-          Page {table.getState().pagination.pageIndex + 1} of{" "}
-          {table.getPageCount()}
-        </div>
+        {table.getPageCount() !== 0 && (
+          <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+            Page {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </div>
+        )}
         <div className="flex items-center space-x-2">
           <Button
             variant="outline"
