d559e4dc1c7ee186a29ce696a5426d594c30c244
Gary Byrne <garyb1@live.ie>
2023-12-08 07:43:47 +0000

    refactor(components): update accordion svg to be aria-hidden

--- apps/www/registry/default/ui/accordion.tsx
+++ apps/www/registry/default/ui/accordion.tsx
@@ -46,7 +46,7 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm transition-all"
+    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
