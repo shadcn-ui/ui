b4c75b861e8062af69505bcafe3515c59f2ba789
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-12-13 08:03:59 +0000

    feat(button): added new variant "xl" for big button

--- apps/www/registry/new-york/ui/button.tsx
+++ apps/www/registry/new-york/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        xl: "h-12 rounded-md px-10 text-xl font-semibold",
         icon: "h-9 w-9",
       },
     },
