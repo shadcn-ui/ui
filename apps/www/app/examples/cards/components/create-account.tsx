626853b02040477e0377e3177f2ac57893e66603
Sony AK <sony@sony-ak.com>
2023-12-23 20:22:54 +0700

    fix: Brand typo, Github to GitHub

--- apps/www/app/examples/cards/components/create-account.tsx
+++ apps/www/app/examples/cards/components/create-account.tsx
@@ -26,7 +26,7 @@ export function DemoCreateAccount() {
         <div className="grid grid-cols-2 gap-6">
           <Button variant="outline">
             <Icons.gitHub className="mr-2 h-4 w-4" />
-            Github
+            GitHub
           </Button>
           <Button variant="outline">
             <Icons.google className="mr-2 h-4 w-4" />
