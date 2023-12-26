a20fc49ac25e95d88e5bd9e7801c411c0e9a9eff
rishabhbizzle <rishabhbizzle@gmail.com>
2023-12-26 21:52:51 +0530

    feat(slider): add vertical orientation support for slider

--- apps/www/registry/default/ui/slider.tsx
+++ apps/www/registry/default/ui/slider.tsx
@@ -8,17 +8,29 @@ import { cn } from "@/lib/utils"
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
+>(({ className, orientation = "horizontal", ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
+    orientation={orientation}
     className={cn(
-      "relative flex w-full touch-none select-none items-center",
+      "relative flex touch-none select-none items-center",
+      orientation === "vertical" ? "flex-col h-full w-[20px]" : "w-full",
       className
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+    <SliderPrimitive.Track
+      className={cn(
+        "relative grow overflow-hidden rounded-full bg-secondary",
+        orientation === "vertical" ? "w-2 h-full" : "h-2 w-full"
+      )}
+    >
+      <SliderPrimitive.Range
+        className={cn(
+          "absolute h-full bg-primary",
+          orientation === "vertical" ? "w-full" : "h-full"
+        )}
+      />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
--- apps/www/registry/new-york/ui/slider.tsx
+++ apps/www/registry/new-york/ui/slider.tsx
@@ -8,17 +8,29 @@ import { cn } from "@/lib/utils"
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
+>(({ className, orientation = "horizontal", ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
+    orientation={orientation}
     className={cn(
-      "relative flex w-full touch-none select-none items-center",
+      "relative flex touch-none select-none items-center",
+      orientation === "vertical" ? "flex-col h-full w-[20px]" : "w-full",
       className
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+    <SliderPrimitive.Track
+      className={cn(
+        "relative grow overflow-hidden rounded-full bg-primary/20",
+        orientation === "vertical" ? "w-1.5 h-full" : "h-1.5 w-full"
+      )}
+    >
+      <SliderPrimitive.Range
+        className={cn(
+          "absolute h-full bg-primary",
+          orientation === "vertical" ? "w-full" : "h-full"
+        )}
+      />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
