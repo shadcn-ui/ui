8a489fd39172ab814a2e635ce588b07ced3b1384
dayongkr <dayongkr@gmail.com>
2023-12-27 16:31:11 +0900

    fix(docs): pagination without item

--- apps/www/registry/default/example/pagination-demo.tsx
+++ apps/www/registry/default/example/pagination-demo.tsx
@@ -15,17 +15,9 @@ export default function PaginationDemo() {
         <PaginationItem>
           <PaginationPrevious href="#" />
         </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#" isActive>
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
-        </PaginationItem>
+        <PaginationLink href="#">1</PaginationLink>
+        <PaginationLink href="#" isActive>2</PaginationLink>
+        <PaginationLink href="#">3</PaginationLink>
         <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem>
