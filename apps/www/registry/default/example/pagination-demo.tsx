6aa08834d6b98c4046cd62bcd76055cefbe2cd79
dayongkr <dayongkr@gmail.com>
2023-12-27 16:35:30 +0900

    fix(docs): pagination prev and next

--- apps/www/registry/default/example/pagination-demo.tsx
+++ apps/www/registry/default/example/pagination-demo.tsx
@@ -12,18 +12,14 @@ export default function PaginationDemo() {
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
+        <PaginationPrevious href="#" />
         <PaginationLink href="#">1</PaginationLink>
         <PaginationLink href="#" isActive>2</PaginationLink>
         <PaginationLink href="#">3</PaginationLink>
         <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
+        <PaginationNext href="#" />
       </PaginationContent>
     </Pagination>
   )
