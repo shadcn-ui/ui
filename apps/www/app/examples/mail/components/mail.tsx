5f62a98b1057f5c2c8e4f61eb4ecf50502004716
viruop <virajrai706@gmail.com>
2023-12-27 11:59:50 +0530

    fix: added event.preventdefault to stop page reload

--- apps/www/app/examples/mail/components/mail.tsx
+++ apps/www/app/examples/mail/components/mail.tsx
@@ -174,7 +174,7 @@ export function Mail({
             </div>
             <Separator />
             <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-              <form>
+              <form onSubmit={(event) =>  event.preventDefault()}>
                 <div className="relative">
                   <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
                   <Input placeholder="Search" className="pl-8" />
