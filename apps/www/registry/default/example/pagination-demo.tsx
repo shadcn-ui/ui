ef5a0cf91b3ce829c710e4b013762d238c1c681d
sfsajid91 <sfsajid91@gmail.com>
2023-12-28 23:37:44 +0600

    fix(docs): paginationlink component without paginationitem component

--- apps/www/registry/default/example/pagination-demo.tsx
+++ apps/www/registry/default/example/pagination-demo.tsx
@@ -12,26 +12,16 @@ export default function PaginationDemo() {
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#" isActive>
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
-        </PaginationItem>
+        <PaginationPrevious href="#" />
+        <PaginationLink href="#">1</PaginationLink>
+        <PaginationLink href="#" isActive>
+          2
+        </PaginationLink>
+        <PaginationLink href="#">3</PaginationLink>
         <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
+        <PaginationNext href="#" />
       </PaginationContent>
     </Pagination>
   )
