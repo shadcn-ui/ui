b370e31f935627bf012fc4320dea06f73b2d29b6
papsavas <papsavas56@gmail.com>
2023-12-29 19:56:54 +0200

    chore: remove arbitrary single mode

--- apps/www/registry/default/example/selector-single-demo.tsx
+++ apps/www/registry/default/example/selector-single-demo.tsx
@@ -40,7 +40,6 @@ const users = [
 export default function SelectorSingleDemo() {
   return (
     <Selector
-      mode="single"
       data={users}
       valueAccessor={(user) => user.id}
       onSelectChange={({ added, removed }) => {
