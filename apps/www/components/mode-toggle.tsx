b28c295f4435be25dfc89060d341bc7d65111dd9
Ajay Mahadeven <m.ajaypradeep@icloud.com>
2023-12-30 01:48:41 +0000

    Update mode-toggle.tsx
    - Users can now view what option they select.
    - Sun and Moon UI is reflected with the change
    - in-house components with icons (Phosphor)

--- apps/www/components/mode-toggle.tsx
+++ apps/www/components/mode-toggle.tsx
@@ -1,40 +1,59 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/registry/new-york/ui/button"
+import { Sun, Moon } from "@phosphor-icons/react";
+
+import { Button } from "@/registry/new-york/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/registry/new-york/ui/dropdown-menu"
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from "@/registry/new-york/ui/dropdown-menu";
 
-export function ModeToggle() {
-  const { setTheme } = useTheme()
+export function ThemeToggle() {
+  const { setTheme } = useTheme();
+  const [position, setPosition] = React.useState("system");
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="w-9 px-0">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" size="icon">
+          <Sun
+            weight="fill"
+            className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Moon
+            weight="fill"
+            className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
+
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem
+            value="light"
+            onClick={() => setTheme("light")}
+          >
+            Light
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="dark" onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem
+            value="system"
+            onClick={() => setTheme("system")}
+          >
+            System
+          </DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
