6278efb133dbbf62f40f3bbebefe792567a89ca5
Ajay Mahadeven <m.ajaypradeep@icloud.com>
2023-12-30 01:59:24 +0000

    Update mode-toggle.tsx

--- apps/www/components/mode-toggle.tsx
+++ apps/www/components/mode-toggle.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
-
+import { Sun, Moon } from "@phosphor-icons/react";
 
 import { Button } from "@/registry/new-york/ui/button";
 import {
@@ -23,11 +23,11 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <SunIcon
+          <Sun
             weight="fill"
             className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
           />
-          <MoonIcon
+          <Moon
             weight="fill"
             className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
           />
