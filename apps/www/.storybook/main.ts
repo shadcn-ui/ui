727d7ba44b5d0312c646afe0cf0d5694cc776ff7
lloydrichards <lloyd.d.richards@gmail.com>
2024-01-04 17:20:13 +0100

    fix(storybook): :bug: swap Geist font to Inter using preview-body.html

--- apps/www/.storybook/main.ts
+++ apps/www/.storybook/main.ts
@@ -4,6 +4,7 @@ const config: StorybookConfig = {
   stories: [
     "../registry/stories/**/*.mdx",
     "../registry/stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "./theme/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
   addons: [
     "@storybook/addon-links",
