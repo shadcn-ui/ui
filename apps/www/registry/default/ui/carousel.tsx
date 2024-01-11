94ef4fedb536670ac459a4fcac7ba5523abb3863
Francisco Fares <franciscofares23@gmail.com>
2024-01-11 07:54:03 -0300

    fix(#2376): Carousel deprecated imported types
    The library embla-carousel has changed the location of this types to the core package (embla-carousel).
    
    Actions needed: pnpm install embla-carousel

--- apps/www/registry/default/ui/carousel.tsx
+++ apps/www/registry/default/ui/carousel.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import * as React from "react"
-import useEmblaCarousel, {
+import useEmblaCarousel from "embla-carousel-react"
+import {
   type EmblaCarouselType as CarouselApi,
   type EmblaOptionsType as CarouselOptions,
   type EmblaPluginType as CarouselPlugin,
-} from "embla-carousel-react"
+} from 'embla-carousel'
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
