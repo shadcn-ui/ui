74a2d5cdbfb29a72806396f7944947f767c0ce16
Lars-Jørgen Kristiansen <1528255+LarsJK@users.noreply.github.com>
2024-01-12 14:41:01 +0100

    fix(components): fix missing value on the progress component

--- apps/www/registry/new-york/ui/progress.tsx
+++ apps/www/registry/new-york/ui/progress.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils"
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -19,7 +19,7 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (props.value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
