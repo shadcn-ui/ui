ec3df7f1bd0e131f756f35e661f325ea1906ed29
Chaejun Lee <chaejunlee@knu.ac.kr>
2024-01-14 00:33:50 -0800

    fix: add more `grid-cols-1`

--- apps/www/registry/new-york/example/cards/index.tsx
+++ apps/www/registry/new-york/example/cards/index.tsx
@@ -13,10 +13,10 @@ import { CardsTeamMembers } from "@/registry/new-york/example/cards/team-members
 
 export default function CardsDemo() {
   return (
-    <div className="grid md:grid-cols-2 md:gap-4 lg:grid-cols-10 xl:grid-cols-11 xl:gap-4">
-      <div className="space-y-4 lg:col-span-4 xl:col-span-6 xl:space-y-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:grid-cols-10 xl:grid-cols-11 xl:gap-4">
+      <div className="grid-cols-1 space-y-4 lg:col-span-4 xl:col-span-6 xl:space-y-4">
         <CardsStats />
-        <div className="grid gap-1 sm:grid-cols-[260px_minmax(0,1fr)] md:hidden">
+        <div className="grid grid-cols-1 gap-1 sm:grid-cols-[260px_minmax(0,1fr)] md:hidden">
           <CardsCalendar />
           <div className="pt-3 sm:pl-2 sm:pt-0 xl:pl-4">
             <CardsActivityGoal />
@@ -25,7 +25,7 @@ export default function CardsDemo() {
             <CardsMetric />
           </div>
         </div>
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
           <div className="space-y-4 xl:space-y-4">
             <CardsTeamMembers />
             <CardsCookieSettings />
