0fe5e938ffa87be1aa154fdb121f32e67445eb68
Simone Cervini <sc.cervini@gmail.com>
2024-01-20 12:20:22 +0100

    fix: remove double space

--- apps/www/registry/default/ui/alert-dialog.tsx
+++ apps/www/registry/default/ui/alert-dialog.tsx
@@ -18,7 +18,7 @@ const AlertDialogOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Overlay
     className={cn(
-      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props}
