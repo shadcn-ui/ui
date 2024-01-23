526db5c69e90e2706a69c946024c427641dd1834
Basem Sufan <xb3sox@users.noreply.github.com>
2024-01-23 03:11:09 +0300

    fix (sheet): extend SheetContentProps types
    Fix interface SheetContentProps to extend React.ComponentPropsWithoutRef and VariantProps

--- apps/www/registry/default/ui/sheet.tsx
+++ apps/www/registry/default/ui/sheet.tsx
@@ -51,7 +51,10 @@ const sheetVariants = cva(
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+    VariantProps<typeof sheetVariants> {
+  children?: React.ReactNode;
+  className?: string;
+}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
