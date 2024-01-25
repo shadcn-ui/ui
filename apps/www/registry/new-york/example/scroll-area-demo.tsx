3057a78a651aff79d60e4c777aca7fafc8b00949
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2024-01-25 18:50:48 -0300

    fix: missing key in `<ScrollArea />` example

--- apps/www/registry/new-york/example/scroll-area-demo.tsx
+++ apps/www/registry/new-york/example/scroll-area-demo.tsx
@@ -13,12 +13,10 @@ export default function ScrollAreaDemo() {
       <div className="p-4">
         <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
         {tags.map((tag) => (
-          <>
-            <div key={tag} className="text-sm">
-              {tag}
-            </div>
+          <React.Fragment key={tag}>
+            <div className="text-sm">{tag}</div>
             <Separator className="my-2" />
-          </>
+          </React.Fragment>
         ))}
       </div>
     </ScrollArea>
