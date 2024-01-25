dad23f496b91c1a4f7f8243763a2a36942cc67e5
adrianrivers <adrianlrivers@gmail.com>
2024-01-25 13:50:28 +1300

    fix(table): checkbox alignment new york

--- apps/www/app/examples/tasks/components/columns.tsx
+++ apps/www/app/examples/tasks/components/columns.tsx
@@ -21,7 +21,6 @@ export const columns: ColumnDef<Task>[] = [
         }
         onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
         aria-label="Select all"
-        className="translate-y-[2px]"
       />
     ),
     cell: ({ row }) => (
@@ -29,7 +28,6 @@ export const columns: ColumnDef<Task>[] = [
         checked={row.getIsSelected()}
         onCheckedChange={(value) => row.toggleSelected(!!value)}
         aria-label="Select row"
-        className="translate-y-[2px]"
       />
     ),
     enableSorting: false,
