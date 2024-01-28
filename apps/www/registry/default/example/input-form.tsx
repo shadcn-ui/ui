7df1007a5b76d8e839a48fdf3ed92d2d52082456
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2024-01-28 05:47:45 -0300

    refactor: update zod import (#2466)

--- apps/www/registry/default/example/input-form.tsx
+++ apps/www/registry/default/example/input-form.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import * as z from "zod"
+import { z } from "zod"
 
 import { Button } from "@/registry/default/ui/button"
 import {
