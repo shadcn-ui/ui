0169f10bc7910ad77e48eb2061743f285dae4e05
Ramit Koul <rkwap2011@gmail.com>
2024-02-05 21:03:15 +0530

    fix: formatting

--- apps/www/registry/new-york/ui/use-toast.ts
+++ apps/www/registry/new-york/ui/use-toast.ts
@@ -35,22 +35,22 @@ type ActionType = typeof actionTypes
 
 type Action =
   | {
-    type: ActionType["ADD_TOAST"]
-    toast: ToasterToast
-  }
+      type: ActionType["ADD_TOAST"]
+      toast: ToasterToast
+    }
   | {
-    type: ActionType["UPDATE_TOAST"]
-    toast: Partial<ToasterToast>
-  }
+      type: ActionType["UPDATE_TOAST"]
+      toast: Partial<ToasterToast>
+    }
   | {
-    type: ActionType["DISMISS_TOAST"]
-    toastId?: ToasterToast["id"]
-    duration?: number
-  }
+      type: ActionType["DISMISS_TOAST"]
+      toastId?: ToasterToast["id"]
+      duration?: number
+    }
   | {
-    type: ActionType["REMOVE_TOAST"]
-    toastId?: ToasterToast["id"]
-  }
+      type: ActionType["REMOVE_TOAST"]
+      toastId?: ToasterToast["id"]
+    }
 
 interface State {
   toasts: ToasterToast[]
@@ -109,9 +109,9 @@ export const reducer = (state: State, action: Action): State => {
         toasts: state.toasts.map((t) =>
           t.id === toastId || toastId === undefined
             ? {
-              ...t,
-              open: false,
-            }
+                ...t,
+                open: false,
+              }
             : t
         ),
       }
