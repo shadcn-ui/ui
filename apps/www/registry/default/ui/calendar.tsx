fae1a5245a5a3735c44e74bcd51480ff0cb01b6f
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-02-06 20:39:53 +0200

    fix: removed duplicate white space from classes

--- apps/www/registry/default/ui/calendar.tsx
+++ apps/www/registry/default/ui/calendar.tsx
@@ -37,7 +37,7 @@ function Calendar({
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: cn(
-          "relative h-9 w-9 p-0 text-center text-sm  focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50",
+          "relative h-9 w-9 p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50",
           props.mode === "range"
             ? "[&:has(.day-range-end)]:rounded-r-md [&:has(.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
             : "[&:has([aria-selected])]:rounded-md"
