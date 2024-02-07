a4900c0bc06af867044244b681a8a0f9052ee0aa
Alissa Tung <alissa-tung@outlook.com>
2024-02-07 19:48:36 +0800

    fix: forwardRef for ResizablePanelGroup

--- apps/www/registry/new-york/ui/resizable.tsx
+++ apps/www/registry/new-york/ui/resizable.tsx
@@ -1,22 +1,25 @@
 "use client"
 
+import * as React from "react"
 import { DragHandleDots2Icon } from "@radix-ui/react-icons"
 import * as ResizablePrimitive from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
-const ResizablePanelGroup = ({
-  className,
-  ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
+const ResizablePanelGroup = React.forwardRef<
+  ResizablePrimitive.ImperativePanelGroupHandle,
+  React.ComponentProps<typeof ResizablePrimitive.PanelGroup>
+>(({ className, ...props }, ref) => (
   <ResizablePrimitive.PanelGroup
+    ref={ref}
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
     )}
     {...props}
   />
-)
+))
+ResizablePanelGroup.displayName = "ResizablePanelGroup"
 
 const ResizablePanel = ResizablePrimitive.Panel
 
