a91d13d08d2b30cd58be7ccd85b16de278b88c64
Le Vaillant Pierre <pierre.levaillant@liveblocks.io>
2024-02-14 11:14:28 +0100

    Remove useless `TooltipPortal` export

--- apps/www/registry/default/ui/tooltip.tsx
+++ apps/www/registry/default/ui/tooltip.tsx
@@ -11,13 +11,11 @@ const Tooltip = TooltipPrimitive.Root
 
 const TooltipTrigger = TooltipPrimitive.Trigger
 
-const TooltipPortal = TooltipPrimitive.Portal
-
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
-  <TooltipPortal>
+  <TooltipPrimitive.Portal>
     <TooltipPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
@@ -27,8 +25,8 @@ const TooltipContent = React.forwardRef<
       )}
       {...props}
     />
-  </TooltipPortal>
+  </TooltipPrimitive.Portal>
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export { Tooltip, TooltipTrigger, TooltipPortal, TooltipContent, TooltipProvider }
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
