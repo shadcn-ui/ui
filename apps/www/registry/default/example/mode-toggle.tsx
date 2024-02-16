a949bf7d156942ff380e6936b2ad327e0bd11e48
Alok Sharma <114926065+aloksharma10@users.noreply.github.com>
2024-02-17 03:13:00 +0530

    Fix the theme toggle warning #2764

--- apps/www/registry/default/example/mode-toggle.tsx
+++ apps/www/registry/default/example/mode-toggle.tsx
@@ -18,11 +18,11 @@ export default function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <div className={buttonVariants({ variant: "outline", size: "icon" })}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
-        </Button>
+        </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
