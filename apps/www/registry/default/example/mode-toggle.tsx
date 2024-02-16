145640a1c5cb5ff4dfb09c1a700b32699d719af7
Alok Sharma <114926065+aloksharma10@users.noreply.github.com>
2024-02-16 21:57:34 +0000

    imported the buttonVariants

--- apps/www/registry/default/example/mode-toggle.tsx
+++ apps/www/registry/default/example/mode-toggle.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/registry/default/ui/button"
+import { buttonVariants } from "@/registry/default/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
