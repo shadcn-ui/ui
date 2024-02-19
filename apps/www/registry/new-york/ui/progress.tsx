75a64fea0dfac9456c1e9c699997e7d5935bb855
jainamdoshi <jainam.doshi2001@gmail.com>
2024-02-19 21:06:54 +1100

    fix: new-york height style

--- apps/www/registry/new-york/ui/progress.tsx
+++ apps/www/registry/new-york/ui/progress.tsx
@@ -17,7 +17,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-        "relative h-4 w-full overflow-hidden rounded-full bg-primary/20",
+        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
       className
     )}
     {...props}
