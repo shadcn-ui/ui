529df4e6414806cdda5991e64486eef401c99ab8
Mohit Khatri <mohit4bug@gmail.com>
2024-02-20 17:41:36 +0530

    refactor(components): changed the way of typing icons object

--- templates/next-template/components/icons.tsx
+++ templates/next-template/components/icons.tsx
@@ -7,10 +7,14 @@ import {
 
 export type Icon = LucideIcon
 
-export const Icons = {
+type IconComponents = {
+  [key: string]: (props: LucideProps) => JSX.Element
+}
+
+export const Icons:IconComponents = {
   sun: SunMedium,
   moon: Moon,
-  twitter: (props: LucideProps) => (
+  twitter: (props) => (
     <svg
       {...props}
       height="23"
@@ -21,7 +25,7 @@ export const Icons = {
       <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
     </svg>
   ),
-  logo: (props: LucideProps) => (
+  logo: (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
       <path
         fill="currentColor"
@@ -29,7 +33,7 @@ export const Icons = {
       />
     </svg>
   ),
-  gitHub: (props: LucideProps) => (
+  gitHub: (props) => (
     <svg viewBox="0 0 438.549 438.549" {...props}>
       <path
         fill="currentColor"
