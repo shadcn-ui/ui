021beb157aa04fd89cce0dca98c76bf69723c647
jainamdoshi <jainam.doshi2001@gmail.com>
2024-02-21 17:48:42 +1100

    fix: remove label from progress component

--- apps/www/registry/new-york/example/progress-with-label.tsx
+++ apps/www/registry/new-york/example/progress-with-label.tsx
@@ -1,7 +1,10 @@
 "use client"
 
-import { Progress } from "@/registry/default/ui/progress"
 import * as React from "react"
+
+import { Progress } from "@/registry/default/ui/progress"
+
+import { Label } from "../ui/label"
 import { Separator } from "../ui/separator"
 
 export default function ProgressDemo() {
@@ -19,10 +22,16 @@ export default function ProgressDemo() {
   }, [])
 
   return (
-    <div className="flex h-full w-full flex-col space-y-5">
-      <Progress label="top" value={progressTop} className="w-[60%]" />
+    <div className="flex flex-col w-full h-full space-y-5">
+      <div className="flex flex-col items-center">
+        <Label className="p-2">{progressTop}%</Label>
+        <Progress value={progressTop} className="w-[60%]" />
+      </div>
       <Separator />
-      <Progress label="bottom" value={progressBottom} className="w-[60%]" />
+      <div className="flex flex-col items-center">
+        <Progress value={progressBottom} className="w-[60%]" />
+        <Label className="p-2">{progressBottom}%</Label>
+      </div>
     </div>
   )
 }
