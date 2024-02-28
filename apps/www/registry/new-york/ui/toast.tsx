be7232b9bb614a475ff50e6e48904da091d0c915
Webdev <arthursantosdeveloper@gmail.com>
2024-02-28 13:51:52 -0300

    chore(app): issue-2872 Added styling responsivity corrections-Toast
    Update toast styling in registry UI for default and new york styled component

--- apps/www/registry/new-york/ui/toast.tsx
+++ apps/www/registry/new-york/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed z-[100] bottom-auto flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 lg:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed top-0 bottom-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
       className
     )}
     {...props}
