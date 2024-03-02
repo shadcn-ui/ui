be3c2bb6ce6f72d84bcc83a9f58dc2d7ad92d755
CHEN Xian-an <xianan.chen@gmail.com>
2024-03-02 16:56:57 +0800

    feat(collapsible): add animation to collapsible component

--- apps/www/registry/new-york/ui/collapsible.tsx
+++ apps/www/registry/new-york/ui/collapsible.tsx
@@ -1,11 +1,28 @@
 "use client"
 
+import * as React from "react"
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+import { cn } from "@/lib/utils"
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+const CollapsibleContent = React.forwardRef<
+  React.ElementRef<typeof CollapsiblePrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <CollapsiblePrimitive.Content
+    ref={ref}
+    className={cn(
+      "overflow-hidden text-sm transition-all data-[state=closed]:animate-collapsible-closed data-[state=open]:animate-collapsible-open",
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </CollapsiblePrimitive.Content>
+))
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -67,10 +67,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "collapsible-open": {
+          from: { height: "0" },
+          to: { height: "var(--radix-collapsible-content-height)" },
+        },
+        "collapsible-closed": {
+          from: { height: "var(--radix-collapsible-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-open": "collapsible-open 0.2s ease-out",
+        "collapsible-closed": "collapsible-closed 0.2s ease-out",
       },
     },
   },
