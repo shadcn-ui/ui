3aa94579c85656c4c2e65c541e4fd35af25b3b7d
shadcn <m@shadcn.com>
2024-03-05 09:42:59 +0400

    feat: update breadcrumb component

--- apps/www/lib/utils.ts
+++ apps/www/lib/utils.ts
@@ -18,9 +18,3 @@ export function formatDate(input: string | number): string {
 export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_APP_URL}${path}`
 }
-
-export function getValidChildren(children: React.ReactNode) {
-  return React.Children.toArray(children).filter((child) =>
-    React.isValidElement(child)
-  ) as React.ReactElement[]
-}
--- apps/www/registry/new-york/ui/breadcrumb.tsx
+++ apps/www/registry/new-york/ui/breadcrumb.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
+import { ChevronRightIcon, DotsHorizontalIcon } from "@radix-ui/react-icons"
 import { Slot } from "@radix-ui/react-slot"
-import { ChevronRight, MoreHorizontal } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -83,7 +83,7 @@ const BreadcrumbSeparator = ({
     className={cn("[&>svg]:size-3.5", className)}
     {...props}
   >
-    {children || <ChevronRight />}
+    {children || <ChevronRightIcon />}
   </li>
 )
 BreadcrumbSeparator.displayName = "BreadcrumbSeparator"
@@ -98,7 +98,7 @@ const BreadcrumbEllipsis = ({
     className={cn("flex h-9 w-9 items-center justify-center", className)}
     {...props}
   >
-    <MoreHorizontal className="h-4 w-4" />
+    <DotsHorizontalIcon className="h-4 w-4" />
     <span className="sr-only">More</span>
   </span>
 )
