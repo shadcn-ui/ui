714257a5297f182f2cb5b226e4acf7e00352464d
Luca Félix <LucaMario.ZieglerFelix@de.bosch.com>
2024-03-08 14:31:12 +0000

    feat: :sparkles: new rating component

---
+++ apps/www/registry/new-york/example/rating-custom-label.tsx
@@ -0,0 +1,5 @@
+import { RatingGroup } from "@/registry/new-york/ui/rating"
+
+export default function RatingCustomLabel() {
+  return <RatingGroup customLabel="This is a custom Label" />
+}
---
+++ apps/www/registry/new-york/example/rating-disabled.tsx
@@ -0,0 +1,5 @@
+import { RatingGroup } from "@/registry/new-york/ui/rating"
+
+export default function RatingDisabled() {
+  return <RatingGroup disabled />
+}
