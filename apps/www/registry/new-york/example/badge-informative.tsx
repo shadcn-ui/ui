43fb53bc324fbdf2dd020273c75ec1598295cd2f
Luca Félix <LucaMario.ZieglerFelix@de.bosch.com>
2024-03-08 09:55:28 +0000

    feat: :sparkles: added informative variant and color

---
+++ apps/www/registry/new-york/example/badge-informative.tsx
@@ -0,0 +1,5 @@
+import { Badge } from "@/registry/new-york/ui/badge"
+
+export default function BadgeInformative() {
+  return <Badge variant="informative">Informative</Badge>
+}
--- apps/www/registry/new-york/ui/badge.tsx
+++ apps/www/registry/new-york/ui/badge.tsx
@@ -14,6 +14,8 @@ const badgeVariants = cva(
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
+        informative:
+          "border-transparent bg-informative text-informative-foreground shadow hover:bg-informative/80",
         outline: "text-foreground",
       },
     },
