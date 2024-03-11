66ea0ad0d823836c389dbcc17dee0c2b15de3fec
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-03-11 06:45:09 -0300

    chore(input-otp): use containerClassName and className separately

--- apps/www/registry/default/ui/input-otp.tsx
+++ apps/www/registry/default/ui/input-otp.tsx
@@ -9,14 +9,14 @@ import { cn } from "@/lib/utils"
 const InputOTP = React.forwardRef<
   React.ElementRef<typeof OTPInput>,
   React.ComponentPropsWithoutRef<typeof OTPInput>
->(({ className, ...props }, ref) => (
+>(({ containerClassName, className, ...props }, ref) => (
   <OTPInput
     ref={ref}
     containerClassName={cn(
       "flex items-center gap-2 has-[:disabled]:opacity-50",
-      className
+      containerClassName
     )}
-    className="disabled:cursor-not-allowed"
+    className={cn("disabled:cursor-not-allowed", className)}
     {...props}
   />
 ))
