915b87bed540496305414df088d4cc362cc1a7ba
malik-jahangir <awan.jahangir.malik@gmail.com>
2024-03-13 06:45:59 +0500

    docs(data table): only show hide btn in column header if enableHiding is true

--- apps/www/app/examples/tasks/components/data-table-column-header.tsx
+++ apps/www/app/examples/tasks/components/data-table-column-header.tsx
@@ -59,11 +59,15 @@ export function DataTableColumnHeader<TData, TValue>({
             <ArrowDownIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
             Desc
           </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => column.toggleVisibility(false)}>
-            <EyeNoneIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-            Hide
-          </DropdownMenuItem>
+          {column.getCanHide() && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => column.toggleVisibility(false)}>
+                <EyeNoneIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+                Hide
+              </DropdownMenuItem>
+            </>
+          )}
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
