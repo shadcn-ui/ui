b1c072a7744a888febe7d95c6bb38fe26fb2071d
G-nizam-A <nizamcs10@gmail.com>
2024-03-17 12:11:35 +0530

    fix(cloudinary): unhandled runtime error #3030
    Fixes #3030

--- templates/next-template/next.config.mjs
+++ templates/next-template/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      //  Cloudinary domain 
+      "res.cloudinary.com"
+    ]
+  }
 }
 
 export default nextConfig
