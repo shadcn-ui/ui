9adb36df894f05fb061585327531e4bc8944de02
Luca Félix <LucaMario.ZieglerFelix@de.bosch.com>
2024-03-18 15:16:34 +0100

    feat: :sparkles: component labels

--- apps/www/app/docs/components/page.tsx
+++ apps/www/app/docs/components/page.tsx
@@ -94,6 +94,12 @@ export default function ComponentsPage() {
 }
 
 function GridListPreview({ side = "left" }: { side?: "left" | "right" }) {
+  const getCompLabel = (componentName: string) => {
+    return Object.values(docsConfig.sidebarNav[1].items).find(
+      (item) => item.title.toLowerCase().split(" ").join("-") === componentName
+    )?.label
+  }
+
   return (
     <div className="flex flex-col gap-3 xl:gap-4 2xl:gap-6">
       {registry
@@ -101,7 +107,7 @@ function GridListPreview({ side = "left" }: { side?: "left" | "right" }) {
         .filter((_, index) => (index + (side === "right" ? 1 : 0)) % 2 === 0)
         .map((component, index) => (
           <Card
-            key={side + index}
+            key={side + index + component.name}
             className="relative flex h-min flex-col transition-colors hover:bg-accent/70"
           >
             <Link
@@ -116,19 +122,9 @@ function GridListPreview({ side = "left" }: { side?: "left" | "right" }) {
                   {component.name.charAt(0).toLocaleUpperCase() +
                     component.name.slice(1)}
                 </span>
-                {docsConfig.sidebarNav.find(
-                  (item) =>
-                    item.title.toLowerCase().split(" ").join("-") ===
-                    component.name
-                )?.label && (
+                {getCompLabel(component.name) && (
                   <span className="ml-2 rounded-md bg-[#adfa1d] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline">
-                    {
-                      docsConfig.sidebarNav.find(
-                        (item) =>
-                          item.title.toLowerCase().split(" ").join("-") ===
-                          component.name
-                      )?.label
-                    }
+                    {getCompLabel(component.name)}
                   </span>
                 )}
               </CardTitle>
