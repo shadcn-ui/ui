88f0807d0da8bec8412453072ddd931db4c5471a
shadcn <m@shadcn.com>
2024-03-20 12:07:35 +0400

    feat: update v0 button

--- apps/www/actions/edit-in-v0.ts
+++ apps/www/actions/edit-in-v0.ts
@@ -2,6 +2,8 @@
 
 import { track } from "@vercel/analytics/server"
 
+const EDIT_IN_V0_SOURCE = "ui.shadcn.com"
+
 export async function editInV0({
   name,
   description,
@@ -11,26 +13,35 @@ export async function editInV0({
   description: string
   code: string
 }) {
-  await track("edit_in_v0", {
-    name,
-    description,
-  })
+  try {
+    await track("edit_in_v0", {
+      name,
+      description,
+    })
 
-  const url = `${process.env.V0_URL}/api/edit`
-  const response = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify({ description, code, source: "ui.shadcn.com" }),
-    headers: {
-      "x-v0-edit-secret": process.env.V0_EDIT_SECRET!,
-      "x-vercel-protection-bypass":
-        process.env.DEPLOYMENT_PROTECTION_BYPASS || "not-set",
-      "Content-Type": "application/json",
-    },
-  })
+    const response = await fetch(`${process.env.V0_URL}/api/edit`, {
+      method: "POST",
+      body: JSON.stringify({ description, code, source: EDIT_IN_V0_SOURCE }),
+      headers: {
+        "x-v0-edit-secret": process.env.V0_EDIT_SECRET!,
+        "x-vercel-protection-bypass":
+          process.env.DEPLOYMENT_PROTECTION_BYPASS || "not-set",
+        "Content-Type": "application/json",
+      },
+    })
 
-  if (response.ok) {
+    if (!response.ok) {
+      if (response.status === 403) {
+        throw new Error("Unauthorized")
+      }
+
+      throw new Error("Something went wrong. Please try again later.")
+    }
+
     return await response.json()
+  } catch (error) {
+    if (error instanceof Error) {
+      return { error: error.message }
+    }
   }
-
-  return null
 }
