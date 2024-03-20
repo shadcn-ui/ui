2fa034363ddf5264628e4d62b5ab446ecb1b9281
shadcn <m@shadcn.com>
2024-03-20 21:19:54 +0400

    feat: update geist font

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
-            fontSans.className
+            fontSans.variable
           )}
         >
           <ThemeProvider
