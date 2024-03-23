37c726e60edde1267fca403c1ec29944530435f2
Ishaan Dey <69771365+ishaan1013@users.noreply.github.com>
2024-03-23 07:01:04 -0400

    fix: dashboard-02 block (shadcn-ui#3098) (#3099)

--- apps/www/registry/new-york/block/dashboard-02.tsx
+++ apps/www/registry/new-york/block/dashboard-02.tsx
@@ -57,7 +57,7 @@ export default function Dashboard() {
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link
-                href="/"
+                href="#"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Home className="h-4 w-4" />
