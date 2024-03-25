23295055024c7ccb6cff0b5e68db2dab055f0f58
yousefhany77 <yousefhany2014@gmail.com>
2024-03-25 15:31:33 +0200

    refactor(Button Component): to support server actions

--- apps/www/registry/default/ui/button.tsx
+++ apps/www/registry/default/ui/button.tsx
@@ -38,19 +38,28 @@ export interface ButtonProps
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
+interface ButtonPropsWithServerAction
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "onClick">,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+  isLoading?: boolean
+  withServerAction: true
+  onClick: () => void
+}
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  ButtonProps | ButtonPropsWithServerAction
+>(({ className, variant, size, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button"
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props}
+    />
+  )
+})
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
