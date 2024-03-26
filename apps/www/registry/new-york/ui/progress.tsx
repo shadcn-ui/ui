b6d4f618801f633998fc5e9ee0248c66f2a5f954
aeither <36173828+aeither@users.noreply.github.com>
2024-03-27 00:08:43 +0700

    fix eslint prefer nullish coalescing operator

--- apps/www/registry/new-york/ui/progress.tsx
+++ apps/www/registry/new-york/ui/progress.tsx
@@ -19,7 +19,7 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
