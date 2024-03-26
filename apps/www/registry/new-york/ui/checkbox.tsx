aa9cc6a10bc02b26d1ad8e2516faef2ca8b6ad44
shrinidhi.upadhyaya <shrinidhi.upadhyaya@stud.uni-bamberg.de>
2024-03-26 17:54:10 +0100

    fix: set icon height and width to parent dimensions #3077

--- apps/www/registry/new-york/ui/checkbox.tsx
+++ apps/www/registry/new-york/ui/checkbox.tsx
@@ -21,7 +21,7 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current")}
     >
-      <CheckIcon className="h-4 w-4" />
+      <CheckIcon className="h-full w-full" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
