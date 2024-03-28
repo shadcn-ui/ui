375fc7a3ac750c111d14c834b19986ab564c1a96
shadcn <m@shadcn.com>
2024-03-28 17:07:00 +0400

    feat(www): update gap for dashboard-05

--- apps/www/registry/new-york/block/dashboard-05.tsx
+++ apps/www/registry/new-york/block/dashboard-05.tsx
@@ -280,7 +280,7 @@ export default function Dashboard() {
         </header>
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
           <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-            <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 lg:gap-8 xl:grid-cols-4">
+            <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
               <Card className="sm:col-span-2">
                 <CardHeader className="pb-3">
                   <CardTitle>Your Orders</CardTitle>
