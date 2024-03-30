6deec9b408ebf236af1f4beed03402076f2cb9ac
Krystian Nowak <kr21032002@icloud.com>
2024-03-30 20:02:31 +0000

    changed checkbox centering

--- apps/www/registry/new-york/ui/checkbox.tsx
+++ apps/www/registry/new-york/ui/checkbox.tsx
@@ -18,10 +18,8 @@ const Checkbox = React.forwardRef<
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
-    >
-      <CheckIcon className="h-4 w-4" />
+    <CheckboxPrimitive.Indicator className={cn("relative text-current")}>
+      <CheckIcon className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
