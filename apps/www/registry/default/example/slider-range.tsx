4164304adcd1aeb1baf73852e66634e6592beb42
JoaoCupido <jcupido13@gmail.com>
2024-03-31 20:21:52 +0100

    feat(slider): add disabled, vertical, range, tick marks and format labels support + update docs

---
+++ apps/www/registry/default/example/slider-range.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils"
+import { Slider } from "@/registry/default/ui/slider"
+
+type SliderProps = React.ComponentProps<typeof Slider>
+
+export default function SliderRange({ className, ...props }: SliderProps) {
+  return (
+    <Slider
+      defaultValue={[25, 75]}
+      max={100}
+      step={1}
+      className={cn("w-[60%]", className)}
+      {...props}
+    />
+  )
+}
