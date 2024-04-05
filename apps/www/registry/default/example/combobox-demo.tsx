55e2d17d6a79f0e5a5425a0d9edaf0772e3c43d1
thesidshah <siddhant997@gmail.com>
2024-04-05 19:27:06 -0400

    fix: include non-empty command inside command list

--- apps/www/registry/default/example/combobox-demo.tsx
+++ apps/www/registry/default/example/combobox-demo.tsx
@@ -64,8 +64,8 @@ export default function ComboboxDemo() {
       <PopoverContent className="w-[200px] p-0">
         <Command>
           <CommandInput placeholder="Search framework..." />
-          <CommandEmpty>No framework found.</CommandEmpty>
           <CommandList>
+          <CommandEmpty>No framework found.</CommandEmpty>
             <CommandGroup>
               {frameworks.map((framework) => (
                 <CommandItem
