fcb44e4d719d80fc6c7c3602db14e6255f16eb1a
Luca Félix <zil1st@bosch.com>
2024-04-06 23:28:48 +0200

    Revert "feat: :sparkles: added informative variant and color"
    This reverts commit 30aa952a89ae1a11a8839c6acbc314081a11573f.

--- apps/www/registry/default/ui/alert.tsx
+++ apps/www/registry/default/ui/alert.tsx
@@ -4,15 +4,13 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
+  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
   {
     variants: {
       variant: {
         default: "bg-background text-foreground",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
-        informative:
-          "border-informative/50 text-informative dark:border-informative [&>svg]:text-informative",
       },
     },
     defaultVariants: {
--- apps/www/registry/new-york/example/badge-informative.tsx
+++
@@ -1,5 +0,0 @@
-import { Badge } from "@/registry/new-york/ui/badge"
-
-export default function BadgeInformative() {
-  return <Badge variant="informative">Informative</Badge>
-}
