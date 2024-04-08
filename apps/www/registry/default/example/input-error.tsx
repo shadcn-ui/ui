84c46dfbf6df1721d37aa5d98d9623a227f9a635
Luca Félix <zil1st@bosch.com>
2024-04-08 10:58:42 +0200

    Revert "feat: :sparkles: input error state"
    This reverts commit c3526f999f1debd4facc3715bbb96ace109ae0b4.

--- apps/www/registry/default/example/input-error.tsx
+++
@@ -1,5 +0,0 @@
-import { Input } from "@/registry/default/ui/input"
-
-export default function InputError() {
-  return <Input type="email" error placeholder="Email" />
-}
--- apps/www/registry/new-york/example/input-error.tsx
+++
@@ -1,5 +0,0 @@
-import { Input } from "@/registry/new-york/ui/input"
-
-export default function InputError() {
-  return <Input type="email" error placeholder="Email" />
-}
