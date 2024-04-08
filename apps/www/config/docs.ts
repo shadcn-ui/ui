dadd7473e862bbe0425ca42ba00cb2a5601e32b1
Luca Félix <zil1st@bosch.com>
2024-04-08 11:05:08 +0200

    Revert "feat: :sparkles: create new timeline component"
    This reverts commit 21ef6b43216f1415a0ebc70cc5cf2b8ed99a996c.

--- apps/www/config/docs.ts
+++ apps/www/config/docs.ts
@@ -316,12 +316,6 @@ export const docsConfig: DocsConfig = {
           items: [],
         },
         {
-          title: "Timeline",
-          href: "/docs/components/timeline",
-          items: [],
-          label: "New",
-        },
-        {
           title: "Toast",
           href: "/docs/components/toast",
           items: [],
