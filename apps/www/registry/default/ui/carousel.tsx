1a7147049d2474c8e3b5e4c91dbe51804ab1c83f
Seunghyun Min <i@seunghyun.in>
2024-04-22 16:33:51 +0900

    refactor(carousel): remove unnecessary falsy handling in Carousel

--- apps/www/registry/default/ui/carousel.tsx
+++ apps/www/registry/default/ui/carousel.tsx
@@ -126,8 +126,7 @@ const Carousel = React.forwardRef<
           carouselRef,
           api: api,
           opts,
-          orientation:
-            orientation || (opts?.axis === "y" ? "vertical" : "horizontal"),
+          orientation,
           scrollPrev,
           scrollNext,
           canScrollPrev,
