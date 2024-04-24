4045f323088d8efbc9915520283831be7a793d7a
Eze Calonge <eze.calonge@naranjax.com>
2024-04-23 21:11:59 -0300

    feat(alert-dialog): change position to bottom

--- apps/www/__registry__/default/ui/alert-dialog.tsx
+++ apps/www/__registry__/default/ui/alert-dialog.tsx
@@ -4,7 +4,8 @@ import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/registry/default/ui/button"
+
+import { buttonVariants } from "./button"
 
 const AlertDialog = AlertDialogPrimitive.Root
 
@@ -36,7 +37,11 @@ const AlertDialogContent = React.forwardRef<
     <AlertDialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        "fixed inset-x-0 bottom-0 z-50 grid w-full max-w-lg translate-x-[-50%] left-1/2 rounded-t-lg p-6 border bg-background shadow-lg duration-200",
+        "slide-in-from-bottom sm:translate-y-[-50%] sm:top-[50%] sm:bottom-auto sm:rounded-lg",
+        "data-[state=open]:slide-in data-[state=closed]:slide-out",
+        "sm:data-[state=open]:fade-in-0 sm:data-[state=closed]:fade-out-0 sm:data-[state=open]:zoom-in-95 sm:data-[state=closed]:zoom-out-95",
+        "sm:data-[state=open]:slide-in-from-top-[48%] sm:data-pstate=closed]:slide-out-to-top-[48%]",
         className
       )}
       {...props}
@@ -50,10 +55,7 @@ const AlertDialogHeader = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn(
-      "flex flex-col space-y-2 text-center sm:text-left",
-      className
-    )}
+    className={cn("flex flex-col space-y-2 text-left", className)}
     {...props}
   />
 )
