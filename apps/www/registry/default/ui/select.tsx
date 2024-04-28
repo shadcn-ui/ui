e27d4caaccdf7866ef9704a19037109314d855fd
Bojan Dragojevic <bdragojevic@gmail.com>
2024-04-28 19:34:35 +0200

    fix: prevent select component to open on scroll for mobile deveices

--- apps/www/registry/default/ui/select.tsx
+++ apps/www/registry/default/ui/select.tsx
@@ -22,6 +22,7 @@ const SelectTrigger = React.forwardRef<
       "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
+    onPointerDown={(e) => e.preventDefault()}
     {...props}
   >
     {children}
--- apps/www/registry/new-york/ui/select.tsx
+++ apps/www/registry/new-york/ui/select.tsx
@@ -27,6 +27,7 @@ const SelectTrigger = React.forwardRef<
       "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
+    onPointerDown={(e) => e.preventDefault()}
     {...props}
   >
     {children}
