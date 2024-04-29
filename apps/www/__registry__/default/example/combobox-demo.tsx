3ba0ba23bfa5937916960dff379f6d8f1f90d0d8
Artur Moisés Alves de Paula <65631470+arturmois@users.noreply.github.com>
2024-04-29 08:47:16 -0300

    Update combobox-demo.tsx

--- apps/www/__registry__/default/example/combobox-demo.tsx
+++ apps/www/__registry__/default/example/combobox-demo.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { Check, ChevronsUpDown } from "lucide-react"
 import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/registry/default/ui/button"
