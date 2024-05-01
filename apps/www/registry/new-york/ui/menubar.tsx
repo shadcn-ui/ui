71f7d3ade94837a1356e62493cb4c0622842eb30
maroKanatani <16130443+maroKanatani@users.noreply.github.com>
2024-05-01 19:18:46 +0900

    refactor: support for extractOptionalPropertyTypes

--- apps/www/registry/new-york/ui/menubar.tsx
+++ apps/www/registry/new-york/ui/menubar.tsx
@@ -148,14 +148,13 @@ MenubarItem.displayName = MenubarPrimitive.Item.displayName
 const MenubarCheckboxItem = React.forwardRef<
   React.ElementRef<typeof MenubarPrimitive.CheckboxItem>,
   React.ComponentPropsWithoutRef<typeof MenubarPrimitive.CheckboxItem>
->(({ className, children, checked, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <MenubarPrimitive.CheckboxItem
     ref={ref}
     className={cn(
       "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
-    checked={checked}
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
