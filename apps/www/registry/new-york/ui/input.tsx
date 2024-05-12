658a57c3fc358fda55bbd2d09b3659b5459481a1
Shane Hoban <shanehoban@gmail.com>
2024-05-12 21:30:13 +0100

    New York style input fix
    This will fix the issue with the input text not centering on the new york style input box.
    
    The `py-1.5` matches the height of the original new york style file input. The `h-auto` is what is needed to fix the issue.

--- apps/www/registry/new-york/ui/input.tsx
+++ apps/www/registry/new-york/ui/input.tsx
@@ -12,7 +12,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          className,
+          type === "file" && "h-auto py-1.5",
         )}
         ref={ref}
         {...props}
