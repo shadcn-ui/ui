63f07860311b87c5846fa94e3ea3cb1b00354fa6
shadcn <m@shadcn.com>
2024-05-13 08:54:47 +0400

    style(command): fix

--- apps/www/registry/default/ui/command.tsx
+++ apps/www/registry/default/ui/command.tsx
@@ -23,7 +23,7 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-interface CommandDialogProps extends DialogProps { }
+interface CommandDialogProps extends DialogProps {}
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
