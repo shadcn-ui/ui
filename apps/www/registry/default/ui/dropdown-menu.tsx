8da564a8be9ce480fd7ae3c0410905f81eb2d305
maroKanatani <16130443+maroKanatani@users.noreply.github.com>
2024-05-22 23:06:54 +0900

    refactor: specify check props by using default value

--- apps/www/registry/default/ui/dropdown-menu.tsx
+++ apps/www/registry/default/ui/dropdown-menu.tsx
@@ -95,13 +95,14 @@ DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName
 const DropdownMenuCheckboxItem = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.CheckboxItem>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.CheckboxItem>
->(({ className, children, ...props }, ref) => (
+>(({ className, children, checked = false, ...props }, ref) => (
   <DropdownMenuPrimitive.CheckboxItem
     ref={ref}
     className={cn(
       "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
+    checked={checked}
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
