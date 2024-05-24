824c23548f6dbd4003387f16da10ed3194b4bbc7
tcortega <tarcisiocaetano2009@gmail.com>
2024-05-24 16:39:07 -0300

    fix(menubar): fixes styling when a MenuSubTrigger is disabled

--- apps/www/registry/new-york/ui/menubar.tsx
+++ apps/www/registry/new-york/ui/menubar.tsx
@@ -59,7 +59,7 @@ const MenubarSubTrigger = React.forwardRef<
   <MenubarPrimitive.SubTrigger
     ref={ref}
     className={cn(
-      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
+      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
       inset && "pl-8",
       className
     )}
