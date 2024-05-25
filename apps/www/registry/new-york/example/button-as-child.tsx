656f8112cec77b5a8510aa6613888de091eea523
Ayberk Kaya <kayaayberk98@gmail.com>
2024-05-25 20:40:32 +0200

    fix(example/button): prevent button asChild from redirecting to 404

--- apps/www/registry/new-york/example/button-as-child.tsx
+++ apps/www/registry/new-york/example/button-as-child.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/registry/new-york/ui/button"
 export default function ButtonAsChild() {
   return (
     <Button asChild>
-      <Link href="/login">Login</Link>
+      <Link href="#">Login</Link>
     </Button>
   )
 }
