f1925b2cd4e3a12723267faa33de8c7c14a49906
tomo-kn <okalemon314@gmail.com>
2024-05-30 22:13:48 +0900

    fix(select): overflow on select portal viewport when select items are too wide(#3575)

--- apps/www/registry/default/ui/select.tsx
+++ apps/www/registry/default/ui/select.tsx
@@ -88,7 +88,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           "p-1",
           position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] max-w-[calc(100vw-32px)]"
         )}
       >
         {children}
