618324a16c4f2ff4b33c16d416c880ad39a21db3
TejasThombare20 <img_2020066@iiitm.ac.in>
2024-06-02 13:51:45 +0530

    fix(command-item): ensure clickability and navigation support

--- apps/www/__registry__/default/ui/command.tsx
+++ apps/www/__registry__/default/ui/command.tsx
@@ -117,7 +117,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground ",
       className
     )}
     {...props}
