d74be6bb60f92078b726aa379cbe612b7038603b
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2024-06-02 13:56:02 -0400

    Merge remote-tracking branch 'upstream/main' into cmdk-bump-and-styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -67,10 +67,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
       },
     },
   },
