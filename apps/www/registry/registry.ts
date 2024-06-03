5df58f126cf89ff68b389b1e8fba95326cb94ae1
Lloyd Richards <59728961+lloydrichards@users.noreply.github.com>
2024-06-03 15:11:23 +0200

    chore: remove update to registry.ts

--- apps/www/registry/registry.ts
+++ apps/www/registry/registry.ts
@@ -3,5 +3,4 @@ import { examples } from "@/registry/examples"
 import { Registry } from "@/registry/schema"
 import { ui } from "@/registry/ui"
 
-
 export const registry: Registry = [...ui, ...examples, ...blocks]
