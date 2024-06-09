549fa77a0bce73be04b6d6535566ba126c93e8f6
Ezequiel Parziale <eparziale@gmail.com>
2024-06-09 17:48:59 -0300

    :bug: fix: add missing popover anchor

--- apps/www/__registry__/default/ui/popover.tsx
+++ apps/www/__registry__/default/ui/popover.tsx
@@ -9,6 +9,8 @@ const Popover = PopoverPrimitive.Root
 
 const PopoverTrigger = PopoverPrimitive.Trigger
 
+const PopoverAnchor = PopoverPrimitive.Anchor
+
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
