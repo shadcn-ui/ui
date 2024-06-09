0692d762feef1c95d0734e9d1662f3fbce9cadb4
Ezequiel Parziale <eparziale@gmail.com>
2024-06-09 17:53:32 -0300

    :bug: fix: add missing export PopoverAnchor

--- apps/www/registry/default/ui/popover.tsx
+++ apps/www/registry/default/ui/popover.tsx
@@ -30,4 +30,4 @@ const PopoverContent = React.forwardRef<
 ))
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
-export { Popover, PopoverTrigger, PopoverContent }
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
