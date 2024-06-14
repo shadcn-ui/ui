ec59a486383bc53d491830af574b25aa387b4683
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2024-06-14 15:52:43 +0330

    refactor(components): remove unnecessary tailwind classes

--- apps/www/registry/default/ui/calendar.tsx
+++ apps/www/registry/default/ui/calendar.tsx
@@ -74,12 +74,12 @@ function Calendar({
             table: "w-full border-collapse space-y-1",
             head_row: "flex",
             head_cell:
-              "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+              "text-muted-foreground rounded-md font-normal text-[0.8rem]",
             row: "flex w-full mt-2",
-            cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+            cell: "text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
             day: cn(
               buttonVariants({ variant: "ghost" }),
-              "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
+              "p-0 font-normal aria-selected:opacity-100"
             ),
             day_range_end: "day-range-end",
             day_selected:
