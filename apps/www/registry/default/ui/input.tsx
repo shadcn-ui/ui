992f4190b4d024165cf24a88c8c6b03c3f8e718b
argcast <103273391+argcast@users.noreply.github.com>
2024-06-24 00:39:01 +0200

    (fix) - @typescript-eslint/no-empty-interface
    An interface declaring no members is equivalent to its supertype.eslint@typescript-eslint/no-empty-interface

--- apps/www/registry/default/ui/input.tsx
+++ apps/www/registry/default/ui/input.tsx
@@ -1,9 +1,8 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "~/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
