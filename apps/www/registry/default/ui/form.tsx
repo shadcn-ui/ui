0b78bcb9189ca6c6bda4b30d46a94ddd9686338c
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2024-06-24 10:04:31 +0900

    fix(registry/form-message): fix error handling in form component
    Consider the case where the type corresponding to name is an array of objects.
    
    Signed-off-by: Ren Adachi <49895682+moeyashi@users.noreply.github.com>

--- apps/www/registry/default/ui/form.tsx
+++ apps/www/registry/default/ui/form.tsx
@@ -145,7 +145,7 @@ const FormMessage = React.forwardRef<
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, children, ...props }, ref) => {
   const { error, formMessageId } = useFormField()
-  const body = error ? String(error?.message) : children
+  const body = error ? String(error?.message || error?.root?.message) : children
 
   if (!body) {
     return null
