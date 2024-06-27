6482d59e61acf33ce38bba478c7f1ad1ba3735cc
Lavdewangan1 <lavkd001@gmail.com>
2024-06-27 17:53:19 +0530

    fix(tabs): changed tablist component display to flex

--- apps/www/__registry__/default/ui/tabs.tsx
+++ apps/www/__registry__/default/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "flex flex-wrap h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
+      "flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
       className
     )}
     {...props}
