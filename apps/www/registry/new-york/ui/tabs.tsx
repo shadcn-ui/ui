430189ceb6bc57751b1e2cce29472e5550e3fd11
Lavdewangan1 <lavkd001@gmail.com>
2024-06-27 17:57:29 +0530

    fix(tabs): changed all tablist component display to flex

--- apps/www/registry/new-york/ui/tabs.tsx
+++ apps/www/registry/new-york/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
+      "flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
       className
     )}
     {...props}
