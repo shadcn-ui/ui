35e84b14459edf0e05aad94c22e3d127169a0057
Bowen <root@bowen.cool>
2024-07-01 19:23:12 +0800

    fix: text colors of dropdown-menu

--- apps/www/registry/new-york/ui/dropdown-menu.tsx
+++ apps/www/registry/new-york/ui/dropdown-menu.tsx
@@ -31,7 +31,7 @@ const DropdownMenuSubTrigger = React.forwardRef<
   <DropdownMenuPrimitive.SubTrigger
     ref={ref}
     className={cn(
-      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent data-[state=open]:bg-accent",
+      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
       inset && "pl-8",
       className
     )}
