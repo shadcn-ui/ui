15acf2e17d07ff4dd74db5fd0b92a0f9882c6ccf
Antonio Galindo <galindosvq@gmail.com>
2024-07-03 17:57:39 +0200

    fix: update ButtonAsChild component to use "#" as the href value for the Link element

--- apps/www/registry/default/example/button-as-child.tsx
+++ apps/www/registry/default/example/button-as-child.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/registry/default/ui/button"
 export default function ButtonAsChild() {
   return (
     <Button asChild>
-      <Link href="/login">Login</Link>
+      <Link href="#as-child">Login</Link>
     </Button>
   )
 }
