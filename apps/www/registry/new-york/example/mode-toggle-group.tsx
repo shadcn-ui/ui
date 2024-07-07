cd911dd4c18bab506e4277ad927e929272508dc2
Parth Parmar <softsparths@gmail.com>
2024-07-07 21:39:21 +0530

    feat: update toggle group component for light and dark mode

--- apps/www/registry/new-york/example/mode-toggle-group.tsx
+++ apps/www/registry/new-york/example/mode-toggle-group.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import { Sun, Moon, Laptop } from "lucide-react"
+import { useEffect, useState } from "react"
 import { useTheme } from "next-themes"
 
 import {
@@ -10,31 +11,42 @@ import {
 } from "@/registry/new-york/ui/toggle-group"
 
 export default function ModeToggleGroup() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
+  const [currentTheme, setCurrentTheme] = useState<string>("");
+
+  useEffect(() => {
+    if (theme === "system") {
+      setCurrentTheme("system");
+    } else if (theme === "dark") {
+      setCurrentTheme("dark");
+    } else if (theme === "light") {
+      setCurrentTheme("light");
+    }
+  }, [theme]);
 
   return (
-    <ToggleGroup type="single" className="rounded-full border p-1">
+    <ToggleGroup type="single" variant="outline" className="scale-90 rounded-full border p-1" value={currentTheme}>
       <ToggleGroupItem
-        value="Light"
-        disabled={theme === "light"}
+        value="light"
         onClick={() => setTheme("light")}
-        className="rounded-full disabled:opacity-100"
+        disabled={theme === "light" ? true : false}
+        className="rounded-full disabled:bg-accent disabled:opacity-100"
       >
         <Sun className="h-5 w-5" />
       </ToggleGroupItem>
       <ToggleGroupItem
-        value="System"
-        disabled={theme === "system"}
+        value="system"
         onClick={() => setTheme("system")}
-        className="rounded-full disabled:opacity-100"
+        disabled={theme === "system" ? true : false}
+        className="rounded-full disabled:bg-accent disabled:opacity-100"
       >
         <Laptop className="h-5 w-5" />
       </ToggleGroupItem>
       <ToggleGroupItem
-        value="Dark"
-        disabled={theme === "dark"}
+        value="dark"
         onClick={() => setTheme("dark")}
-        className="rounded-full disabled:opacity-100"
+        disabled={theme === "dark" ? true : false}
+        className="rounded-full disabled:bg-accent disabled:opacity-100"
       >
         <Moon className="h-5 w-5" />
       </ToggleGroupItem>
