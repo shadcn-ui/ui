f6b4637195289cf39496641f7a6f2fc5ee62605b
Antonio Galindo <galindosvq@gmail.com>
2024-07-10 14:45:24 +0200

    chore: run format:write

--- apps/www/__registry__/default/block/dashboard-07.tsx
+++ apps/www/__registry__/default/block/dashboard-07.tsx
@@ -535,7 +535,8 @@ export default function Dashboard() {
                   </CardContent>
                 </Card>
                 <Card
-                  className="overflow-hidden" x-chunk="dashboard-07-chunk-4"
+                  className="overflow-hidden"
+                  x-chunk="dashboard-07-chunk-4"
                 >
                   <CardHeader>
                     <CardTitle>Product Images</CardTitle>
