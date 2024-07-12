0f7e5dc5b3a3b331df7c6d42e9ceafd7e0c2c73a
Andre Patacas <andre.patacas@morphotech.co.uk>
2024-07-12 19:23:14 +0100

    fix(charts): labels not displaying 0 correctly

--- apps/www/registry/new-york/ui/chart.tsx
+++ apps/www/registry/new-york/ui/chart.tsx
@@ -238,7 +238,7 @@ const ChartTooltipContent = React.forwardRef<
                           {itemConfig?.label || item.name}
                         </span>
                       </div>
-                      {item.value && (
+                      {item.value !== undefined && (
                         <span className="font-mono font-medium tabular-nums text-foreground">
                           {item.value.toLocaleString()}
                         </span>
