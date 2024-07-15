28ceb130a043f3c765227d265759e073e9cf39f7
yeonsubak <yeonsubak@gmail.com>
2024-07-15 19:07:06 +0900

    fix(command): fix breaking changes for cmdk@1.0.0
    - add disabled prop for <CommandPrimitive.Item />
    - set cmdk version to @1.0.0 in "package.json"

--- apps/www/registry/new-york/ui/command.tsx
+++ apps/www/registry/new-york/ui/command.tsx
@@ -113,11 +113,13 @@ CommandSeparator.displayName = CommandPrimitive.Separator.displayName
 const CommandItem = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
->(({ className, ...props }, ref) => (
+>(({ className, disabled, ...props }, ref) => (
   <CommandPrimitive.Item
     ref={ref}
+    disabled={disabled}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground",
+      disabled ? "opacity-50" : '',
       className
     )}
     {...props}
