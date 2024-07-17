8a39ee22c58534392a5239389b47481906fecd22
shadcn <m@shadcn.com>
2024-07-17 11:49:13 +0400

    fix: frame

--- apps/www/components/block-preview.tsx
+++ apps/www/components/block-preview.tsx
@@ -69,6 +69,7 @@ export function BlockPreview({
               onLoad={() => {
                 setIsLoading(false)
               }}
+              allowTransparency
             />
           </ResizablePanel>
           <ResizableHandle
