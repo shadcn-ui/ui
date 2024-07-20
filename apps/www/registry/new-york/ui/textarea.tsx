a1b147b8b6fc38b2a4865cbefebd765b2138dafe
Alecia Vogel <alecia.vogel@playable.video>
2024-07-20 15:34:15 -0600

    fix(components): replace forwardRef in textarea for react v19

--- apps/www/registry/new-york/ui/textarea.tsx
+++ apps/www/registry/new-york/ui/textarea.tsx
@@ -2,22 +2,16 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export type TextareaProps = React.ComponentProps<"textarea">
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <textarea
-        className={cn(
-          "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
+const Textarea: React.FC<TextareaProps> = ({ className, ...props }) => (
+  <textarea
+    className={cn(
+      "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+      className
+    )}
+    {...props}
+  />
 )
 Textarea.displayName = "Textarea"
 
