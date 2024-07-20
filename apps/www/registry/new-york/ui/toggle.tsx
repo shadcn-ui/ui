3825993e767ac0703b1f6ebde4e4639052335bd2
Alecia Vogel <alecia.vogel@playable.video>
2024-07-20 15:39:35 -0600

    fix(components): replace forwardRef in toggle for react v19

--- apps/www/registry/new-york/ui/toggle.tsx
+++ apps/www/registry/new-york/ui/toggle.tsx
@@ -28,17 +28,15 @@ const toggleVariants = cva(
   }
 )
 
-const Toggle = React.forwardRef<
-  React.ElementRef<typeof TogglePrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
+const Toggle: React.FC<
+  React.ComponentProps<typeof TogglePrimitive.Root> &
     VariantProps<typeof toggleVariants>
->(({ className, variant, size, ...props }, ref) => (
+> = ({ className, variant, size, ...props }) => (
   <TogglePrimitive.Root
-    ref={ref}
     className={cn(toggleVariants({ variant, size, className }))}
     {...props}
   />
-))
+)
 
 Toggle.displayName = TogglePrimitive.Root.displayName
 
