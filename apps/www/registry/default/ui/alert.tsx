545c4caa74b52350605a0b5588ecb85e30b00f7c
Alecia Vogel <alecia.vogel@playable.video>
2024-07-20 13:46:08 -0600

    fix(components): replace forwardRef in alert for react v19

--- apps/www/registry/default/ui/alert.tsx
+++ apps/www/registry/default/ui/alert.tsx
@@ -19,41 +19,34 @@ const alertVariants = cva(
   }
 )
 
-const Alert = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
->(({ className, variant, ...props }, ref) => (
+const Alert: React.FC<
+  React.ComponentProps<"div"> & VariantProps<typeof alertVariants>
+> = ({ className, variant, ...props }) => (
   <div
-    ref={ref}
     role="alert"
     className={cn(alertVariants({ variant }), className)}
     {...props}
   />
-))
+)
 Alert.displayName = "Alert"
 
-const AlertTitle = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
+const AlertTitle: React.FC<React.ComponentProps<"h5">> = ({
+  className,
+  ...props
+}) => (
   <h5
-    ref={ref}
     className={cn("mb-1 font-medium leading-none tracking-tight", className)}
     {...props}
   />
-))
+)
 AlertTitle.displayName = "AlertTitle"
 
-const AlertDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("text-sm [&_p]:leading-relaxed", className)}
-    {...props}
-  />
-))
+const AlertDescription: React.FC<React.ComponentProps<"div">> = ({
+  className,
+  ...props
+}) => (
+  <div className={cn("text-sm [&_p]:leading-relaxed", className)} {...props} />
+)
 AlertDescription.displayName = "AlertDescription"
 
 export { Alert, AlertTitle, AlertDescription }
