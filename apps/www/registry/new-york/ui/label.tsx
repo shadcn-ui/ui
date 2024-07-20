95e7ba052234034458afa054406de54db8f2ac4b
Alecia Vogel <alecia.vogel@playable.video>
2024-07-20 14:51:34 -0600

    fix(components): replace forwardRef in label for react v19

--- apps/www/registry/new-york/ui/label.tsx
+++ apps/www/registry/new-york/ui/label.tsx
@@ -10,17 +10,12 @@ const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+const Label: React.FC<
+  React.ComponentProps<typeof LabelPrimitive.Root> &
     VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-))
+> = ({ className, ...props }) => (
+  <LabelPrimitive.Root className={cn(labelVariants(), className)} {...props} />
+)
 Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
