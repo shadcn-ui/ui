a123171a4048d26dbb222cf2a19bc60cf7c09d40
Alecia Vogel <alecia.vogel@playable.video>
2024-07-20 15:29:13 -0600

    fix(components): replace forwardRef in switch for react v19

--- apps/www/registry/default/ui/switch.tsx
+++ apps/www/registry/default/ui/switch.tsx
@@ -5,17 +5,16 @@ import * as SwitchPrimitives from "@radix-ui/react-switch"
 
 import { cn } from "@/lib/utils"
 
-const Switch = React.forwardRef<
-  React.ElementRef<typeof SwitchPrimitives.Root>,
-  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
->(({ className, ...props }, ref) => (
+const Switch: React.FC<React.ComponentProps<typeof SwitchPrimitives.Root>> = ({
+  className,
+  ...props
+}) => (
   <SwitchPrimitives.Root
     className={cn(
       "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
       className
     )}
     {...props}
-    ref={ref}
   >
     <SwitchPrimitives.Thumb
       className={cn(
@@ -23,7 +22,7 @@ const Switch = React.forwardRef<
       )}
     />
   </SwitchPrimitives.Root>
-))
+)
 Switch.displayName = SwitchPrimitives.Root.displayName
 
 export { Switch }
