15a57b574725b34ac9a9e9cb72d6b4ef16749bb8
Alecia Vogel <alecia.vogel@playable.video>
2024-07-20 15:07:20 -0600

    fix(components): replace forwardRef in progress for react v19

--- apps/www/registry/default/ui/progress.tsx
+++ apps/www/registry/default/ui/progress.tsx
@@ -5,12 +5,10 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
-const Progress = React.forwardRef<
-  React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+const Progress: React.FC<
+  React.ComponentProps<typeof ProgressPrimitive.Root>
+> = ({ className, value, ...props }) => (
   <ProgressPrimitive.Root
-    ref={ref}
     className={cn(
       "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
       className
@@ -22,7 +20,7 @@ const Progress = React.forwardRef<
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
-))
+)
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
