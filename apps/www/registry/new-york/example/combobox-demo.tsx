f2e33415c6d825bd0e3dc3fe522e4457868f8da9
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2024-08-06 02:42:26 +0500

    fix(www): combobox examples error (#4503)
    * bug-fix: added command-list component to eliminate error
    
    Wrap CommandEmpty and CommandGroup inside CommandList
    Eliminated Error: TypeError: undefined is not iterable (cannot read property Symbol(Symbol.iterator))
    
    * fix(www): command examples
    
    ---------
    
    Co-authored-by: shadcn <m@shadcn.com>

--- apps/www/registry/new-york/example/combobox-demo.tsx
+++ apps/www/registry/new-york/example/combobox-demo.tsx
@@ -64,27 +64,29 @@ export default function ComboboxDemo() {
       <PopoverContent className="w-[200px] p-0">
         <Command>
           <CommandInput placeholder="Search framework..." className="h-9" />
-          <CommandEmpty>No framework found.</CommandEmpty>
-          <CommandGroup>
-            {frameworks.map((framework) => (
-              <CommandItem
-                key={framework.value}
-                value={framework.value}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? "" : currentValue)
-                  setOpen(false)
-                }}
-              >
-                {framework.label}
-                <CheckIcon
-                  className={cn(
-                    "ml-auto h-4 w-4",
-                    value === framework.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-              </CommandItem>
-            ))}
-          </CommandGroup>
+          <CommandList>
+            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandGroup>
+              {frameworks.map((framework) => (
+                <CommandItem
+                  key={framework.value}
+                  value={framework.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue)
+                    setOpen(false)
+                  }}
+                >
+                  {framework.label}
+                  <CheckIcon
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
         </Command>
       </PopoverContent>
     </Popover>
