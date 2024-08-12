5eaea65cc4bf489905092115cb32579b8e1ef92a
shanedownes <shanedownes01@gmail.com>
2024-08-12 22:48:48 +0100

    chore(registry): export cva variants from all ui components

--- apps/www/registry/default/ui/alert.tsx
+++ apps/www/registry/default/ui/alert.tsx
@@ -56,4 +56,4 @@ const AlertDescription = React.forwardRef<
 ))
 AlertDescription.displayName = "AlertDescription"
 
-export { Alert, AlertTitle, AlertDescription }
+export { Alert, AlertTitle, AlertDescription, alertVariants }
