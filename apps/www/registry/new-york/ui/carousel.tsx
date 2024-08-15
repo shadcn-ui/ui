f8dda9a2b8ee436ec3e09c021bdca51dcc5bce0f
Rafael <rafael.najman@hotmail.com>
2024-08-15 22:23:32 +0300

    [FIX] overflow of itens, adding an space to the page in some cases

--- apps/www/registry/new-york/ui/carousel.tsx
+++ apps/www/registry/new-york/ui/carousel.tsx
@@ -137,7 +137,7 @@ const Carousel = React.forwardRef<
         <div
           ref={ref}
           onKeyDownCapture={handleKeyDown}
-          className={cn("relative", className)}
+          className={cn("relative overflow-hidden", className)}
           role="region"
           aria-roledescription="carousel"
           {...props}
