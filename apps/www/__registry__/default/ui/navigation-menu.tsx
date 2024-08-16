ca379e5e79e74676fc9c46d361cf0d1108b68ef0
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-08-16 12:40:44 +0900

    refactor: Remove trailing whitespace in navigation-menu.tsx

--- apps/www/__registry__/default/ui/navigation-menu.tsx
+++ apps/www/__registry__/default/ui/navigation-menu.tsx
@@ -53,7 +53,7 @@ const NavigationMenuTrigger = React.forwardRef<
     className={cn(navigationMenuTriggerStyle(), "group", className)}
     {...props}
   >
-    {children}{" "}
+    {children}
     <ChevronDown
       className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
       aria-hidden="true"
