a639fa3c00aafa4a32fc8d6624cde5fb28a6ced0
suiboli314 <23085379+suiboli314@users.noreply.github.com>
2024-08-19 23:05:21 -0700

    fixed carousel autoPlay in default Theme as well

--- apps/www/registry/default/example/carousel-plugin.tsx
+++ apps/www/registry/default/example/carousel-plugin.tsx
@@ -12,15 +12,18 @@ import {
 
 export default function CarouselPlugin() {
   const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
+    Autoplay({
+      delay: 2000,
+      stopOnInteraction: false,
+      stopOnMouseEnter: true,
+      stopOnFocusIn: false,
+    })
   )
 
   return (
     <Carousel
       plugins={[plugin.current]}
       className="w-full max-w-xs"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
     >
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
