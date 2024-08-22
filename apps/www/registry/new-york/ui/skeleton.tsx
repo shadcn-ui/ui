3bdc856dd5089d7daf8ee0d78de947b29880d95a
viruop <virajrai706@gmail.com>
2024-08-22 15:33:44 +0530

    added skeleton-wrapper

--- apps/www/registry/new-york/ui/skeleton.tsx
+++ apps/www/registry/new-york/ui/skeleton.tsx
@@ -1,15 +1,30 @@
-import { cn } from "@/lib/utils"
+import React from 'react';
+import { cn } from "@/lib/utils";
+
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  loading?: boolean;
+}
 
 function Skeleton({
   className,
+  children,
+  loading = true,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: SkeletonProps) {
+  if (!loading) return children;
+
   return (
-    <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
-      {...props}
-    />
-  )
+    <div className="relative inline-block">
+      <div
+        className={cn(
+          'absolute inset-0 animate-pulse rounded-md bg-secondary',
+          className,
+        )}
+        {...props}
+      />
+      <div className="invisible">{children}</div>
+    </div>
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
