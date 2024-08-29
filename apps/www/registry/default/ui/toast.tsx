1ab40e28a9bbe48e8f7ad9a46a5a25865e8f8e43
dpaulos6 <itzframepvp@outlook.com>
2024-08-29 15:50:37 +0100

    fix: incorrect new-york variant

--- apps/www/registry/default/ui/toast.tsx
+++ apps/www/registry/default/ui/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-        success: "success group border-green-600 bg-green-500 text-white",
+        success: "group border-green-600 bg-green-500 text-white",
       },
     },
     defaultVariants: {
--- apps/www/registry/new-york/ui/toast.tsx
+++ apps/www/registry/new-york/ui/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-        success: "border bg-green-700 text-green-50",
+        success: "group border-green-600 bg-green-500 text-white",
       },
     },
     defaultVariants: {
