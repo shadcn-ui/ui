27344eedd1ae5205a588ee696032a86e8603717c
Ali Issa <ali.issa.lb@gmail.com>
2024-08-31 21:53:56 -0400

    fix(separator): set vertical separator height to auto

--- apps/www/registry/default/example/separator-demo.tsx
+++ apps/www/registry/default/example/separator-demo.tsx
@@ -10,7 +10,7 @@ export default function SeparatorDemo() {
         </p>
       </div>
       <Separator className="my-4" />
-      <div className="flex h-5 items-center space-x-4 text-sm">
+      <div className="flex space-x-4 text-sm">
         <div>Blog</div>
         <Separator orientation="vertical" />
         <div>Docs</div>
