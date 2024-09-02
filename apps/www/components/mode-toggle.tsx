67d7ff133078491a37ca8d59afe6a374a81023db
Rahman <museabdirahman@gmail.com>
2024-09-02 16:29:20 +0100

    fix(components): ensure moon icon is displayed correctly in dark mode

--- apps/www/components/mode-toggle.tsx
+++ apps/www/components/mode-toggle.tsx
@@ -13,18 +13,18 @@ import {
 } from "@/registry/new-york/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme,theme } = useTheme()
+  const { setTheme, theme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="h-8 w-8 px-0">
-        {theme == "dark" ? (
+          {theme == "dark" ? (
             <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           ) : (
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           )}
-        <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
--- apps/www/registry/default/example/mode-toggle.tsx
+++ apps/www/registry/default/example/mode-toggle.tsx
@@ -13,13 +13,13 @@ import {
 } from "@/registry/default/ui/dropdown-menu"
 
 export default function ModeToggle() {
-  const { setTheme,theme } = useTheme()
+  const { setTheme, theme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-        {theme == "dark" ? (
+          {theme == "dark" ? (
             <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           ) : (
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
