0cdecdfced05ce0abe89e044a4ba9ee72eff0590
Rahman <museabdirahman@gmail.com>
2024-09-02 16:04:19 +0100

    fixed issue with dark theme icon not showing during toggling on dark mode-toggle.

--- apps/www/components/mode-toggle.tsx
+++ apps/www/components/mode-toggle.tsx
@@ -13,14 +13,17 @@ import {
 } from "@/registry/new-york/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme,theme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="h-8 w-8 px-0">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        {theme == "dark" ? (
+            <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          ) : (
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
