6ab9cf4be3329c0ded8512c3e1e5dde270caa315
Chetan <chetanbhosale810@gmail.com>
2024-09-04 13:09:13 +0530

    feature:#4739

--- apps/www/registry/new-york/ui/toast.tsx
+++ apps/www/registry/new-york/ui/toast.tsx
@@ -32,6 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+        success: "border-green-500 bg-green-500 text-white", 
       },
     },
     defaultVariants: {
