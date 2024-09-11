399387f7e2c14b6648df03e13782f6bdef6265c5
chinazaaa <obiekweagnesmary@gmail.com>
2024-09-11 17:19:16 +0100

    fix: added error boundary to the chart

---
+++ apps/www/app/(app)/charts/error.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
