a2fd579dd159f303383d82ca06b6e9050b2cbcbe
Gulshan Prajapati <gulshanprajapati436@gmail.com>
2024-09-13 21:40:55 +0530

    Solve #4834 by adding className for both

--- apps/www/registry/default/ui/command.tsx
+++ apps/www/registry/default/ui/command.tsx
@@ -37,17 +37,24 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   )
 }
 
+interface CommandInputProps
+  extends React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input> {
+  inputClassName?: string
+}
 const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
->(({ className, ...props }, ref) => (
-  <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
+  CommandInputProps
+>(({ className, inputClassName, ...props }, ref) => (
+  <div
+    className={cn("flex items-center border-b px-3", className)}
+    cmdk-input-wrapper=""
+  >
     <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
         "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
-        className
+        inputClassName
       )}
       {...props}
     />
