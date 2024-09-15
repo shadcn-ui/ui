f8aef7f23983417bb53584cd98cb1e0c71ef748f
Mohamed Alnahhas <alnahhas55@gmail.com>
2024-09-15 23:45:14 +0200

    feat(carousel): improve rtl support

--- apps/www/registry/new-york/ui/carousel.tsx
+++ apps/www/registry/new-york/ui/carousel.tsx
@@ -167,7 +167,7 @@ const CarouselContent = React.forwardRef<
         ref={ref}
         className={cn(
           "flex",
-          orientation === "horizontal" ? "-ml-4" : "-mt-4 flex-col",
+          orientation === "horizontal" ? "-ms-4" : "-mt-4 flex-col",
           className
         )}
         {...props}
@@ -190,7 +190,7 @@ const CarouselItem = React.forwardRef<
       aria-roledescription="slide"
       className={cn(
         "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "pl-4" : "pt-4",
+        orientation === "horizontal" ? "ps-4" : "pt-4",
         className
       )}
       {...props}
