ebdaa2a84a69b426a838051d24c58e66b515bc16
Hichem Fantar <hichemfantar2049@gmail.com>
2024-09-15 03:18:24 +0100

    update registry

--- apps/www/__registry__/new-york/block/dashboard-01.tsx
+++ apps/www/__registry__/new-york/block/dashboard-01.tsx
@@ -229,7 +229,9 @@ export default function Dashboard() {
           </Card>
         </div>
         <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-          <Card className="xl:col-span-2" x-chunk="dashboard-01-chunk-4">
+          <Card
+            className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
+          >
             <CardHeader className="flex flex-row items-center">
               <div className="grid gap-2">
                 <CardTitle>Transactions</CardTitle>
