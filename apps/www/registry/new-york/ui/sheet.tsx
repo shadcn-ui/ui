c3943f51f5e8c0110fea0ac5317be62d413eef67
Hichem Fantar <hichemfantar2049@gmail.com>
2024-09-15 14:33:43 +0100

    Refactor sheet footer to use "gap-2" instead of "sm:space-x-2" for spacing.

--- apps/www/registry/new-york/ui/sheet.tsx
+++ apps/www/registry/new-york/ui/sheet.tsx
@@ -94,7 +94,7 @@ const SheetFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
       className
     )}
     {...props}
