ef749631daec0136d5f5e81cbb2c1845ec03b729
Mohamed Alnahhas <alnahhas55@gmail.com>
2024-09-15 23:25:46 +0200

    feat(pagination): add rtl support

--- apps/www/registry/new-york/ui/pagination.tsx
+++ apps/www/registry/new-york/ui/pagination.tsx
@@ -70,10 +70,10 @@ const PaginationPrevious = ({
   <PaginationLink
     aria-label="Go to previous page"
     size="default"
-    className={cn("gap-1 pl-2.5", className)}
+    className={cn("gap-1 ps-2.5", className)}
     {...props}
   >
-    <ChevronLeftIcon className="h-4 w-4" />
+    <ChevronLeftIcon className="h-4 w-4 rtl:rotate-180" />
     <span>Previous</span>
   </PaginationLink>
 )
@@ -86,11 +86,11 @@ const PaginationNext = ({
   <PaginationLink
     aria-label="Go to next page"
     size="default"
-    className={cn("gap-1 pr-2.5", className)}
+    className={cn("gap-1 pe-2.5", className)}
     {...props}
   >
     <span>Next</span>
-    <ChevronRightIcon className="h-4 w-4" />
+    <ChevronRightIcon className="h-4 w-4 rtl:rotate-180" />
   </PaginationLink>
 )
 PaginationNext.displayName = "PaginationNext"
