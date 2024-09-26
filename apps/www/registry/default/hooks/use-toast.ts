045aaee6749115bba13257162df480c8a22fdad4
Parth Parmar <softsparths@gmail.com>
2024-09-27 00:20:26 +0530

    fix: 'actionTypes' is assigned a value but only used as a type. @typescript-eslint/no-unused-vars

--- apps/www/registry/default/hooks/use-toast.ts
+++ apps/www/registry/default/hooks/use-toast.ts
@@ -18,6 +18,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
