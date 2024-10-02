a02c6f21844532c61375964b00e1df1349da88df
Kerry McCrory <92750962+kmccrory-grx@users.noreply.github.com>
2024-10-02 10:17:42 -0500

    Update dropdown-menu.tsx
    the content includes the portal component, however we do not have access to the portal props, including `container`.  if we want to portal our main menu to another element, we can not.  
    
    i think if the container includes the portal component, the portal props should be exposed, at least the container.  
    
    as it stands currently, shad exports the menuContent that includes the portal, as well as the menuportal component itself.  it also points users to the radix documentation for proper use of the api.  radix says to wrap your element in a portal component and provide the container prop.  
    
    the confusing situation that i found myself in, was i tried wraping the menucontent with the menuportal that was provided.  this did not work and it took me a very long time to figure out that content was already wrapped in a portal. 
    
    my specific use case for this change is on our doc site, all of our code exists within an element with a specific id and our tailwind classes are prefixed with that ID, so we need to portal the dropdown to exist within that element with that specific id.

--- apps/www/registry/default/ui/dropdown-menu.tsx
+++ apps/www/registry/default/ui/dropdown-menu.tsx
@@ -58,21 +58,20 @@ DropdownMenuSubContent.displayName =
 
 const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
-  <DropdownMenuPrimitive.Portal>
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content> & {
+    container?: Element | null | undefined;
+  }
+>(({ className, sideOffset = 8, container, ...props }, ref) => (
+  <DropdownMenuPrimitive.Portal {...(container && { container })}>
     <DropdownMenuPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
-      className={cn(
-        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-        className
-      )}
+      className={cn(DropdownMenuContentClasses(), className)}
       {...props}
     />
   </DropdownMenuPrimitive.Portal>
-))
-DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
+));
+DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName;
 
 const DropdownMenuItem = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Item>,
