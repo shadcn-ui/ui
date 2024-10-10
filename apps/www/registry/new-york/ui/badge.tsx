9e9f59a508f1175f724c682fc4364c5a36cc1cc3
Khánh Hoàng <hi@khanh.id>
2024-10-10 21:03:10 +0700

    Add ref to Badge
    Sometime I need to show some tooltips with the badge, so we have this PR

--- apps/www/registry/new-york/ui/badge.tsx
+++ apps/www/registry/new-york/ui/badge.tsx
@@ -23,14 +23,15 @@ const badgeVariants = cva(
   }
 )
 
-export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
-
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
-}
+const Badge = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof badgeVariants>
+>(({ className, variant, ...props }, ref) => (
+  <div
+    className={cn(badgeVariants({ variant }), className)}
+    ref={ref}
+    {...props}
+  />
+))
 
 export { Badge, badgeVariants }
