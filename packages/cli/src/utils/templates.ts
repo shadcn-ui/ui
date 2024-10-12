78b14f0b3e39833f2814860504ce09d752a229f9
halafy-ds <66537087+halafy-ds@users.noreply.github.com>
2024-10-12 19:10:52 +0200

    Update templates.ts to fix boolean issue

--- packages/cli/src/utils/templates.ts
+++ packages/cli/src/utils/templates.ts
@@ -26,7 +26,7 @@ module.exports = {
   prefix: "<%- prefix %>",
   theme: {
     container: {
-      center: true,
+      center: ${true},
       padding: "2rem",
       screens: {
         "2xl": "1400px",
