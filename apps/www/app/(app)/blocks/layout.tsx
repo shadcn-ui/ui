9ec433838f16d8f2c4a7e8c0ead94ea07f44f6fb
shadcn <m@shadcn.com>
2024-10-17 01:11:00 +0400

    fix: lint

--- apps/www/app/(app)/blocks/layout.tsx
+++ apps/www/app/(app)/blocks/layout.tsx
@@ -43,7 +43,7 @@ export default function BlocksLayout({
         </PageActions>
       </PageHeader>
       <section id="blocks" className="scroll-mt-24">
-        <div className="text-sm text-center px-4 py-8 md:hidden text-red-600">
+        <div className="px-4 py-8 text-center text-sm text-red-600 md:hidden">
           Note: The blocks preview does not work on mobile yet.
         </div>
         {children}
