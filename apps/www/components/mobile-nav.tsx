d49c99e5ffec72f3eab1844c602a68b596510e5c
Suvankar Mahato <suvankar_m@pursuitsoftware.com>
2024-10-17 19:17:28 +0530

    Fixed:Sidebar has extra space at bottom when framed

--- apps/www/components/mobile-nav.tsx
+++ apps/www/components/mobile-nav.tsx
@@ -64,7 +64,7 @@ export function MobileNav() {
           <Icons.logo className="mr-2 h-4 w-4" />
           <span className="font-bold">{siteConfig.name}</span>
         </MobileLink>
-        <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
+        <ScrollArea className="my-4 h-[calc(100vh-3rem)] pb-10 pl-6">
           <div className="flex flex-col space-y-3">
             {docsConfig.mainNav?.map(
               (item) =>
