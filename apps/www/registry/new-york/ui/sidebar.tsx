3259fb7ca165b0ae11124257a20b71001049ecca
shadcn <m@shadcn.com>
2024-10-19 01:28:07 +0400

    fix: overflow on icon

--- apps/www/registry/new-york/ui/sidebar.tsx
+++ apps/www/registry/new-york/ui/sidebar.tsx
@@ -405,7 +405,7 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
         className
       )}
       {...props}
