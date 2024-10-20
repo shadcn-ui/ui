8ab537618249dc8cd68c1d5e98e24561c7003e40
quorafind <951011105@qq.com>
2024-10-20 10:43:33 +0800

    fix: style issue related to team switcher in sidebar component

--- apps/www/registry/default/block/sidebar-07/components/team-switcher.tsx
+++ apps/www/registry/default/block/sidebar-07/components/team-switcher.tsx
@@ -40,7 +40,7 @@ export function TeamSwitcher({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+              <div className="flex aspect-square size-8 shrink-0 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                 <activeTeam.logo className="size-4" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
--- apps/www/registry/new-york/v0/sidebar-07.tsx
+++ apps/www/registry/new-york/v0/sidebar-07.tsx
@@ -224,7 +224,7 @@ export default function Page() {
                     size="lg"
                     className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                   >
-                    <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                    <div className="flex aspect-square size-8 shrink-0 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                       <activeTeam.logo className="size-4" />
                     </div>
                     <div className="grid flex-1 text-left text-sm leading-tight">
