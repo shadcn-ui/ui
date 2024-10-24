e75b8ae2cce9487f8c028796c4be1381591f340c
Medaillek <95227250+Medaillek@users.noreply.github.com>
2024-10-24 13:19:21 +0200

    ADD sheet title to avoid getting error when using SheetContent without Title

--- apps/www/registry/default/ui/sidebar.tsx
+++ apps/www/registry/default/ui/sidebar.tsx
@@ -207,6 +207,7 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <SheetTitle className="sr-only">Menu</SheetTitle>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
