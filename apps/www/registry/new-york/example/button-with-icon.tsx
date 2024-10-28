86a212f596ed1d9541c39ee94f8bb81085ff4ce8
shadcn <m@shadcn.com>
2024-10-28 12:49:36 +0400

    fix: mail open

--- apps/www/registry/new-york/example/button-with-icon.tsx
+++ apps/www/registry/new-york/example/button-with-icon.tsx
@@ -1,11 +1,11 @@
-import { Mail } from "lucide-react"
+import { MailOpen } from "lucide-react"
 
 import { Button } from "@/registry/new-york/ui/button"
 
 export default function ButtonWithIcon() {
   return (
     <Button>
-      <Mail /> Login with Email
+      <MailOpen /> Login with Email
     </Button>
   )
 }
