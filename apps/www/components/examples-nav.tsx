c9db07801b6a706040d5b861a976d614e26e07a4
Nitish Mahawar <nitishmahawar.dev@gmail.com>
2024-11-01 14:59:26 +0530

    examples tab always active issue fixed

--- apps/www/components/examples-nav.tsx
+++ apps/www/components/examples-nav.tsx
@@ -70,8 +70,8 @@ export function ExamplesNav({ className, ...props }: ExamplesNavProps) {
               key={example.href}
               className={cn(
                 "flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary",
-                pathname?.startsWith(example.href) ||
-                  (index === 0 && pathname === "/")
+                pathname === example.href ||
+                  (index === 0 && pathname === "/" && example.href === "/")
                   ? "bg-muted font-medium text-primary"
                   : "text-muted-foreground"
               )}
