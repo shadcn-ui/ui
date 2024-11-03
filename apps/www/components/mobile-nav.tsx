5fed29cf5b71519234b205b7e99dd5cae4af65f2
manfromexistence04 Emon <manfromexistence04@gmail.com>
2024-11-03 13:33:25 +0000

    fix: svg props casing to camelCase

--- apps/www/components/mobile-nav.tsx
+++ apps/www/components/mobile-nav.tsx
@@ -27,13 +27,13 @@ export function MobileNav() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="size-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.75 9h16.5m-16.5 6.75h16.5"
             />
           </svg>
