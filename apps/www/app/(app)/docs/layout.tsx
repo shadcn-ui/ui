2f0868fed046f36039c8dc0bdb83de34ebb6dfd6
shadcn <m@shadcn.com>
2024-11-04 15:03:25 +0400

    fix: scrollbars

--- apps/www/app/(app)/docs/layout.tsx
+++ apps/www/app/(app)/docs/layout.tsx
@@ -10,10 +10,10 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
     <div className="border-b border-border/40 dark:border-border">
       <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-        <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
-          <ScrollArea className="h-full py-6 pr-6 lg:py-8">
+        <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block border-r border-border/40 dark:border-border">
+          <div className="h-full py-6 px-6 lg:py-7 overflow-auto no-scrollbar">
             <DocsSidebarNav config={docsConfig} />
-          </ScrollArea>
+          </div>
         </aside>
         {children}
       </div>
