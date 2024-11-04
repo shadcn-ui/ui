e0f860377aad93686312988de9fa9aeda19f3be6
Ali Hashemi <programmer.ice@gmail.com>
2024-11-04 09:45:44 -0400

    chore: add jsdoc comments

--- packages/shadcn/src/utils/package-manager-commands.ts
+++ packages/shadcn/src/utils/package-manager-commands.ts
@@ -10,6 +10,13 @@ export interface PackageManagerOptions {
   cwd: string;
 }
 
+/**
+ * Generates command arguments for specified package manager for installing dependencies
+ * arguments then will be used to construct and execute a command to install dependencies using specified package manager
+ * @param {PackageManager} packageManager - package manager name (npm , yarn etc)
+ * @param {string[]} dependencies - list of npm packages to be installed
+ * @returns returns a string[] with command arguments for installing the dependencies using the specified package manager
+ */
 function dependencyCommand(packageManager: PackageManager, dependencies: string[]) {
   const commands: string[] = [];
   switch (packageManager) {
@@ -33,6 +40,13 @@ function dependencyCommand(packageManager: PackageManager, dependencies: string[
   return commands;
 }
 
+/**
+ * Generates command arguments for specified package manager for installing devDependencies
+ * arguments then will be used to construct and execute a command to install devDependencies using specified package manager
+ * @param {PackageManager} packageManager - package manager name (npm , yarn etc)
+ * @param {string[]} devDependencies - list of npm packages to be installed as devDependencies
+ * @returns returns a string[] with command arguments for installing the dependencies using the specified package manager
+ */
 function devDependencyCommand(packageManager: PackageManager, devDependencies: string[]) {
   const commands = dependencyCommand(packageManager, devDependencies);
 
@@ -50,6 +64,12 @@ function devDependencyCommand(packageManager: PackageManager, devDependencies: s
   return commands;
 }
 
+/**
+ * Installs npm package dependencies and devDependencies using the specifies package manager
+ * @param {Dependencies} dependencies - on object that contains list of dependencies and devDependencies to be installed
+ * @param {PackageManagerOptions} options - to specify cwd for running the installation command
+ * @returns
+ */
 export async function installDependencies(dependencies: Dependencies, options:PackageManagerOptions) {
 
   if ((!dependencies.dependencies || dependencies.dependencies.length === 0) &&
