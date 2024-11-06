8a1d1aa74da121a07b682ce37b7396d4695b56f8
shadcn <m@shadcn.com>
2024-11-06 16:33:32 +0400

    feat(www): update layout

--- apps/www/components/announcement.tsx
+++ apps/www/components/announcement.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link"
-import { ArrowRight, PanelLeft } from "lucide-react"
-
-import { Separator } from "@/registry/new-york/ui/separator"
+import { ArrowRight } from "lucide-react"
 
 export function Announcement() {
   return (
     <Link
       href="/docs/components/sidebar"
-      className="group inline-flex items-center px-0.5 text-sm font-medium"
+      className="group mb-2 inline-flex items-center px-0.5 text-sm font-medium"
     >
-      <PanelLeft className="h-4 w-4" />{" "}
-      <Separator className="mx-2 h-4" orientation="vertical" />{" "}
       <span className="underline-offset-4 group-hover:underline">
         New sidebar component
       </span>
