2027cbd0c87468c7a8ae547e5b182846f6682976
guesung <gueit214@naver.com>
2024-11-21 11:39:38 +0900

    feat: support when orientation is vertical

--- apps/www/registry/default/ui/carousel.tsx
+++ apps/www/registry/default/ui/carousel.tsx
@@ -87,12 +87,22 @@ const Carousel = React.forwardRef<
 
     const handleKeyDown = React.useCallback(
       (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === "ArrowLeft") {
-          event.preventDefault()
-          scrollPrev()
-        } else if (event.key === "ArrowRight") {
-          event.preventDefault()
-          scrollNext()
+        if (orientation === "horizontal") {
+          if (event.key === "ArrowLeft") {
+            event.preventDefault()
+            scrollPrev()
+          } else if (event.key === "ArrowRight") {
+            event.preventDefault()
+            scrollNext()
+          }
+        } else if (orientation === "vertical") {
+          if (event.key === "ArrowUp") {
+            event.preventDefault()
+            scrollPrev()
+          } else if (event.key === "ArrowDown") {
+            event.preventDefault()
+            scrollNext()
+          }
         }
       },
       [scrollPrev, scrollNext]
@@ -116,6 +126,7 @@ const Carousel = React.forwardRef<
       api.on("select", onSelect)
 
       return () => {
+        api?.off("reInit", onSelect)
         api?.off("select", onSelect)
       }
     }, [api, onSelect])
