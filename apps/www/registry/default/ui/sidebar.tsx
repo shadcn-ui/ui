444857817e2f628a88984e436729c6efefbe07f5
te-ing <hi2177@naver.com>
2024-11-24 22:45:53 +0900

    feat(sidebar): Flip the icon in the right sidebar

--- apps/www/registry/default/ui/sidebar.tsx
+++ apps/www/registry/default/ui/sidebar.tsx
@@ -278,7 +278,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      <PanelLeft className="group-data-[side=right]:-scale-x-100" />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
