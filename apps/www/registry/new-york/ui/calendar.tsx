02b0659b4ec0a0d75c5fb43b99f11d97ff7c76bb
Leonard Austin <leonardaustin@users.noreply.github.com>
2024-11-26 17:40:55 +0000

    Update calendar.tsx

--- apps/www/registry/new-york/ui/calendar.tsx
+++ apps/www/registry/new-york/ui/calendar.tsx
@@ -60,8 +60,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" {...props} />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />,
       }}
       {...props}
     />
