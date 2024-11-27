14ba98c4bff2b2bf59e9485e349e3333bae79ad3
jjeem <alkheder.jad@gmail.com>
2024-11-27 22:10:30 +0300

    feat(input-otp): add rtl support

--- apps/www/registry/new-york/ui/input-otp.tsx
+++ apps/www/registry/new-york/ui/input-otp.tsx
@@ -16,7 +16,7 @@ const InputOTP = React.forwardRef<
       "flex items-center gap-2 has-[:disabled]:opacity-50",
       containerClassName
     )}
-    className={cn("disabled:cursor-not-allowed", className)}
+    className={cn("disabled:cursor-not-allowed rtl:-mr-[40px]", className)}
     {...props}
   />
 ))
@@ -41,7 +41,7 @@ const InputOTPSlot = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        "relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
+        "relative flex h-9 w-9 items-center justify-center border-y border-e border-input text-sm shadow-sm transition-all first:rounded-s-md first:border-s last:rounded-e-md",
         isActive && "z-10 ring-1 ring-ring",
         className
       )}
