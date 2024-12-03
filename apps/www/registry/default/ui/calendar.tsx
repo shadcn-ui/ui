7d9b19f8e63c517cea03777881776dbc1eb79ed1
shiqocred <90097870+shiqocred@users.noreply.github.com>
2024-12-03 21:40:38 +0700

    Update calendar.tsx
    update calendar custom components to following react-day-picker updated version

--- apps/www/registry/default/ui/calendar.tsx
+++ apps/www/registry/default/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        PreviousMonthButton: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
+        NextMonthButton: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
       }}
       {...props}
     />
