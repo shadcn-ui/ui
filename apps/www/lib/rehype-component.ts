2f869a2590e90f79890e6fa00cbfcbf8a241b034
shadcn <m@shadcn.com>
2024-12-14 19:13:06 +0400

    feat(www): code blocks package manager (#6075)
    * feat(www): code blocks
    
    * fix: code style

--- apps/www/lib/rehype-component.ts
+++ apps/www/lib/rehype-component.ts
@@ -37,11 +37,14 @@ export function rehypeComponent() {
             } else {
               const component = Index[style.name][name]
               src = fileName
-                ? component.files.find((file: string) => {
-                    return (
-                      file.endsWith(`${fileName}.tsx`) ||
-                      file.endsWith(`${fileName}.ts`)
-                    )
+                ? component.files.find((file: unknown) => {
+                    if (typeof file === "string") {
+                      return (
+                        file.endsWith(`${fileName}.tsx`) ||
+                        file.endsWith(`${fileName}.ts`)
+                      )
+                    }
+                    return false
                   }) || component.files[0]?.path
                 : component.files[0]?.path
             }
