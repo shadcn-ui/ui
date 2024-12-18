117e52f24a4655e028c5cf338d74261c20434030
shadcn <m@shadcn.com>
2024-12-18 14:32:41 +0400

    fix: eslint rules

--- templates/monorepo-next/packages/eslint-config/next.js
+++ templates/monorepo-next/packages/eslint-config/next.js
@@ -45,6 +45,7 @@ export const nextJsConfig = [
       ...pluginReactHooks.configs.recommended.rules,
       // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
 ]
