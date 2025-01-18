995193f30a6cac3f01a2c63b2807089c3268358f
nevsky118 <nevsky118@gmail.com>
2025-01-18 22:06:16 +0300

    feat(blocks): footer-01

---
+++ apps/www/__registry__/default/blocks/footer-01/page.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "@/registry/default/blocks/footer-01/components/footer"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-end">
+      <div className="w-full">
+        <Footer />
+      </div>
+    </div>
+  )
+}
---
+++ apps/www/registry/new-york/blocks/footer-01/page.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "@/registry/new-york/blocks/footer-01/components/footer"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-end">
+      <div className="w-full">
+        <Footer />
+      </div>
+    </div>
+  )
+}
