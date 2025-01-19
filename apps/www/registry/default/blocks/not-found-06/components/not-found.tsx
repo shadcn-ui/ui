6a1f5b267ce97a211c0936cf48dcad179073caa6
nevsky118 <nevsky118@gmail.com>
2025-01-19 21:55:51 +0300

    feat(blocks): not-found-06

---
+++ apps/www/registry/default/blocks/not-found-06/components/not-found.tsx
@@ -0,0 +1,26 @@
+import { ChevronRight, Search } from "lucide-react"
+
+import { Button } from "@/registry/default/ui/button"
+import { Input } from "@/registry/default/ui/input"
+
+export function NotFound() {
+  return (
+    <div className="text-center">
+      <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-primary sm:text-7xl max-w-4xl">
+        The page you're looking for can't be found.
+      </h1>
+      <div className="mt-14 flex flex-col items-center justify-center gap-y-4 max-w-md mx-auto">
+        <div className="relative w-full">
+          <Search className="absolute left-2 top-4 h-4 w-4 text-muted-foreground" />
+          <Input placeholder="Search example.com" className="pl-8 h-12" />
+        </div>
+        <Button variant="link" className="gap-x-0.5 text-base" asChild>
+          <a href="#">
+            Or see our site map
+            <ChevronRight />
+          </a>
+        </Button>
+      </div>
+    </div>
+  )
+}
