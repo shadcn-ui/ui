dee50ff11cfb82f6ab8176b58e35081a44f2d261
javidev <javier.developer0@gmail.com>
2025-01-21 20:12:06 +0100

    fix: update fillStars function to use className for styling StarHalf component

--- apps/www/registry/new-york/blocks/profile-01/lib/fill-stars.tsx
+++ apps/www/registry/new-york/blocks/profile-01/lib/fill-stars.tsx
@@ -14,7 +14,7 @@ export const fillStars = (rating: number): JSX.Element[] => {
     } else if (index < rating && rating - index >= 0.5) {
       return (
         <span key={index} className="relative">
-          <StarHalf size={16} fill="white" strokeWidth={0} />
+          <StarHalf size={16} className="fill-foreground" strokeWidth={0} />
           <Star
             size={16}
             strokeWidth={0.5}
