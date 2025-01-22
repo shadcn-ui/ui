a6f2d31ef8d58164e6ca55d377555fd444e40746
Nick Petalas <webmasternikos@gmail.com>
2025-01-22 21:16:46 +0000

    fix(shadcn): fixes form error color to use text-destructive-foreground

--- apps/www/registry/new-york/ui/form.tsx
+++ apps/www/registry/new-york/ui/form.tsx
@@ -95,7 +95,7 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && "text-destructive", className)}
+      className={cn(error && "text-destructive-foreground", className)}
       htmlFor={formItemId}
       {...props}
     />
