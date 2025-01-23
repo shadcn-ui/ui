a2b5748495256a273faeb81d24c8584737cfa6a8
nevsky118 <nevsky118@gmail.com>
2025-01-23 18:02:24 +0300

    feat(blocks): faq-04

---
+++ apps/www/__registry__/default/blocks/faq-04/page.tsx
@@ -0,0 +1,9 @@
+import { FAQ } from "@/registry/default/blocks/faq-04/components/faq"
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh flex-col">
+      <FAQ />
+    </div>
+  )
+}
