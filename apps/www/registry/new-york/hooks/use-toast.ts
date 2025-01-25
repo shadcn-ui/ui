31fd8ad28f5b314dc65cb6f2405198a1e95786d1
SainathPoojary <spoojary614@gmail.com>
2025-01-25 22:09:09 +0530

    fix(toast): refactor 'actionTypes' to resolve ESLint no-unused-vars error

--- apps/www/registry/new-york/hooks/use-toast.ts
+++ apps/www/registry/new-york/hooks/use-toast.ts
@@ -18,12 +18,12 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-const actionTypes = {
+type ActionType = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
   REMOVE_TOAST: "REMOVE_TOAST",
-} as const
+};
 
 let count = 0
 
@@ -31,8 +31,6 @@ function genId() {
   count = (count + 1) % Number.MAX_SAFE_INTEGER
   return count.toString()
 }
-
-type ActionType = typeof actionTypes
 
 type Action =
   | {
