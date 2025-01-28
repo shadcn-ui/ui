248369fccd2b3c39768ed4fb897265f2ab0422b5
Ehsan Hosseini <53467610+e-hosseini@users.noreply.github.com>
2025-01-28 12:35:52 +0100

    Avatar should keep the image ratio and just cover the container

--- apps/www/registry/default/ui/avatar.tsx
+++ apps/www/registry/default/ui/avatar.tsx
@@ -26,7 +26,7 @@ const AvatarImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    className={cn("aspect-square h-full w-full object-cover", className)}
     {...props}
   />
 ))
