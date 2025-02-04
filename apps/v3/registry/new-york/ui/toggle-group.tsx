52cab1c1e6d8305a251fa91a88b93bc1f3780ee0
shadcn <m@shadcn.com>
2025-02-04 12:17:18 +0400

    fix: shadow

--- apps/v3/registry/new-york/ui/toggle-group.tsx
+++ apps/v3/registry/new-york/ui/toggle-group.tsx
@@ -60,7 +60,7 @@ function ToggleGroupItem({
           variant: context.variant || variant,
           size: context.size || size,
         }),
-        "min-w-0 shrink-0 rounded-none first:rounded-l-md last:rounded-r-md focus:z-10 focus-visible:z-10 data-[variant=outline]:border-l-0 data-[variant=outline]:first:border-l",
+        "min-w-0 shrink-0 rounded-none shadow-none first:rounded-l-md last:rounded-r-md focus:z-10 focus-visible:z-10 data-[variant=outline]:border-l-0 data-[variant=outline]:first:border-l",
         className
       )}
       {...props}
