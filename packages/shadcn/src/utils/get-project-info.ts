1a1fc9156bb1f181296a2788575769eb3c4fde7a
Prateek (प्रतीक) <me@prateek.page>
2025-02-07 11:28:21 +0530

    fix(tanstack): check for @tanstack/start in devDependencies as well

--- packages/shadcn/src/utils/get-project-info.ts
+++ packages/shadcn/src/utils/get-project-info.ts
@@ -119,7 +119,7 @@ export async function getProjectInfo(cwd: string): Promise<ProjectInfo | null> {
   // TanStack Start.
   if (
     configFiles.find((file) => file.startsWith("app.config."))?.length &&
-    Object.keys(packageJson?.dependencies ?? {}).find((dep) =>
+    [...Object.keys(packageJson?.dependencies ?? {}), ...Object.keys(packageJson?.devDependencies ?? {})].find((dep) =>
       dep.startsWith("@tanstack/start")
     )
   ) {
