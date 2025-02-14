f22a483d0bd386d48ba5f0e9506c078086bd9bb3
Gus Cost <guscost@gmail.com>
2025-02-14 15:53:06 +0000

    Fix useIsMobile media query to have consistent behavior

--- apps/www/registry/default/hooks/use-mobile.tsx
+++ apps/www/registry/default/hooks/use-mobile.tsx
@@ -7,11 +7,11 @@ export function useIsMobile() {
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    const onChange = (e: MediaQueryListEvent | MediaQueryList) => {
+      setIsMobile(e.matches)
     }
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    onChange(mql)
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
