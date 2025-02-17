34131fc853ae74835b33e7411705e03611a20958
Valentin Gavran <valentin.gavran@icloud.com>
2025-02-17 22:07:32 +0100

    fix(context-menu): make context menu scrollable if height is limited
    Context menu content now becomes scrollable when the browser window height is too small to display the full menu. This improves usability on smaller screens.
    
    Radix also supports the same behavior for other components. I will also submit PRs for the other components separately.
    
    Added the following properties to the Context Menu Content component:
    
    - `max-h-[--radix-context-menu-content-available-height]`
    - `overflow-y-auto` & `overflow-x-hidden` instead of `overflow-hidden` (existed before)

--- apps/v4/registry/new-york-v4/ui/context-menu.tsx
+++ apps/v4/registry/new-york-v4/ui/context-menu.tsx
@@ -85,7 +85,7 @@ function ContextMenuSubContent({
     <ContextMenuPrimitive.SubContent
       data-slot="context-menu-sub-content"
       className={cn(
-        "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden rounded-md border p-1 shadow-lg",
+        "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 max-h-[--radix-context-menu-content-available-height] min-w-[8rem] overflow-x-hidden overflow-y-auto rounded-md border p-1 shadow-lg",
         className
       )}
       {...props}
