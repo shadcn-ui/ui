7d2499c8035dac2db9b90f7d5dfa17a7ad3f9fd7
shadcn <m@shadcn.com>
2025-02-21 21:08:07 +0400

    fix(v4): dark mode

--- apps/v4/components/login-form.tsx
+++ apps/v4/components/login-form.tsx
@@ -97,7 +97,7 @@ export function LoginForm({
                 fill
                 src={imageUrl}
                 alt="Image"
-                className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                className="absolute inset-0 h-full w-full object-cover"
               />
             )}
           </div>
