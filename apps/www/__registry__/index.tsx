134ffcb7f95e60d027c1990fa9d7ecfa40e0e646
Aquamarine <2418013844@qq.com>
2025-03-03 16:30:40 +0800

    feat: - The time-picker component first come out

--- apps/www/__registry__/index.tsx
+++ apps/www/__registry__/index.tsx
@@ -530,6 +530,21 @@ export const Index: Record<string, any> = {
       source: "",
       meta: undefined,
     },
+    "time-picker": {
+      name: "time-picker",
+      description: "",
+      type: "registry:ui",
+      registryDependencies: ["slider"],
+      files: [{
+        path: "registry/new-york/ui/time-picker.tsx",
+        type: "registry:ui",
+        target: ""
+      }],
+      categories: undefined,
+      component: React.lazy(() => import("@/registry/new-york/ui/time-picker.tsx")),
+      source: "",
+      meta: undefined,
+    },
     "sidebar": {
       name: "sidebar",
       description: "",
@@ -5302,6 +5317,21 @@ export const Index: Record<string, any> = {
       source: "",
       meta: undefined,
     },
+    "time-picker-container-demo": {
+      name: "time-picker-container-demo",
+      description: "",
+      type: "registry:example",
+      registryDependencies: undefined,
+      files: [{
+        path: "registry/new-york/examples/time-picker-container-demo.tsx",
+        type: "registry:example",
+        target: ""
+      }],
+      categories: undefined,
+      component: React.lazy(() => import("@/registry/new-york/examples/time-picker-container-demo.tsx")),
+      source: "",
+      meta: undefined,
+    },
   },  "default": {
     "accordion": {
       name: "accordion",
@@ -5825,6 +5855,21 @@ export const Index: Record<string, any> = {
       }],
       categories: undefined,
       component: React.lazy(() => import("@/registry/default/ui/sheet.tsx")),
+      source: "",
+      meta: undefined,
+    },
+    "time-picker": {
+      name: "time-picker",
+      description: "",
+      type: "registry:ui",
+      registryDependencies: ["slider"],
+      files: [{
+        path: "registry/default/ui/time-picker.tsx",
+        type: "registry:ui",
+        target: ""
+      }],
+      categories: undefined,
+      component: React.lazy(() => import("@/registry/default/ui/time-picker.tsx")),
       source: "",
       meta: undefined,
     },
@@ -10597,6 +10642,21 @@ export const Index: Record<string, any> = {
       }],
       categories: undefined,
       component: React.lazy(() => import("@/registry/default/examples/chart-tooltip-demo.tsx")),
+      source: "",
+      meta: undefined,
+    },
+    "time-picker-container-demo": {
+      name: "time-picker-container-demo",
+      description: "",
+      type: "registry:example",
+      registryDependencies: undefined,
+      files: [{
+        path: "registry/default/examples/time-picker-container-demo.tsx",
+        type: "registry:example",
+        target: ""
+      }],
+      categories: undefined,
+      component: React.lazy(() => import("@/registry/default/examples/time-picker-container-demo.tsx")),
       source: "",
       meta: undefined,
     },
