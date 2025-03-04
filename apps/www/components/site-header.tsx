a7b3dbf1210fc17d6a40866df7ee7c18b33a42fc
shadcn <m@shadcn.com>
2025-03-04 16:51:58 +0400

    feat(www): site updates (#6854)
    * feat(www): site updates
    
    * fix: lint
    
    * docs: add tailwind notice

--- apps/www/components/site-header.tsx
+++ apps/www/components/site-header.tsx
@@ -12,11 +12,11 @@ export function SiteHeader() {
   return (
     <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container-wrapper">
-        <div className="container flex h-14 items-center">
+        <div className="container flex h-14 items-center gap-2 md:gap-4">
           <MainNav />
           <MobileNav />
-          <div className="flex flex-1 items-center justify-between gap-2 md:justify-end">
-            <div className="w-full flex-1 md:w-auto md:flex-none">
+          <div className="ml-auto flex items-center gap-2 md:flex-1 md:justify-end">
+            <div className="hidden w-full flex-1 md:flex md:w-auto md:flex-none">
               <CommandMenu />
             </div>
             <nav className="flex items-center gap-0.5">
