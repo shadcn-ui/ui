57e2d75dbc884cbdbe834c87ed62c478dde99101
shadcn <m@shadcn.com>
2025-03-04 13:46:21 +0400

    fix: chart and input-otp lint errors

--- apps/v4/registry/new-york-v4/ui/input-otp.tsx
+++ apps/v4/registry/new-york-v4/ui/input-otp.tsx
@@ -44,7 +44,7 @@ function InputOTPSlot({
   index: number
 }) {
   const inputOTPContext = React.useContext(OTPInputContext)
-  const { char, hasFakeCaret, isActive } = inputOTPContext.slots[index]
+  const { char, hasFakeCaret, isActive } = inputOTPContext?.slots[index] ?? {}
 
   return (
     <div
