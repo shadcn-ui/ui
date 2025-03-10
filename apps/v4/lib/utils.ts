596189143509bf27b4221e4e04acca360adf5a94
shadcn <m@shadcn.com>
2025-03-10 20:34:28 +0400

    fix

--- apps/v4/lib/utils.ts
+++ apps/v4/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function absoluteUrl(path: string) {
+  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`
+}
