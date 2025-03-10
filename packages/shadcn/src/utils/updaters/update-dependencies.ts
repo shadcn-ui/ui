384c07c8f7c7343cfedd6ccb6ee7975fe5c7ae17
Fathurrohman <fathurrohmanrosyadi@gmail.com>
2025-03-10 23:03:31 +0700

    fix(cli): prevent incorrect URL parsing in React 19 message

--- packages/shadcn/src/utils/updaters/update-dependencies.ts
+++ packages/shadcn/src/utils/updaters/update-dependencies.ts
@@ -34,7 +34,7 @@ export async function updateDependencies(
   if (isUsingReact19(config) && packageManager === "npm") {
     dependenciesSpinner.stopAndPersist()
     logger.warn(
-      "\nIt looks like you are using React 19. \nSome packages may fail to install due to peer dependency issues in npm (see https://ui.shadcn.com/react-19).\n"
+      "\nIt looks like you are using React 19. \nSome packages may fail to install due to peer dependency issues in npm (see https://ui.shadcn.com/react-19 ).\n"
     )
     const confirmation = await prompts([
       {
