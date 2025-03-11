5669d63f2bd8552291983486452eba8370c61577
shadcn <m@shadcn.com>
2025-03-11 07:52:20 +0400

    feat(v4): update skeleton and switch colors

--- apps/v4/registry/new-york-v4/ui/skeleton.tsx
+++ apps/v4/registry/new-york-v4/ui/skeleton.tsx
@@ -4,10 +4,7 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn(
-        "bg-primary/10 dark:bg-primary/20 animate-pulse rounded-md",
-        className
-      )}
+      className={cn("bg-accent animate-pulse rounded-md", className)}
       {...props}
     />
   )
