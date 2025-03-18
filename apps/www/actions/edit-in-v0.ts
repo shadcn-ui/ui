c77ddfc8b7432773deffbfdb569d5923e794d4c2
shadcn <m@shadcn.com>
2025-03-18 13:59:28 +0400

    fix: tailwind config

--- apps/www/actions/edit-in-v0.ts
+++ apps/www/actions/edit-in-v0.ts
@@ -153,6 +153,13 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			},
         sidebar: {
           DEFAULT: "hsl(var(--sidebar-background))",
           foreground: "hsl(var(--sidebar-foreground))",
