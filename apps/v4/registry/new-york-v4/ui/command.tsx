a9f89169abb8420ea6867270945fd35d534b53e1
Andy Hwang <andy.hwang@mycase.com>
2025-04-02 09:33:32 -0700

    feat(v4 command): update top padding on cmdk-groups
    Currently, pt-0 is applied to subsequent command group siblings in
    CommandDialog. This makes sense when one command group immediately
    proceeds another, maintaining even spacing between each group.
    However, we also end up removing the top padding on command groups
    that immediately proceed a separator, which makes the spacing
    between a group and separator visually inconsistent with that
    between the first group and the command input.
    
    To address this, we can update the following class to use the next-
    sibling combinator (+) instead of the subsequent sibling
    combinator (~) to target only command groups that immediately
    proceed another:
    
      [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0
    
    becomes:
    
      [&_[cmdk-group]:not([hidden])_+[cmdk-group]]:pt-0

--- apps/v4/registry/new-york-v4/ui/command.tsx
+++ apps/v4/registry/new-york-v4/ui/command.tsx
@@ -45,7 +45,7 @@ function CommandDialog({
         <DialogDescription>{description}</DialogDescription>
       </DialogHeader>
       <DialogContent className="overflow-hidden p-0">
-        <Command className="[&_[cmdk-group-heading]]:text-muted-foreground **:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+        <Command className="[&_[cmdk-group-heading]]:text-muted-foreground **:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_+[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
       </DialogContent>
