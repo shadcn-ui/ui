195abee9433a59f2146c8b800213eaab92f58816
Valentin Politov <valentin@politov.md>
2025-04-07 04:30:21 +0300

    fix: restore `asChild` API in `Tooltip` Tailwind v4 component

--- apps/v4/registry/new-york-v4/ui/tooltip.tsx
+++ apps/v4/registry/new-york-v4/ui/tooltip.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import { Slottable } from "@radix-ui/react-slot"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
 import { cn } from "@/lib/utils"
@@ -51,7 +52,7 @@ function TooltipContent({
         )}
         {...props}
       >
-        {children}
+        <Slottable>{children}</Slottable>
         <TooltipPrimitive.Arrow className="bg-primary fill-primary z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]" />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
