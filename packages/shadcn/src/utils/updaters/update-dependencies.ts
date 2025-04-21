6f702f5fbf2b82a388e7da6ea08bcc84c2ec19c6
迷渡 <justjavac@gmail.com>
2025-04-22 03:36:36 +0800

    fix: Add `npm:` specifier when install dependencies with Deno (#6899)
    * fix: Add `npm:` specifier when install dependencies with Deno
    
    * chore: changeset
    
    ---------
    
    Co-authored-by: shadcn <m@shadcn.com>

--- packages/shadcn/src/utils/updaters/update-dependencies.ts
+++ packages/shadcn/src/utils/updaters/update-dependencies.ts
@@ -68,7 +68,9 @@ export async function updateDependencies(
       [
         packageManager === "npm" ? "install" : "add",
         ...(packageManager === "npm" && flag ? [`--${flag}`] : []),
-        ...dependencies,
+        ...(packageManager === "deno"
+          ? dependencies.map((dep) => `npm:${dep}`)
+          : dependencies),
       ],
       {
         cwd: config.resolvedPaths.cwd,
@@ -83,7 +85,9 @@ export async function updateDependencies(
         packageManager === "npm" ? "install" : "add",
         ...(packageManager === "npm" && flag ? [`--${flag}`] : []),
         "-D",
-        ...devDependencies,
+        ...(packageManager === "deno"
+          ? devDependencies.map((dep) => `npm:${dep}`)
+          : devDependencies),
       ],
       {
         cwd: config.resolvedPaths.cwd,
