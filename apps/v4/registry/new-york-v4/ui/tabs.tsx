e47c01c787702aa1aede03ae7f702071f167107a
Mike Bannister <notimpossiblemike@gmail.com>
2025-05-24 14:11:37 -0400

    Fix item alignment in TabList
    Without this the trigger can be misaligned showing a sliver
    of the background color from the tab list.

--- apps/v4/registry/new-york-v4/ui/tabs.tsx
+++ apps/v4/registry/new-york-v4/ui/tabs.tsx
@@ -26,7 +26,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-1",
+        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-stretch justify-center rounded-lg p-1",
         className
       )}
       {...props}
