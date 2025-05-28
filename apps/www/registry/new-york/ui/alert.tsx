19b8e6e6e387d7037f31275918ab89923e9503e1
Satyam Shivhare <mkssshivhare@gmail.com>
2025-05-28 10:38:55 +0530

    fix: Alert components that include a shape and exclude a Title aren't vertically centered #7473

--- apps/www/registry/new-york/ui/alert.tsx
+++ apps/www/registry/new-york/ui/alert.tsx
@@ -50,7 +50,7 @@ const AlertDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    className={cn("text-sm [&_p]:leading-relaxed items-center", className)}
     {...props}
   />
 ))
