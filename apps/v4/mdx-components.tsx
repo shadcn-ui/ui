fb67a4617814bb556a7b8ba5078802598ca8daf9
CHEN Xian-an <xianan.chen@gmail.com>
2025-05-31 11:35:11 +0800

    chore: use `em` instead of `rem` for `code`

--- apps/v4/mdx-components.tsx
+++ apps/v4/mdx-components.tsx
@@ -228,7 +228,7 @@ export const mdxComponents = {
       return (
         <code
           className={cn(
-            "bg-muted relative rounded-md px-[0.3rem] py-[0.2rem] font-mono text-[0.8rem] outline-none",
+            "bg-muted relative rounded-md px-[0.3rem] py-[0.2rem] font-mono text-[0.8em] outline-none",
             className
           )}
           {...props}
