a4abbe42b373132b603bb1d8563a7486c20feb80
Sushant Gandhi <sushantg@vellestudio.com>
2025-06-06 17:13:32 +0530

    feat(blocks) - add new login-06 block

---
+++ apps/www/registry/new-york/blocks/login-06/page.tsx
@@ -0,0 +1,34 @@
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { LoginForm } from "@/registry/new-york/blocks/login-06/components/login-form"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="relative hidden lg:block py-[3vh] pl-[3vh]">
+        <div className="bg-muted h-full rounded-xl overflow-hidden">
+          <img
+            src="/placeholder.svg"
+            alt="Image"
+            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Acme Inc.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
