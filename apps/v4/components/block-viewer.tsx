55282fe8642b344321f686290904922e4f5c5d60
shadcn <m@shadcn.com>
2025-06-06 11:20:55 +0400

    fix: block viewer patterns

--- apps/v4/components/block-viewer.tsx
+++ apps/v4/components/block-viewer.tsx
@@ -244,7 +244,8 @@ function BlockViewerView() {
 
   return (
     <div className="hidden group-data-[view=code]/block-view-wrapper:hidden md:h-(--height) lg:flex">
-      <div className="grid w-full gap-4">
+      <div className="relative grid w-full gap-4">
+        <div className="absolute inset-0 right-4 [background-image:radial-gradient(#d4d4d4_1px,transparent_1px)] [background-size:20px_20px] dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]"></div>
         <ResizablePanelGroup
           direction="horizontal"
           className="after:bg-surface/50 relative z-10 after:absolute after:inset-0 after:right-3 after:z-0 after:rounded-xl"
