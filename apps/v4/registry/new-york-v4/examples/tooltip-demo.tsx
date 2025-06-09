d96eb5321ca04b6ae75824f2869e43d7255d5533
Lucas <50617944+lucasporrini@users.noreply.github.com>
2025-06-10 01:23:39 +0200

    feat: tooltip can now accept an highlight

--- apps/v4/registry/new-york-v4/examples/tooltip-demo.tsx
+++ apps/v4/registry/new-york-v4/examples/tooltip-demo.tsx
@@ -11,7 +11,7 @@ export default function TooltipDemo() {
       <TooltipTrigger asChild>
         <Button variant="outline">Hover</Button>
       </TooltipTrigger>
-      <TooltipContent>
+      <TooltipContent highlight="Ctrl + K">
         <p>Add to library</p>
       </TooltipContent>
     </Tooltip>
