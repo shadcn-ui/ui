96a38911811223033d2cea796e197d54eac2b8da
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-10 09:40:49 +0530

    made changes in active-theme.tsx component to support custom theme generation

--- apps/v4/components/active-theme.tsx
+++ apps/v4/components/active-theme.tsx
@@ -7,12 +7,18 @@ import {
   useEffect,
   useState,
 } from "react"
+import { hex2oklch } from "colorizr"
+
+import { generateThemeFromPrimary } from "@/lib/generate-custom-theme-from-primary"
 
 const DEFAULT_THEME = "default"
+const DEFAULT_CUSTOM_COLOR = "#c800de";
 
 type ThemeContextType = {
   activeTheme: string
   setActiveTheme: (theme: string) => void
+  customColor: string
+  setCustomColor: (color: string) => void
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
@@ -20,15 +26,22 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 export function ActiveThemeProvider({
   children,
   initialTheme,
+  initialCustomColor,
 }: {
   children: ReactNode
   initialTheme?: string
+  initialCustomColor?: string
 }) {
   const [activeTheme, setActiveTheme] = useState<string>(
     () => initialTheme || DEFAULT_THEME
   )
 
+  const [customColor, setCustomColor] = useState<string>(
+    () => initialCustomColor || DEFAULT_CUSTOM_COLOR
+  )
+
   useEffect(() => {
+    console.log("use effect started");
     Array.from(document.body.classList)
       .filter((className) => className.startsWith("theme-"))
       .forEach((className) => {
@@ -38,10 +51,21 @@ export function ActiveThemeProvider({
     if (activeTheme.endsWith("-scaled")) {
       document.body.classList.add("theme-scaled")
     }
-  }, [activeTheme])
+    console.log("active theme is : ", activeTheme)
+    if (activeTheme.endsWith("custom")) {
+      const oklch = hex2oklch(customColor)
+      const customTheme = generateThemeFromPrimary(oklch)
+      Object.entries(customTheme).forEach(([key, value]) => {
+        console.log("key and value are :", key, value);
+        document.body.style.setProperty(key, value)
+      })
+    }
+  }, [activeTheme, customColor])
 
   return (
-    <ThemeContext.Provider value={{ activeTheme, setActiveTheme }}>
+    <ThemeContext.Provider
+      value={{ activeTheme, setActiveTheme, customColor, setCustomColor }}
+    >
       {children}
     </ThemeContext.Provider>
   )
