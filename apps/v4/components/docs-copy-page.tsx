47302762568fa7c9e585a7e31fcab2ce7c780d74
shadcn <m@shadcn.com>
2025-07-23 11:30:23 +0400

    fix: spacing

--- apps/v4/components/docs-copy-page.tsx
+++ apps/v4/components/docs-copy-page.tsx
@@ -54,7 +54,7 @@ const menuItems = {
       >
         <path d="M56 50.203V14h14v46.156C70 65.593 65.593 70 60.156 70c-2.596 0-5.158-1-7-2.843L0 14h19.797L56 50.203ZM147 56h-14V23.953L100.953 56H133v14H96.687C85.814 70 77 61.186 77 50.312V14h14v32.156L123.156 14H91V0h36.312C138.186 0 147 8.814 147 19.688V56Z" />
       </svg>
-      Open in v0
+      <span className="-translate-x-[2px]">Open in v0</span>
     </a>
   ),
   chatgpt: (url: string) => (
