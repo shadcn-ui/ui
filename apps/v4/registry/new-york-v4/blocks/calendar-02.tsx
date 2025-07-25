01b9d14f090ac5dd9cd35e61615b2e03b204fa68
Shourov Roy <shourovroy2389@gmail.com>
2025-07-25 13:28:33 +0600

    Fix: #7885 - Range Calender component fixed in shadcn/ui Library

--- apps/v4/registry/new-york-v4/blocks/calendar-02.tsx
+++ apps/v4/registry/new-york-v4/blocks/calendar-02.tsx
@@ -1,21 +1,23 @@
 "use client"
 
 import * as React from "react"
+import { DateRange } from "react-day-picker"
 
 import { Calendar } from "@/registry/new-york-v4/ui/calendar"
 
 export default function Calendar02() {
-  const [date, setDate] = React.useState<Date | undefined>(
-    new Date(2025, 5, 12)
-  )
+  const [date] = React.useState<Date | undefined>(new Date(2025, 5, 12))
+  const [range, setRange] = React.useState<DateRange | undefined>(undefined)
 
   return (
     <Calendar
-      mode="single"
+      mode="range"
       defaultMonth={date}
       numberOfMonths={2}
-      selected={date}
-      onSelect={setDate}
+      selected={range}
+      onSelect={(range) => {
+        setRange(range)
+      }}
       className="rounded-lg border shadow-sm"
     />
   )
