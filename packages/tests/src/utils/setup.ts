58cb9a3cc52e9a6b01168ee52679b3aa1bf47774
shadcn <m@shadcn.com>
2025-07-30 10:39:00 +0400

    fix: test

--- packages/tests/src/utils/setup.ts
+++ packages/tests/src/utils/setup.ts
@@ -14,19 +14,19 @@ let globalRegistry: Registry | null = null
 beforeAll(async () => {
   await rimraf(TEMP_DIR)
 
-  if (!globalRegistry) {
-    globalRegistry = new Registry()
-    await globalRegistry.start()
+  // if (!globalRegistry) {
+  //   globalRegistry = new Registry()
+  //   await globalRegistry.start()
 
-    process.env.TEST_REGISTRY_URL = globalRegistry.url
-  }
-}, 120000)
+  //   process.env.TEST_REGISTRY_URL = globalRegistry.url
+  // }
+})
 
 afterAll(async () => {
-  if (globalRegistry) {
-    await globalRegistry.stop()
-    globalRegistry = null
-  }
+  // if (globalRegistry) {
+  //   await globalRegistry.stop()
+  //   globalRegistry = null
+  // }
 
   await rimraf(TEMP_DIR)
 
@@ -42,5 +42,5 @@ afterAll(async () => {
 })
 
 export function getRegistryUrl(): string {
-  return process.env.TEST_REGISTRY_URL || "http://localhost:4000/r"
+  return process.env.REGISTRY_URL || "https://ui.shadcn.com/r"
 }
