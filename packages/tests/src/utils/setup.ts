200157bb670a0e64475184c4ed99a3a14d927c45
shadcn <m@shadcn.com>
2025-07-30 11:53:44 +0400

    tests: cleanup

--- packages/tests/src/utils/setup.ts
+++ packages/tests/src/utils/setup.ts
@@ -1,42 +1,18 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
+import { tmpdir } from "os"
 import path from "path"
-import { fileURLToPath } from "url"
 import fs from "fs-extra"
 import { rimraf } from "rimraf"
 
-import { Registry } from "./registry"
+const TEMP_DIR = fs.mkdtempSync(path.join(tmpdir(), "shadcn-tests"))
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const TEMP_DIR = path.join(__dirname, "../../temp")
-
-let globalRegistry: Registry | null = null
+console.log("TEMP_DIR", TEMP_DIR)
 
 export async function setup() {
   await rimraf(TEMP_DIR)
   await fs.ensureDir(TEMP_DIR)
-
-  // if (!globalRegistry) {
-  //   globalRegistry = new Registry()
-  //   await globalRegistry.start()
-  //   process.env.REGISTRY_URL = globalRegistry.url
-  // }
 }
 
 export async function teardown() {
-  try {
-    const { execa } = await import("execa")
-    await execa("pnpm", ["install"], {
-      cwd: path.join(__dirname, "../../../.."),
-      stdio: "inherit",
-    })
-
-    await rimraf(TEMP_DIR)
-
-    // if (globalRegistry) {
-    //   await globalRegistry.stop()
-    //   globalRegistry = null
-    // }
-  } catch (error) {
-    console.error("Failed to restore lockfile:", error)
-  }
+  await rimraf(TEMP_DIR)
 }
