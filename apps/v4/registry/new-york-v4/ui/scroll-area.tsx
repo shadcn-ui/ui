d3d758476f0d88ac3e5cba20d2118ddcdc07e5a0
ChoByeongchan <jobc4550@gmail.com>
2025-08-06 16:06:58 +0900

    feat(registry): add viewportRef prop to allow forwarding ref to Viewport

--- apps/v4/registry/new-york-v4/ui/scroll-area.tsx
+++ apps/v4/registry/new-york-v4/ui/scroll-area.tsx
@@ -5,11 +5,11 @@ import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 
 import { cn } from "@/lib/utils"
 
-function ScrollArea({
-  className,
-  children,
-  ...props
-}: React.ComponentProps<typeof ScrollAreaPrimitive.Root>) {
+type Props = React.ComponentProps<typeof ScrollAreaPrimitive.Root> & {
+  viewportRef?: React.Ref<HTMLDivElement>
+}
+
+function ScrollArea({ className, children, viewportRef, ...props }: Props) {
   return (
     <ScrollAreaPrimitive.Root
       data-slot="scroll-area"
@@ -17,6 +17,7 @@ function ScrollArea({
       {...props}
     >
       <ScrollAreaPrimitive.Viewport
+        ref={viewportRef}
         data-slot="scroll-area-viewport"
         className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
       >
