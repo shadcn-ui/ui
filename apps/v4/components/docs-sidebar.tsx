fd44aead8ebc50dea6906d1089dab7f75d7388a9
shadcn <m@shadcn.com>
2025-08-13 18:15:40 +0400

    docs(www): add cli command to docs

--- apps/v4/components/docs-sidebar.tsx
+++ apps/v4/components/docs-sidebar.tsx
@@ -46,7 +46,10 @@ export function DocsSidebar({
                             isActive={item.url === pathname}
                             className="data-[active=true]:bg-accent data-[active=true]:border-accent 3xl:fixed:w-full 3xl:fixed:max-w-48 relative h-[30px] w-fit overflow-visible border border-transparent text-[0.8rem] font-medium after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md"
                           >
-                            <Link href={item.url}>{item.name}</Link>
+                            <Link href={item.url}>
+                              <span className="absolute inset-0 flex w-(--sidebar-width) bg-transparent" />
+                              {item.name}
+                            </Link>
                           </SidebarMenuButton>
                         </SidebarMenuItem>
                       )
