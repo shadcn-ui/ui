d7421a8ffc448e45fe6d5d429c01c3c7493eac0d
shadcn <m@shadcn.com>
2025-09-15 12:46:47 +0400

    feat: add getRegistriesIndex

--- packages/shadcn/src/registry/index.ts
+++ packages/shadcn/src/registry/index.ts
@@ -1,4 +1,4 @@
-export { getRegistryItems, resolveRegistryItems, getRegistry } from "./api"
+export { getRegistryItems, resolveRegistryItems, getRegistry, getRegistriesIndex } from "./api"
 
 export { searchRegistries } from "./search"
 
@@ -11,6 +11,7 @@ export {
   RegistryNotConfiguredError,
   RegistryLocalFileError,
   RegistryParseError,
+  RegistriesIndexParseError,
   RegistryMissingEnvironmentVariablesError,
   RegistryInvalidNamespaceError,
 } from "./errors"
