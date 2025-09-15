b7c28199be94375ab2b2ecac0d3c3971d3657853
shadcn <m@shadcn.com>
2025-09-15 10:46:58 +0400

    docs: add import and plugin examples (#8215)

--- apps/v4/app/(app)/docs/[[...slug]]/page.tsx
+++ apps/v4/app/(app)/docs/[[...slug]]/page.tsx
@@ -193,14 +193,13 @@ export default async function Page(props: {
           )}
         </div>
       </div>
-      <div className="sticky top-[calc(var(--header-height)+1px)] z-30 ml-auto hidden h-[calc(100svh-var(--footer-height)+2rem)] w-72 flex-col gap-4 overflow-hidden overscroll-none pb-8 xl:flex">
+      <div className="sticky top-[calc(var(--header-height)+1px)] z-30 ml-auto hidden w-72 flex-col gap-4 overflow-hidden overscroll-none pb-8 xl:flex">
         <div className="h-(--top-spacing) shrink-0" />
         {/* @ts-expect-error - revisit fumadocs types. */}
         {doc.toc?.length ? (
           <div className="no-scrollbar overflow-y-auto px-8">
             {/* @ts-expect-error - revisit fumadocs types. */}
             <DocsTableOfContents toc={doc.toc} />
-            <div className="h-12" />
           </div>
         ) : null}
         <div className="flex flex-1 flex-col gap-12 px-6">
