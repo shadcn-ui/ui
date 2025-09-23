dc4db38ecbbd2b5b845d43f4a24be80c9f2c4c89
Landry Carroll <carroll.landry@gmail.com>
2025-09-22 21:49:46 -0400

    fix(sidebar): broken arbitrary css classes

--- apps/v4/components/docs-sidebar.tsx
+++ apps/v4/components/docs-sidebar.tsx
@@ -74,7 +74,7 @@ export function DocsSidebar({
                       className="data-[active=true]:bg-accent data-[active=true]:border-accent 3xl:fixed:w-full 3xl:fixed:max-w-48 relative h-[30px] w-fit overflow-visible border border-transparent text-[0.8rem] font-medium after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md"
                     >
                       <Link href={href}>
-                        <span className="absolute inset-0 flex w-(--sidebar-width) bg-transparent" />
+                        <span className="absolute inset-0 flex w-[var(--sidebar-width)] bg-transparent" />
                         {name}
                       </Link>
                     </SidebarMenuButton>
@@ -116,7 +116,7 @@ export function DocsSidebar({
                               className="data-[active=true]:bg-accent data-[active=true]:border-accent 3xl:fixed:w-full 3xl:fixed:max-w-48 relative h-[30px] w-fit overflow-visible border border-transparent text-[0.8rem] font-medium after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md"
                             >
                               <Link href={item.url}>
-                                <span className="absolute inset-0 flex w-(--sidebar-width) bg-transparent" />
+                                <span className="absolute inset-0 flex w-[var(--sidebar-width)] bg-transparent" />
                                 {item.name}
                               </Link>
                             </SidebarMenuButton>
