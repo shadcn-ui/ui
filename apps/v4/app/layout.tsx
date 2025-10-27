7abc02fe6a7996c6a5e01849f82e05a35c42263b
AdhamMohamedSaleh <adham.work24@gmail.com>
2025-10-27 15:25:13 +0300

    added global rtl to html

--- apps/v4/app/layout.tsx
+++ apps/v4/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning dir="rtl">
       <head>
         <script
           dangerouslySetInnerHTML={{
