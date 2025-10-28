0b0e83339ad551b92b43dcd9618d09ecadce6e73
wankong <1549072621@qq.com>
2025-10-28 13:31:34 +0800

    fix(sidebar): change to overflow-hidden just in horizontal

--- apps/v4/registry/new-york-v4/ui/sidebar.tsx
+++ apps/v4/registry/new-york-v4/ui/sidebar.tsx
@@ -374,7 +374,7 @@ function SidebarContent({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="sidebar-content"
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-x-hidden",
         className
       )}
       {...props}
