ffe93721bae6ec7ac69f2b06300f6f19a8a5497f
Mirko Tebaldi <realtebo@users.noreply.github.com>
2025-11-11 10:10:02 +0100

    Fix "Calling an impure function can produce unstable result"
    Replace memoized width with state to update randomly.

--- apps/v4/registry/new-york-v4/ui/sidebar.tsx
+++ apps/v4/registry/new-york-v4/ui/sidebar.tsx
@@ -606,10 +606,12 @@ function SidebarMenuSkeleton({
 }: React.ComponentProps<"div"> & {
   showIcon?: boolean
 }) {
+
   // Random width between 50 to 90%.
-  const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
-  }, [])
+  const [width, setWidth] = React.useState("90%");
+  React.useEffect(() => {
+    setWidth(`${Math.floor(Math.random() * 40) + 50}%`);
+  }, []);
 
   return (
     <div
