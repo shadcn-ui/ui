595197d6a8b954350260c5ea63c819349e21f40e
yash <yashkumaringale23@gmail.com>
2025-12-01 22:33:51 +0530

    fix: update tsconfig and calendar for range fix

--- apps/v4/registry/new-york-v4/ui/calendar.tsx
+++ apps/v4/registry/new-york-v4/ui/calendar.tsx
@@ -14,7 +14,7 @@ import { Button, buttonVariants } from "@/registry/new-york-v4/ui/button"
 function Calendar({
   className,
   classNames,
-  showOutsideDays = true,
+  showOutsideDays = false,
   captionLayout = "label",
   buttonVariant = "ghost",
   formatters,
@@ -198,11 +198,12 @@ function CalendarDayButton({
         modifiers.selected &&
         !modifiers.range_start &&
         !modifiers.range_end &&
-        !modifiers.range_middle
+        !modifiers.range_middle &&
+        !modifiers.outside
       }
-      data-range-start={modifiers.range_start}
-      data-range-end={modifiers.range_end}
-      data-range-middle={modifiers.range_middle}
+      data-range-start={modifiers.range_start && !modifiers.outside}
+      data-range-end={modifiers.range_end && !modifiers.outside}
+      data-range-middle={modifiers.range_middle && !modifiers.outside}
       className={cn(
         "data-[selected-single=true]:bg-primary data-[selected-single=true]:text-primary-foreground data-[range-middle=true]:bg-accent data-[range-middle=true]:text-accent-foreground data-[range-start=true]:bg-primary data-[range-start=true]:text-primary-foreground data-[range-end=true]:bg-primary data-[range-end=true]:text-primary-foreground group-data-[focused=true]/day:border-ring group-data-[focused=true]/day:ring-ring/50 dark:hover:text-accent-foreground flex aspect-square size-auto w-full min-w-(--cell-size) flex-col gap-1 leading-none font-normal group-data-[focused=true]/day:relative group-data-[focused=true]/day:z-10 group-data-[focused=true]/day:ring-[3px] data-[range-end=true]:rounded-md data-[range-end=true]:rounded-r-md data-[range-middle=true]:rounded-none data-[range-start=true]:rounded-md data-[range-start=true]:rounded-l-md [&>span]:text-xs [&>span]:opacity-70",
         defaultClassNames.day,
