89bdd52bc547c8159c95922310f900dd2d83f03e
Paul Lotz <paul.lotz@gmx.at>
2025-12-12 16:30:39 +0100

    fix(components): dont use index as key

--- apps/v4/registry/new-york-v4/ui/field.tsx
+++ apps/v4/registry/new-york-v4/ui/field.tsx
@@ -211,8 +211,8 @@ function FieldError({
     return (
       <ul className="ml-4 flex list-disc flex-col gap-1">
         {uniqueErrors.map(
-          (error, index) =>
-            error?.message && <li key={index}>{error.message}</li>
+          (error) =>
+            error?.message && <li key={error.message}>{error.message}</li>
         )}
       </ul>
     )
