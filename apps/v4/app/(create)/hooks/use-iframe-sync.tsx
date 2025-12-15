80f97e39eaa9183411bf41ff99132c204857850b
François Best <github@francoisbest.com>
2025-12-15 21:26:26 +0100

    fix: preview iframe URL state sync

--- apps/v4/app/(create)/hooks/use-iframe-sync.tsx
+++ apps/v4/app/(create)/hooks/use-iframe-sync.tsx
@@ -1,7 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
 
 import * as React from "react"
+
+import type { DesignSystemSearchParams } from "@/app/(create)/lib/search-params"
+
+type ParentToIframeMessage = {
+  type: "design-system-params"
+  data: DesignSystemSearchParams
+}
 
 export const isInIframe = () => {
   if (typeof window === "undefined") {
@@ -10,157 +16,21 @@ export const isInIframe = () => {
   return window.self !== window.top
 }
 
-export function createIframeSyncStore<T extends Record<string, any>>(
-  messageType: string,
-  defaultValues: T
-) {
-  const store = new Map<keyof T, any>()
-  const listeners = new Map<keyof T, Set<() => void>>()
-
-  if (typeof window !== "undefined") {
-    Object.entries(defaultValues).forEach(([key, value]) => {
-      store.set(key as keyof T, value)
-    })
-  }
-
-  if (typeof window !== "undefined" && isInIframe()) {
-    window.addEventListener("message", (event: MessageEvent) => {
-      if (event.data.type === messageType && event.data.params) {
-        Object.keys(event.data.params).forEach((key) => {
-          const newValue = event.data.params[key]
-          const oldValue = store.get(key as keyof T)
-
-          if (newValue !== oldValue) {
-            store.set(key as keyof T, newValue)
-
-            const keyListeners = listeners.get(key as keyof T)
-            if (keyListeners) {
-              keyListeners.forEach((listener) => listener())
-            }
-          }
-        })
-      }
-    })
-  }
-
-  return {
-    store,
-    listeners,
-    subscribe(key: keyof T, callback: () => void) {
-      if (!isInIframe()) return () => {}
-
-      if (!listeners.has(key)) {
-        listeners.set(key, new Set())
-      }
-      const keyListeners = listeners.get(key)!
-      keyListeners.add(callback)
-
-      return () => {
-        keyListeners.delete(callback)
-        if (keyListeners.size === 0) {
-          listeners.delete(key)
-        }
-      }
-    },
-    subscribeAll(keys: (keyof T)[], callback: () => void) {
-      if (!isInIframe()) return () => {}
-
-      keys.forEach((key) => {
-        if (!listeners.has(key)) {
-          listeners.set(key, new Set())
-        }
-        listeners.get(key)!.add(callback)
-      })
-
-      return () => {
-        keys.forEach((key) => {
-          const keyListeners = listeners.get(key)
-          if (keyListeners) {
-            keyListeners.delete(callback)
-            if (keyListeners.size === 0) {
-              listeners.delete(key)
-            }
-          }
-        })
-      }
-    },
-    get(key: keyof T) {
-      return store.get(key) as T[keyof T]
-    },
-    getAll() {
-      const result = {} as T
-      store.forEach((value, key) => {
-        result[key as keyof T] = value
-      })
-      return result
-    },
-  }
-}
-
-export function useIframeSyncValue<T>(
-  store: ReturnType<typeof createIframeSyncStore<any>>,
-  key: string,
-  urlValue: T
-) {
-  const subscribe = React.useCallback(
-    (callback: () => void) => {
-      return store.subscribe(key, callback)
-    },
-    [store, key]
-  )
-
-  const getSnapshot = React.useCallback(() => {
-    if (!isInIframe()) {
-      return urlValue
-    }
-    return store.get(key) as T
-  }, [store, key, urlValue])
-
-  const getServerSnapshot = React.useCallback(() => {
-    return urlValue
-  }, [urlValue])
-
-  return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
-}
-
-export function useIframeSyncAll<T extends Record<string, any>>(
-  store: ReturnType<typeof createIframeSyncStore<T>>,
-  keys: (keyof T)[],
-  urlValues: T
-) {
-  const subscribe = React.useCallback(
-    (callback: () => void) => {
-      return store.subscribeAll(keys, callback)
-    },
-    [store, keys]
-  )
-
-  const getSnapshot = React.useCallback(() => {
-    if (!isInIframe()) {
-      return urlValues
-    }
-    return store.getAll()
-  }, [store, urlValues])
-
-  const getServerSnapshot = React.useCallback(() => {
-    return urlValues
-  }, [urlValues])
-
-  return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
-}
-
-export function useParentMessageListener<T>(
-  messageType: string,
-  onMessage: (data: T) => void
+export function useIframeMessageListener<
+  Message extends ParentToIframeMessage,
+  MessageType extends Message["type"],
+>(
+  messageType: MessageType,
+  onMessage: (data: Extract<Message, { type: MessageType }>["data"]) => void
 ) {
   React.useEffect(() => {
-    if (isInIframe()) {
+    if (!isInIframe()) {
       return
     }
 
     const handleMessage = (event: MessageEvent) => {
       if (event.data.type === messageType) {
-        onMessage(event.data)
+        onMessage(event.data.data)
       }
     }
 
@@ -171,33 +41,22 @@ export function useParentMessageListener<T>(
   }, [messageType, onMessage])
 }
 
-export function sendToIframe(
+export function sendToIframe<
+  Message extends ParentToIframeMessage,
+  MessageType extends Message["type"],
+>(
   iframe: HTMLIFrameElement | null,
-  messageType: string,
-  data: any
+  messageType: MessageType,
+  data: Extract<Message, { type: MessageType }>["data"]
 ) {
-  if (!iframe || !iframe.contentWindow) {
+  if (!iframe?.contentWindow) {
     return
   }
 
   iframe.contentWindow.postMessage(
     {
       type: messageType,
-      ...data,
-    },
-    "*"
-  )
-}
-
-export function sendToParent(messageType: string, data: any) {
-  if (!isInIframe()) {
-    return
-  }
-
-  window.parent.postMessage(
-    {
-      type: messageType,
-      ...data,
+      data,
     },
     "*"
   )
