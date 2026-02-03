926df433a73b855ebc9986d6107d28b7b484c9e3
shadcn <m@shadcn.com>
2026-02-03 16:43:17 +0400

    fix

--- apps/v4/registry/bases/radix/ui/button-group.tsx
+++ apps/v4/registry/bases/radix/ui/button-group.tsx
@@ -67,7 +67,7 @@ function ButtonGroupSeparator({
       data-slot="button-group-separator"
       orientation={orientation}
       className={cn(
-        "cn-button-group-separator relative self-stretch data-[orientation=horizontal]:mx-px data-[orientation=horizontal]:w-auto data-[orientation=vertical]:my-px data-[orientation=vertical]:h-auto",
+        "cn-button-group-separator relative self-stretch data-horizontal:mx-px data-horizontal:w-auto data-vertical:my-px data-vertical:h-auto",
         className
       )}
       {...props}
