381f2ef165e78290f128a3faffe62f22752f9b24
shadcn <m@shadcn.com>
2026-02-04 15:11:30 +0400

    fix

--- apps/v4/examples/radix/ui-rtl/context-menu.tsx
+++ apps/v4/examples/radix/ui-rtl/context-menu.tsx
@@ -126,7 +126,7 @@ function ContextMenuSubTrigger({
         hugeicons="ArrowRight01Icon"
         phosphor="CaretRightIcon"
         remixicon="RiArrowRightSLine"
-        className="ms-auto"
+        className="ms-auto rtl:rotate-180"
       />
     </ContextMenuPrimitive.SubTrigger>
   )
