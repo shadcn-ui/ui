c2f88d459db3f90dc93777801c3d9a33c337e051
shadcn <m@shadcn.com>
2026-02-06 15:30:17 +0400

    fix

--- apps/v4/registry/styles.tsx
+++ apps/v4/registry/styles.tsx
@@ -116,6 +116,29 @@ export const STYLES = [
       </svg>
     ),
   },
+  {
+    name: "rhea",
+    title: "Rhea",
+    description: "Rhea style.",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="128"
+        height="128"
+        viewBox="0 0 24 24"
+        fill="none"
+        role="img"
+        color="currentColor"
+      >
+        <path
+          d="M2 12C2 9.19974 2 7.79961 2.54497 6.73005C3.02433 5.78924 3.78924 5.02433 4.73005 4.54497C5.79961 4 7.19974 4 10 4H14C16.8003 4 18.2004 4 19.27 4.54497C20.2108 5.02433 20.9757 5.78924 21.455 6.73005C22 7.79961 22 9.19974 22 12C22 14.8003 22 16.2004 21.455 17.27C20.9757 18.2108 20.2108 18.9757 19.27 19.455C18.2004 20 16.8003 20 14 20H10C7.19974 20 5.79961 20 4.73005 19.455C3.78924 18.9757 3.02433 18.2108 2.54497 17.27C2 16.2004 2 14.8003 2 12Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinejoin="round"
+        ></path>
+      </svg>
+    ),
+  },
 ] as const
 
 export type Style = (typeof STYLES)[number]
