b86885512f8acf8e114712876c16d8e8f57fa343
shadcn <m@shadcn.com>
2026-02-16 16:56:24 +0400

    fix: move className in badgeVariants

--- apps/v4/examples/base/ui/badge.tsx
+++ apps/v4/examples/base/ui/badge.tsx
@@ -36,7 +36,7 @@ function Badge({
     defaultTagName: "span",
     props: mergeProps<"span">(
       {
-        className: cn(badgeVariants({ className, variant })),
+        className: cn(badgeVariants({ variant }), className),
       },
       props
     ),
