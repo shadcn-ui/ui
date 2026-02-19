8ded0658d475b31e5cbb31c95c915983eb85b630
shadcn <m@shadcn.com>
2026-02-19 12:44:06 +0400

    fix

--- templates/vite-app/src/App.tsx
+++ templates/vite-app/src/App.tsx
@@ -1,7 +1,18 @@
 export function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-4 p-6">
-      <p>Hello World</p>
+    <div className="flex min-h-svh p-6">
+      <div className="max-w-md text-sm leading-loose">
+        <h1 className="font-medium">Project ready!</h1>
+        <p>You may now add components and start building.</p>
+        <a
+          href="https://ui.shadcn.com/docs/new"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-2 inline-flex text-primary underline underline-offset-4"
+        >
+          Read the docs
+        </a>
+      </div>
     </div>
   )
 }
