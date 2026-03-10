ae733168cd2f6932dcaac0dc96fe33c798f1639d
shadcn <m@shadcn.com>
2026-03-10 13:29:15 +0400

    fix

--- apps/v4/examples/base/ui/context-menu.tsx
+++ apps/v4/examples/base/ui/context-menu.tsx
@@ -147,7 +147,7 @@ function ContextMenuSubContent({
   return (
     <ContextMenuContent
       data-slot="context-menu-sub-content"
-      className="cn-menu-target cn-menu-translucent min-w-32 rounded-lg border bg-popover p-1 text-popover-foreground shadow-lg duration-100 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95"
+      className="cn-menu-target cn-menu-translucent shadow-lg"
       side="right"
       {...props}
     />
