281a4572ac6073661f2e2e3eb4e41a95cfd81559
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-12 20:48:23 +0700

    Update apps/v4/app/layout.tsx

--- apps/v4/app/layout.tsx
+++ apps/v4/app/layout.tsx
@@ -108,6 +108,7 @@ export default function RootLayout({
               <Analytics />
             </ActiveThemeProvider>
           </LayoutProvider>
+          
         </ThemeProvider>
       </body>
     </html>
