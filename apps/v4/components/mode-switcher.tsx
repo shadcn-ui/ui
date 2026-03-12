2a42bce48d56f4bf8c841e4a6c95d4c657e7ed5c
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-12 20:36:50 +0700

    Update apps/v4/components/mode-switcher.tsx

--- apps/v4/components/mode-switcher.tsx
+++ apps/v4/components/mode-switcher.tsx
@@ -54,6 +54,7 @@ export function ModeSwitcher() {
         <Button
           variant="ghost"
           size="icon"
+          
           className="group/toggle extend-touch-target size-8"
           onClick={toggleTheme}
         >
