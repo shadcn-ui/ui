bc50f11e4da35ea63800202421c4d76916e14047
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-12 20:47:46 +0700

    Update apps/v4/app/layout.tsx

--- apps/v4/app/layout.tsx
+++ apps/v4/app/layout.tsx
@@ -6,6 +6,7 @@ import { NuqsAdapter } from "nuqs/adapters/next/app"
 import { META_THEME_COLORS, siteConfig } from "@/lib/config"
 
 import { fontVariables } from "@/lib/fonts"
+
 import { cn } from "@/lib/utils"
 import { LayoutProvider } from "@/hooks/use-layout"
 import { ActiveThemeProvider } from "@/components/active-theme"
