4a5fc89ea9055e7739b6e42d5b94541e632b0c0f
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-12 13:41:44 +0700

    Update apps/v4/app/layout.tsx

--- apps/v4/app/layout.tsx
+++ apps/v4/app/layout.tsx
@@ -40,6 +40,7 @@ export const metadata: Metadata = {
     images: [
       {
         url: `${process.env.NEXT_PUBLIC_APP_URL}/opengraph-image.png`,
+        
         width: 1200,
         height: 630,
         alt: siteConfig.name,
