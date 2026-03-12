0ab19ec94c798918c2b97ce40bb1c5da1cd50956
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-12 09:43:02 +0700

    Update apps/v4/app/(internal)/sink/components/form-demo.tsx

--- apps/v4/app/(internal)/sink/components/form-demo.tsx
+++ apps/v4/app/(internal)/sink/components/form-demo.tsx
@@ -298,6 +298,7 @@ export function FormDemo() {
                         </FormItem>
                       )
                     }}
+                    
                   />
                 ))}
               </div>
