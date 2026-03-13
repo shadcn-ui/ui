81f791f9b66c4c0387c248fc17e75a7967b1a140
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-13 12:27:28 +0700

    Update apps/v4/components/mode-switcher.tsx

--- apps/v4/components/mode-switcher.tsx
+++ apps/v4/components/mode-switcher.tsx
@@ -24,6 +24,7 @@ import {
 
 export const DARK_MODE_FORWARD_TYPE = "dark-mode-forward"
 
+
 export function ModeSwitcher() {
   const { setTheme, resolvedTheme } = useTheme()
   const { setMetaColor, metaColor } = useMetaColor()
