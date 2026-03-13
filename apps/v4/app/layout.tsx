acaecea4a38e1a5fbd9fbd0ddd1b897479526048
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-13 11:57:02 +0700

    Update apps/v4/app/layout.tsx

--- apps/v4/app/layout.tsx
+++ apps/v4/app/layout.tsx
@@ -85,6 +85,7 @@ export default function RootLayout({
                 }
                 if (localStorage.layout) {
                   document.documentElement.classList.add('layout-' + localStorage.layout)
+                  
                 }
                 
               } catch (_) {}
