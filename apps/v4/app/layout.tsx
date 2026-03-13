2a6ac7aa4f6212933266ee960a33c80da2bb4297
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-13 11:31:32 +0700

    Update apps/v4/app/layout.tsx

--- apps/v4/app/layout.tsx
+++ apps/v4/app/layout.tsx
@@ -95,6 +95,7 @@ export default function RootLayout({
       <body
         className={cn(
           "group/body overscroll-none antialiased [--footer-height:calc(var(--spacing)*14)] [--header-height:calc(var(--spacing)*14)] xl:[--footer-height:calc(var(--spacing)*24)]"
+          
         )}
       >
         <ThemeProvider>
