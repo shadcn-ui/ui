427868080e7197c6292375077a1e6d387023a954
Tack kee <233528480+PichaiTK@users.noreply.github.com>
2026-03-17 11:49:03 +0700

    Update apps/v4/app/layout.tsx

--- apps/v4/app/layout.tsx
+++ apps/v4/app/layout.tsx
@@ -138,6 +138,7 @@ export default function RootLayout({
             <ActiveThemeProvider>
             
             
+            
               <NuqsAdapter>
               
                 {children}
