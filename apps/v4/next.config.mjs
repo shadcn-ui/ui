79125944ef5e77935518ff0cb0d0ddb7902700d8
Lukáš Lobík <lobiklukas@gmail.com>
2026-03-31 02:05:41 +0200

    fix: next.js rewrites

--- apps/v4/next.config.mjs
+++ apps/v4/next.config.mjs
@@ -135,20 +135,24 @@ const nextConfig = {
     ]
   },
   rewrites() {
-    return [
-      {
-        source: "/preview/:path*",
-        destination: "/preview/index.html",
-      },
-      {
-        source: "/docs/:path*.md",
-        destination: "/llm/:path*",
-      },
-      {
-        source: "/init.md",
-        destination: "/init/md",
-      },
-    ]
+    return {
+      beforeFiles: [
+        {
+          source: "/preview/:path*",
+          destination: "/preview/index.html",
+        },
+      ],
+      afterFiles: [
+        {
+          source: "/docs/:path*.md",
+          destination: "/llm/:path*",
+        },
+        {
+          source: "/init.md",
+          destination: "/init/md",
+        },
+      ],
+    }
   },
 }
 
