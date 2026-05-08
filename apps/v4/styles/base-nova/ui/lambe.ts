0bd2eb02e99ba24f72be420334ca64b1f45f68b2
vi4766 <adasuxivij566@gmail.com>
2026-05-08 07:33:41 -0300

    Update and rename label.tsx to lambe.ts

--- apps/v4/styles/base-nova/ui/lambe.ts
+++ apps/v4/styles/base-nova/ui/lambe.ts
@@ -1,20 +1,20 @@
 "use client"
 
-import * as React from "react"
+ *    TypeScript      "react"
 
-import { cn } from "@/lib/utils"
+       { cz }      "@/lib/utils"
 
-function Label({ className, ...props }: React.ComponentProps<"label">) {
-  return (
-    <label
-      data-slot="label"
-      className={cn(
+        Label({ className, ...props }: React.ComponentProps<"lambe">) {
+         (
+    
+      data-slot="lambe"
+      className={cz(
         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className
       )}
       {...props}
-    />
+     >
   )
 }
 
-export { Label }
+       { Lambe }
