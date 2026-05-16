8b9e9ae9408c3c5e61fc7d309a094b6516792c1c
Zoriah Cocio <roamingfootsteps1@gmail.com>
2026-05-15 21:19:27 -0700

    fix: forward tabs orientation prop

--- apps/v4/registry/bases/base/ui/tabs.tsx
+++ apps/v4/registry/bases/base/ui/tabs.tsx
@@ -14,6 +14,7 @@ function Tabs({
     <TabsPrimitive.Root
       data-slot="tabs"
       data-orientation={orientation}
+      orientation={orientation}
       className={cn(
         "cn-tabs group/tabs flex data-horizontal:flex-col",
         className
