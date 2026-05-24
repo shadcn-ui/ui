13e988d7c8b0ccff933b025c16e036a4b4a1f547
Bhavik Parmar <bhavikparmar38@gmail.com>
2026-05-24 22:11:01 +0200

    fix(carousel): prevent click suppression on nav buttons

--- apps/v4/styles/radix-nova/ui/carousel.tsx
+++ apps/v4/styles/radix-nova/ui/carousel.tsx
@@ -187,8 +187,8 @@ function CarouselPrevious({
       className={cn(
         "absolute touch-manipulation rounded-full",
         orientation === "horizontal"
-          ? "top-1/2 -left-12 -translate-y-1/2"
-          : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
+          ? "top-1/2 -left-12 -translate-y-1/2 active:not-aria-[haspopup]:-translate-y-[calc(50%-0.5px)]"
+          : "-top-12 left-1/2 -translate-x-1/2 rotate-90 active:not-aria-[haspopup]:-translate-x-1/2 active:not-aria-[haspopup]:translate-y-[0.5px]",
         className
       )}
       disabled={!canScrollPrev}
@@ -217,8 +217,8 @@ function CarouselNext({
       className={cn(
         "absolute touch-manipulation rounded-full",
         orientation === "horizontal"
-          ? "top-1/2 -right-12 -translate-y-1/2"
-          : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
+          ? "top-1/2 -right-12 -translate-y-1/2 active:not-aria-[haspopup]:-translate-y-[calc(50%-0.5px)]"
+          : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90 active:not-aria-[haspopup]:-translate-x-1/2 active:not-aria-[haspopup]:translate-y-[0.5px]",
         className
       )}
       disabled={!canScrollNext}
