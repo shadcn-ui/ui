df1752dfe092957f84aa467d2270436139e3ecc7
shadcn <m@shadcn.com>
2026-05-26 22:54:07 +0400

    feat: rhea (#10779)
    * feat: add rhea
    
    * fix: blocks
    
    * feat: build chat example
    
    * fix
    
    * fix: sidebar
    
    * fix
    
    * feat: update home
    
    * fix
    
    * fix
    
    * fix
    
    * feat: optimizine fonts
    
    * feat
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix: font in preview
    
    * fix

--- apps/v4/components/site-header.tsx
+++ apps/v4/components/site-header.tsx
@@ -8,11 +8,9 @@ import { siteConfig } from "@/lib/config"
 import { source } from "@/lib/source"
 import { CommandMenu } from "@/components/command-menu"
 import { GitHubLink } from "@/components/github-link"
-import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { MobileNav } from "@/components/mobile-nav"
 import { ModeSwitcher } from "@/components/mode-switcher"
-import { SiteConfig } from "@/components/site-config"
 import { Separator } from "@/registry/new-york-v4/ui/separator"
 import { Button } from "@/styles/radix-nova/ui/button"
 import { ProjectForm } from "@/app/(app)/create/components/project-form"
@@ -31,17 +29,6 @@ export function SiteHeader() {
             items={siteConfig.navItems}
             className="flex lg:hidden"
           />
-          <Button
-            asChild
-            variant="ghost"
-            size="icon"
-            className="hidden size-8 lg:flex"
-          >
-            <Link href="/">
-              <Icons.logo className="size-5" />
-              <span className="sr-only">{siteConfig.name}</span>
-            </Link>
-          </Button>
           <MainNav items={siteConfig.navItems} className="hidden lg:flex" />
           <div className="ml-auto flex items-center gap-2 md:flex-1 md:justify-end">
             <div className="hidden w-full flex-1 md:flex md:w-auto md:flex-none">
@@ -56,11 +43,6 @@ export function SiteHeader() {
               className="ml-2 hidden lg:block"
             />
             <GitHubLink />
-            <Separator
-              orientation="vertical"
-              className="hidden group-has-data-[slot=designer]/layout:hidden 3xl:flex"
-            />
-            <SiteConfig className="hidden 3xl:flex 3xl:group-has-data-[slot=designer]/layout:hidden" />
             <Separator orientation="vertical" />
             <ModeSwitcher />
             <div className="hidden items-center gap-2 group-has-data-[slot=designer]/layout:md:flex">
