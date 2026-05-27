acdd6655a350789f86bb6bc88100e86c710811ef
shadcn <m@shadcn.com>
2026-05-27 19:05:37 +0400

    Revert "chore: update templates (#10784)"
    This reverts commit f3e16e7db72b0cd3b6696bdbb643da3574847acb.

--- templates/start-monorepo/apps/web/vite.config.ts
+++ templates/start-monorepo/apps/web/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite"
-import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
+import viteTsConfigPaths from "vite-tsconfig-paths"
 import tailwindcss from "@tailwindcss/vite"
+import { nitro } from "nitro/vite"
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    nitro(),
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
 })
 
 export default config
