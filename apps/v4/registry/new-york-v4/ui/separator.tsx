7865d35d3d90b91c9a523fa6ad4de616b24f702a
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2026-05-30 19:16:10 +0530

    fix(v4): use border-based separators

--- apps/v4/registry/new-york-v4/ui/separator.tsx
+++ apps/v4/registry/new-york-v4/ui/separator.tsx
@@ -17,7 +17,7 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        "shrink-0 border-none border-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=horizontal]:border-b data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px data-[orientation=vertical]:border-l",
         className
       )}
       {...props}
