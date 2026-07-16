58c910ecf15f1500d25c59590c50c4a4b38b0d7c
shadcn <m@shadcn.com>
2026-07-16 09:44:25 +0400

    fix: style

--- apps/v4/mdx-components.tsx
+++ apps/v4/mdx-components.tsx
@@ -184,7 +184,7 @@ export const mdxComponents = {
       <pre
         data-not-typeset
         className={cn(
-          "no-scrollbar min-w-0 overflow-x-auto overflow-y-auto overscroll-x-contain overscroll-y-auto px-4 py-3.5 outline-none has-data-highlighted-line:px-0 has-data-line-numbers:px-0 has-data-[slot=tabs]:p-0",
+          "has-data-highlighted-line:px-0 no-scrollbar min-w-0 overflow-x-auto overflow-y-auto overscroll-x-contain overscroll-y-auto px-4 py-3.5 outline-none has-data-line-numbers:px-0 has-data-[slot=tabs]:p-0",
           className
         )}
         {...props}
