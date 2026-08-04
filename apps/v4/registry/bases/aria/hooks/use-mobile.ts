f21dffce8f38dd6aec7abffdd557810f4b5ac6a3
hokkyss <hokkyss2@gmail.com>
2026-08-04 13:43:45 +0800

    refactor: update useIsMobile to use React.useSyncExternalStore

--- apps/v4/registry/bases/aria/hooks/use-mobile.ts
+++ apps/v4/registry/bases/aria/hooks/use-mobile.ts
@@ -1,19 +1,19 @@
-import * as React from "react"
+import * as React from 'react';
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  return React.useSyncExternalStore(
+    subscribe,
+    () => window.innerWidth < MOBILE_BREAKPOINT,
+    false,
+  );
+}
 
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+function subscribe(callback: () => void) : () => void {
+  const mql = window.matchMedia(`not (min-width: ${MOBILE_BREAKPOINT}px)`);
 
-  return !!isMobile
+  mql.addEventListener('change', callback);
+
+  return () => mql.removeEventListener('change', callback);
 }
