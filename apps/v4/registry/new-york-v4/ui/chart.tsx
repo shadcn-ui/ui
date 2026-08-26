dbcba9810dd360240ee52ecb926bc7c49d1cbba8
Elysson Marconi <ellyssonmiike@hotmail.com>
2026-08-26 11:29:10 -0300

    fix(registry): add missing spacing to ChartTooltipContent

--- apps/v4/registry/new-york-v4/ui/chart.tsx
+++ apps/v4/registry/new-york-v4/ui/chart.tsx
@@ -242,7 +242,7 @@ function ChartTooltipContent({
                     )}
                     <div
                       className={cn(
-                        "flex flex-1 justify-between leading-none",
+                        "flex flex-1 justify-between leading-none gap-2",
                         nestLabel ? "items-end" : "items-center"
                       )}
                     >
