648d71b35425130a3c255db7c5e29ec3597e37ed
Wonu Lee <wonwoolee24@gmail.com>
2026-09-19 18:51:20 +0900

    fix(combobox): use Base UI subpath import

--- apps/v4/registry/bases/base/ui/combobox.tsx
+++ apps/v4/registry/bases/base/ui/combobox.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Combobox as ComboboxPrimitive } from "@base-ui/react"
+import { Combobox as ComboboxPrimitive } from "@base-ui/react/combobox"
 import { cn } from "cn"
 
 import { Button } from "@/registry/bases/base/ui/button"
